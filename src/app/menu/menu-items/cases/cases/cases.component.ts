9aea025c8e1340fd7bd2aeaef7d912028e2ce16c
YairTawil <yairtawil@gmail.com>
2017-03-19 10:36:54 +0200

    base cases component with infinite scroll (#4)

--- src/app/menu/menu-items/cases/cases/cases.component.ts
+++ src/app/menu/menu-items/cases/cases/cases.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['cases.component.scss']
 })
 export class CasesComponent implements OnInit {
-
+  i = 0;
+  count = 60;
+  cases = [];
   constructor() { }
 
+  getData(){
+    for(let index = this.i; index < this.i + this.count ; index++) {
+      index++ ;
+      this.cases.push(index);
+    }
+    this.i += this.count
+  }
   ngOnInit() {
+    this.getData();
+  }
+  ddd(){
+    this.getData();
   }
 
 }

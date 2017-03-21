a591ba5b6b90de8331658567e497f9b13b787b63
YairTawil <yairtawil@gmail.com>
2017-03-21 16:40:34 +0200

    create packages links with 'postinstall' in package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import {MenuItemsModule} from "./menu-items/menu-items.module";
+import {MenuItemsModule} from "@ansyn/menu-items";
 import {CoreModule} from "@ansyn/core";
 import {MenuModule} from "@ansyn/menu";
 

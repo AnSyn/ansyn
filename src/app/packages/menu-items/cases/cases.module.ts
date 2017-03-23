29c6bf14ebe1b43d3478b332f8132fa49c678fe4
Ohad <sadanoh@gmail.com>
2017-03-23 12:37:22 +0200

    same as previous commit

--- src/app/packages/menu-items/cases/cases.module.ts
+++ src/app/packages/menu-items/cases/cases.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CasesComponent } from './cases/cases.component';
-import {CoreModule, StoreService, MenuItem} from "@ansyn/core";
-import {InfiniteScrollModule} from "@ansyn/utils";
+import { CoreModule, StoreService, MenuItem } from "@ansyn/core";
+import { InfiniteScrollModule } from "@ansyn/utils";
 
 @NgModule({
   imports: [CommonModule, CoreModule, InfiniteScrollModule],

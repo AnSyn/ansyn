063bbf0b7fb3a9c9f275d173e49b11a0a0d60708
MarkShleifer <Mark.Shleifer@gmail.com>
2017-03-30 16:20:19 +0300

    added test to layer-tree-component: should initialize treeview after nodes was assigned

--- src/app/packages/menu-items/layers-manager/layer-tree/layer-tree.component.spec.ts
+++ src/app/packages/menu-items/layers-manager/layer-tree/layer-tree.component.spec.ts
@@ -1,12 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { LayerTreeComponent } from './layer-tree.component';
-import { TreeModule } from 'angular-tree-component';
 import { LayersManagerModule } from '../layers-manager.module';
 import { CoreModule } from '../../../core/core.module';
+import { ILayerTreeNode } from '../models/layer-tree-node'
 
 fdescribe('LayerTreeComponent', () => {
   let component: LayerTreeComponent;
   let fixture: ComponentFixture<LayerTreeComponent>;
+  const nodes: ILayerTreeNode[] = [{
+    name: 'Fields',
+    id: 1,
+    children: [
+      {
+        name: 'Rice Fields', id: 2, children: [
+          { name: 'Brown Rice', id: 5, children: [] },
+          { name: 'Persian Rice', id: 6, children: [] }]
+      },
+      { name: 'Wheat Fields', id: 3, children: [] },
+      { name: 'Oat Fields', id: 4, children: [] }
+    ]
+  }];
+
+  function flattenNodeTree(rootNode: ILayerTreeNode, flattenedArray: ILayerTreeNode[] = []): ILayerTreeNode[] {
+    flattenedArray.push(rootNode);
+
+    rootNode.children.forEach((child: ILayerTreeNode) => flattenedArray.concat(flattenNodeTree(child, flattenedArray)));
+
+    return flattenedArray;
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,4 +47,19 @@ fdescribe('LayerTreeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize treeview after nodes was assigned', () => {
+    component.source = nodes;
+    fixture.detectChanges();
+    component.treeComponent.treeModel.expandAll();
+    fixture.detectChanges();
+
+    const allSpans: Array<HTMLSpanElement> = Array.from<HTMLSpanElement>(fixture.nativeElement.querySelectorAll('span.title'));
+    const allNodes: ILayerTreeNode[] = flattenNodeTree(nodes[0]);
+
+    expect(allSpans.length).toEqual(allNodes.length);
+    expect(allSpans.map(span => span.textContent)).toEqual(allNodes.map(node => node.name));
+  });
+
+
 });

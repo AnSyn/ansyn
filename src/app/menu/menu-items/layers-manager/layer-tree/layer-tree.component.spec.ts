b28db9d96235aaf17b0b9a4153b039a4fc8e7a56
MarkShleifer <Mark.Shleifer@gmail.com>
2017-03-30 11:05:56 +0300

    fixed unit test of LayerTreeComponent

--- src/app/menu/menu-items/layers-manager/layer-tree/layer-tree.component.spec.ts
+++ src/app/menu/menu-items/layers-manager/layer-tree/layer-tree.component.spec.ts
@@ -1,14 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LayerTreeComponent } from './layer-tree.component';
 import { TreeModule } from 'angular-tree-component';
-import { DataLayersModule } from '../layers-manager.module';
-describe('LayerTreeComponent', () => {
+import { LayersManagerModule } from '../layers-manager.module';
+import { CoreModule } from '../../../core/core.module';
+
+fdescribe('LayerTreeComponent', () => {
   let component: LayerTreeComponent;
   let fixture: ComponentFixture<LayerTreeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [DataLayersModule],
+      imports: [LayersManagerModule, CoreModule],
     })
       .compileComponents();
   }));

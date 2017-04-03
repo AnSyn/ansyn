b2da6f13b3d56f81165e84ae7c3a88878226499e
MarkShleifer <Mark.Shleifer@gmail.com>
2017-04-03 22:02:29 +0300

    Layer-tree.component is now able to get input indicating wheter a node is selected or not. Added test to layer-tree.component which tests the input

--- src/app/packages/menu-items/layers-manager/layer-tree/layer-tree.component.spec.ts
+++ src/app/packages/menu-items/layers-manager/layer-tree/layer-tree.component.spec.ts
@@ -10,10 +10,25 @@ import { TreeNode } from 'angular-tree-component';
 fdescribe('LayerTreeComponent', () => {
   let component: LayerTreeComponent;
   let fixture: ComponentFixture<LayerTreeComponent>;
-  const nodes: ILayerTreeNode[] = [{
+  let allFalseNodes: ILayerTreeNode[] = [{
     name: 'Fields',
     id: 1,
     isChecked: false,
+    children: [
+      {
+        name: 'Rice Fields', id: 2, isChecked: false, children: [
+          { name: 'Brown Rice', id: 5, isChecked: false, children: [] },
+          { name: 'Persian Rice', id: 6, isChecked: false, children: [] }]
+      },
+      { name: 'Wheat Fields', id: 3, isChecked: false, children: [] },
+      { name: 'Oat Fields', id: 4, isChecked: false, children: [] }
+    ]
+  }];
+
+  let trueRootNodes: ILayerTreeNode[] = [{
+    name: 'Fields',
+    id: 1,
+    isChecked: true,
     children: [
       {
         name: 'Rice Fields', id: 2, isChecked: false, children: [
@@ -51,15 +66,13 @@ fdescribe('LayerTreeComponent', () => {
   });
 
   it('should initialize treeview after nodes was assigned', () => {
-    component.source = nodes;
+    component.source = allFalseNodes;
     fixture.detectChanges();
     component.treeComponent.treeModel.expandAll();
     fixture.detectChanges();
-    component.treeComponent.treeModel.virtualScroll.setNewScroll({ viewport: fixture.nativeElement });
-    fixture.detectChanges();
 
     const allSpans: Array<HTMLSpanElement> = Array.from<HTMLSpanElement>(fixture.nativeElement.querySelectorAll('span.title'));
-    const allNodes: ILayerTreeNode[] = flattenNodeTree(nodes[0]);
+    const allNodes: ILayerTreeNode[] = flattenNodeTree(allFalseNodes[0]);
 
     expect(allSpans.length).toEqual(allNodes.length);
     expect(allSpans.map(span => span.textContent)).toEqual(allNodes.map(node => node.name));
@@ -68,12 +81,11 @@ fdescribe('LayerTreeComponent', () => {
   it('selection in the root should bubble to its children', () => {
     let activatedNodes: TreeNode[] = [];
 
-    component.source = nodes;
+    component.source = allFalseNodes;
     fixture.detectChanges();
     component.treeComponent.treeModel.expandAll();
     fixture.detectChanges();
-    component.treeComponent.treeModel.virtualScroll.setNewScroll({ viewport: fixture.nativeElement });
-    fixture.detectChanges();
+
     component.nodeActivationChanged.subscribe((event) => {
       if (event.newState) {
         activatedNodes.push(event.node);
@@ -83,8 +95,8 @@ fdescribe('LayerTreeComponent', () => {
       }
     });
 
-    const rootDiv: HTMLElement = fixture.debugElement.query(By.css('#node' + nodes[0].id)).nativeElement;
-    const allNodes: ILayerTreeNode[] = flattenNodeTree(nodes[0]);
+    const rootDiv: HTMLElement = fixture.debugElement.query(By.css('#node' + allFalseNodes[0].id)).nativeElement;
+    const allNodes: ILayerTreeNode[] = flattenNodeTree(allFalseNodes[0]);
 
     rootDiv.click();
     fixture.detectChanges();
@@ -106,12 +118,11 @@ fdescribe('LayerTreeComponent', () => {
 
   it('selection in sub children should select the parent, and set indeterminate in the grandparent', () => {
     let activatedNodes: TreeNode[] = [];
-    component.source = nodes;
+    component.source = allFalseNodes;
     fixture.detectChanges();
     component.treeComponent.treeModel.expandAll();
     fixture.detectChanges();
-    component.treeComponent.treeModel.virtualScroll.setNewScroll({ viewport: fixture.nativeElement });
-    fixture.detectChanges();
+
     component.nodeActivationChanged.subscribe((event) => {
       if (event.newState) {
         activatedNodes.push(event.node);
@@ -148,12 +159,11 @@ fdescribe('LayerTreeComponent', () => {
 
   it('selection and unselection in sub children should return to the initial state', () => {
     let activatedNodes: TreeNode[] = [];
-    component.source = nodes;
+    component.source = allFalseNodes;
     fixture.detectChanges();
     component.treeComponent.treeModel.expandAll();
     fixture.detectChanges();
-    component.treeComponent.treeModel.virtualScroll.setNewScroll({ viewport: fixture.nativeElement });
-    fixture.detectChanges();
+
     component.nodeActivationChanged.subscribe((event) => {
       if (event.newState) {
         activatedNodes.push(event.node);
@@ -189,5 +199,39 @@ fdescribe('LayerTreeComponent', () => {
 
     const riceParentInput: HTMLInputElement = fixture.debugElement.query(By.css('#node1 > input')).nativeElement;
     expect(riceParentInput.indeterminate).toBe(false);
+  });
+
+  it('the component should obey to the initial state of the nodes', () => {
+    let activatedNodes: TreeNode[] = [];
+
+    component.nodeActivationChanged.subscribe((event) => {
+      if (event.newState) {
+        activatedNodes.push(event.node);
+      } else {
+        let nodeIndex: number = activatedNodes.indexOf(event.node)
+        activatedNodes.splice(nodeIndex, 1);
+      }
+    });
+
+    component.source = trueRootNodes;
+    fixture.detectChanges();
+    component.treeComponent.treeModel.expandAll();
+    fixture.detectChanges();
+    component.ngAfterViewInit();
+    fixture.detectChanges();
+
+    expect(activatedNodes.find(node => node.data.id === 1)).toBeTruthy();
+    expect(activatedNodes.find(node => node.data.id === 2)).toBeTruthy();
+    expect(activatedNodes.find(node => node.data.id === 3)).toBeTruthy();
+    expect(activatedNodes.find(node => node.data.id === 4)).toBeTruthy();
+    expect(activatedNodes.find(node => node.data.id === 5)).toBeTruthy();
+    expect(activatedNodes.find(node => node.data.id === 6)).toBeTruthy();
+
+    expect(fixture.debugElement.query(By.css('#node1 > input')).nativeElement.checked).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#node2 > input')).nativeElement.checked).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#node3 > input')).nativeElement.checked).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#node4 > input')).nativeElement.checked).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#node5 > input')).nativeElement.checked).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('#node6 > input')).nativeElement.checked).toBeTruthy();
   });
 });

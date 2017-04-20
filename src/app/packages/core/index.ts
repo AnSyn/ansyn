b578fe495223d2f71a2fe52f39ac6b5c1ef43eca
YairTawil <yairtawil@gmail.com>
2017-04-20 18:01:24 +0300

    rebase fix

--- src/app/packages/core/index.ts
+++ src/app/packages/core/index.ts
@@ -1,8 +1,8 @@
-export {CoreModule} from "./core.module";
-export {StoreService} from "./store.service";
-export {MenuService} from "./services/menu.service";
-export {MenuItem} from "./models/menu-item.model";
-export {Case} from "./models/case.model";
-export {CasesService} from "./services/cases.service";
+export { CoreModule } from "./core.module";
+export { StoreService } from "./store.service";
+export { MenuService } from "./services/menu.service";
+export { MenuItem } from "./models/menu-item.model";
+export { Case } from "./models/case.model";
+export { CasesService, CaseModal } from "./services/cases.service";
 export { DataLayersService } from './services/data-layers.service';
 export { ILayerTreeNode } from './models/layer-tree-node';

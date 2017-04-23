7274323d422d4c9a7a247003f676120a67dac569
YairTawil <yairtawil@gmail.com>
2017-04-23 10:40:52 +0300

    update describe for casesService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { CoreModule } from "@ansyn/core";
-import { MenuModule } from "@ansyn/menu";
+import { CoreModule } from '@ansyn/core';
+import { MenuModule } from '@ansyn/menu';
 import { TimelineModule } from '@ansyn/timeline';
 import { MenuItemsModule } from "@ansyn/menu-items";
 

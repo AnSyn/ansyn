0092b493ab7c22de8f03981396add137ecb781b2
asaf masa <asaf.masa@gmail.com>
2017-04-25 18:10:55 +0300

    code review fix - remove js files

--- src/app/packages/imagery/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Created by AsafMasa on 24/04/2017.
- */
-var imagery_module_1 = require('./imagery.module');
-exports.ImageryModule = imagery_module_1.ImageryModule;
--- src/app/packages/imagery/model/model.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by AsafMasa on 25/04/2017.
- */
-"use strict";

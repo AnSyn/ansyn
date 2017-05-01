1cefe1a04e851f510fe0b260d6e2c8b9a105e330
YairTawil <yairtawil@gmail.com>
2017-05-01 13:58:08 +0300

    rebase with master

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,17 +1,17 @@
 import { TimelineReducer } from '@ansyn/timeline';
-import { compose } from '@ngrx/core/compose';
-import { ActionReducer } from '@ngrx/store';
+import { casesReducer } from '@ansyn/menu-items/cases';
 
-export const  AppReducers = {    
-	overlays : TimelineReducer
-}
+export const AppReducers = {
+	overlays: TimelineReducer,
+	cases: casesReducer
+};
 
-//export const tmpCombineReducers = combineReducers; 
+//export const tmpCombineReducers = combineReducers;
 
 
 
 //export { combineReducers } from '@ngrx/store';
 
-/*export function AppReducers() { 
-	return combineReducers(reducers); 
+/*export function AppReducers() {
+	return combineReducers(reducers);
 };*/

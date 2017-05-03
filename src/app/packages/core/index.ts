2383adfc36cba976673165f03fc56db6e2fbfc20
YairTawil <yairtawil@gmail.com>
2017-05-03 10:52:47 +0300

    fix import ILayerTreeNode

--- src/app/packages/core/index.ts
+++ src/app/packages/core/index.ts
@@ -3,4 +3,4 @@ export { StoreService } from "./store.service";
 export { MenuService } from "./services/menu.service";
 export { MenuItem } from "./models/menu-item.model";
 export { DataLayersService } from './services/data-layers.service';
-export { ILayerTreeNode } from './models/layer-tree-node';
+export { ILayerTreeNode } from './models/data-layers/layer-tree-node';

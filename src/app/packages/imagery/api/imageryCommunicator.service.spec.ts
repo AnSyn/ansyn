d6b4b758dce0e05b6477839fb5a53edb5e71d857
asafMasa <asaf.masa@gmail.com>
2017-05-08 17:24:20 +0300

    fix pull request comments

---
+++ src/app/packages/imagery/api/imageryCommunicator.service.spec.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by AsafMas on 08/05/2017.
+ */
+
+import { async, inject, TestBed } from '@angular/core/testing';
+import { ImageryCommunicatorService } from '../api/imageryCommunicator.service';
+
+describe('ImageryCommunicator', () => {
+	let imageryCommunicatorService: ImageryCommunicatorService;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({declarations: [], providers: [ ImageryCommunicatorService]}).compileComponents();
+	}));
+
+	beforeEach(inject([ImageryCommunicatorService], (_imageryCommunicatorService) => {
+		imageryCommunicatorService = _imageryCommunicatorService;
+	}));
+
+	it('should create "imageryCommunicatorService" service', () => {
+		expect(imageryCommunicatorService).toBeTruthy();
+	});
+});
---
+++ src/app/packages/imagery/imageryProviderService/imageryProvider.service.spec.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by AsafMas on 08/05/2017.
+ */
+
+import { async, inject, TestBed } from '@angular/core/testing';
+import { ImageryProviderService } from '../imageryProviderService/imageryProvider.service';
+
+describe('ImageryProviderService', () => {
+	let imageryProviderService: ImageryProviderService;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({declarations: [], providers: [ ImageryProviderService]}).compileComponents();
+	}));
+
+	beforeEach(inject([ImageryProviderService], (_ImageryProviderService) => {
+		imageryProviderService = _ImageryProviderService;
+	}));
+
+	it('should create "ImageryProviderService" service', () => {
+		expect(ImageryProviderService).toBeTruthy();
+	});
+});

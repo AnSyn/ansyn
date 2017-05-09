962bae704f7e5b03bd99d553a54ed9cea7497097
osadan <sadanoh@gmail.com>
2017-05-09 09:49:43 +0300

    fix some simple bug

--- src/app/effects/cases.app.effects.ts
+++ src/app/effects/cases.app.effects.ts
@@ -3,11 +3,14 @@ import { Actions, Effect } from '@ngrx/effects';
 import { Action, Store } from '@ngrx/store';
 import { IAppState } from '../app.reducers.module';
 import { Observable } from 'rxjs';
-import { ActionTypes } from '@ansyn/timeline/actions/timeline.actions';
+import { ActionTypes } from '@ansyn/overlays/actions/overlays.actions';
 import { CasesService } from '@ansyn/menu-items/cases';
 import { ICasesState } from '@ansyn/menu-items/cases';
 import { Case } from '@ansyn/menu-items/cases';
 import { UpdateCaseSuccessAction } from '@ansyn/menu-items/cases';
+import { empty } from 'rxjs/observable/empty';
+
+import 'rxjs/add/operator/withLatestFrom';
 
 @Injectable()
 export class CasesAppEffects{
@@ -20,6 +23,9 @@ export class CasesAppEffects{
 		.withLatestFrom(this.store$.select('cases'))
 		.switchMap( ([action, state]: [{payload: any}, ICasesState]) => {
 			let selected_case: Case = state.cases.find((case_value) =>  case_value.id == state.selected_case_id);
+			if(!selected_case) {
+				return Observable.empty();
+			}
 			selected_case.state.selected_overlay_id = action.payload;
 			return this.casesService.updateCase(selected_case).map((updated_case) => {
 				return new UpdateCaseSuccessAction(updated_case);

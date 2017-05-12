1aeb003009b96276f8d76a40a5960da28c843da7
MarkShleifer <Mark.Shleifer@gmail.com>
2017-05-12 19:16:46 +0300

    added tests to layer.app.effects and to map.app.effects

---
+++ src/app/effects/layers.app.effects.spec.ts
@@ -0,0 +1,39 @@
+import { LayersReducer } from '@ansyn/menu-items/layers-manager/reducers/layers.reducer';
+import { BeginLayerTreeLoadAction } from '@ansyn/menu-items/layers-manager/actions/layers.actions';
+import { SelectCaseAction } from './../packages/menu-items/cases/actions/cases.actions';
+import { EffectsRunner, EffectsTestingModule } from '@ngrx/effects/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
+import { LayersAppEffects } from './layers.app.effects';
+import { Observable } from 'rxjs/Observable';
+import { Store, StoreModule } from '@ngrx/store';
+
+describe('LayersAppEffects', () => {
+	let layersAppEffects: LayersAppEffects;
+	let effectsRunner: EffectsRunner;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [EffectsTestingModule,StoreModule.provideStore({layers : LayersReducer})],
+			providers: [LayersAppEffects]
+
+		}).compileComponents();
+	}));
+
+	beforeEach(inject([LayersAppEffects, EffectsRunner], (_layersAppEffects: LayersAppEffects, _effectsRunner: EffectsRunner) => {
+		layersAppEffects = _layersAppEffects;
+		effectsRunner = _effectsRunner;
+	}));
+
+	it('selectOverlay$ should push overlay.id to selected_overlays_ids on cases', () => {
+		let selectedCaseId = 'asdfasdf';
+
+		effectsRunner.queue(new SelectCaseAction(selectedCaseId));
+		let result: BeginLayerTreeLoadAction ;
+		layersAppEffects.selectCase$.subscribe( (_result: BeginLayerTreeLoadAction) => {
+			result = _result;
+		});
+
+		expect(result instanceof BeginLayerTreeLoadAction).toBeTruthy();
+		expect(result.payload).toEqual({caseId: selectedCaseId});
+	});
+});

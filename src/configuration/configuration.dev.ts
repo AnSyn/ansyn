e6c06d0f187ec1ec321f78c790c62e18b1206963
osadan <sadanoh@gmail.com>
2017-05-17 17:56:11 +0300

    add env to the configuration

--- src/configuration/configuration.dev.ts
+++ src/configuration/configuration.dev.ts
@@ -4,6 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const configuration = {
+  env: 'dev',
   production: false,
   CasesConfig: {
     casesBaseUrl: 'http://localhost:9001/api/v1/cases'
--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -1,4 +1,5 @@
 export const configuration = {
+  env: 'default',
   production: false,
   CasesConfig: {
     casesBaseUrl: 'http://localhost:9001/api/v1/cases'

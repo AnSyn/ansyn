d3fd0aae5176537d89ebc9c693fda3dfe444b4f5
osadan <sadanoh@gmail.com>
2017-05-18 10:31:32 +0300

    fix the configuration errors

--- src/app/packages/overlays/index.ts
+++ src/app/packages/overlays/index.ts
@@ -1,9 +1,9 @@
 export { OverlaysModule } from './overlays.module';
 export { TimelineEmitterService } from './services/timeline-emitter.service';
-export { OverlaysService,overlaysConfig } from './services/overlays.service';
+export { OverlaysService,OverlaysConfig } from './services/overlays.service';
 export { OverlaysActionTypes,SelectOverlayAction, UnSelectOverlayAction, LoadOverlaysAction, LoadOverlaysSuccessAction, LoadOverlaysFailAction,	ClearFilter, SetFilter, DemoAction}  from './actions/overlays.actions' ;
 export { OverlaysEffects } from './effects/overlays.effects';
 export { Overlay } from './models/overlay.model';
-export { IOverlayConfig } from './models/overlays.config';
+export { IOverlaysConfig } from './models/overlays.config';
 export { IOverlayState,overlayInitialState,OverlayReducer }  from './reducers/overlays.reducer';
 export { OverlaysContainer } from './container/overlays-container.component';

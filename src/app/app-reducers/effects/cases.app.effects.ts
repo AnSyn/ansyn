e9893ed48e481f7b014bb747904dce77535feb3c
osadan <sadanoh@gmail.com>
2017-06-04 09:38:38 +0300

    fix conflicts

--- src/app/app-reducers/effects/cases.app.effects.ts
+++ src/app/app-reducers/effects/cases.app.effects.ts
@@ -8,7 +8,11 @@ import { ICasesState } from '@ansyn/menu-items/cases';
 import { Case } from '@ansyn/menu-items/cases';
 import { UpdateCaseSuccessAction, CasesActionTypes } from '@ansyn/menu-items/cases';
 import 'rxjs/add/operator/withLatestFrom';
+
+
 import '@ansyn/core/utils/debug';
+import { MapActionTypes, PositionChangedAction } from '@ansyn/map-facade/actions/map.actions';
+
 import { IAppState } from '../';
 import { isEmpty } from 'lodash';
 import "@ansyn/core/utils/clone-deep";
@@ -88,6 +92,21 @@ export class CasesAppEffects {
 			return new LoadOverlaysAction(overlayFilter);
 		});
 
-	constructor(private actions$: Actions, private store$: Store<IAppState>, private casesService: CasesService) {}
+	@Effect()
+	positionChanged$: Observable<UpdateCaseSuccessAction> = this.actions$
+		.ofType(MapActionTypes.POSITION_CHANGED)
+		.withLatestFrom(this.store$.select('cases'))
+		.switchMap( ([action, state]: [PositionChangedAction, ICasesState]) => {
+			const selected_case: Case = state.cases.find((case_value) =>  case_value.id === state.selected_case_id);
+			if(!selected_case){
+				return Observable.empty();
+			}
+			selected_case.state.maps = [{position: action.payload}];
+			return this.casesService.updateCase(selected_case).map((updated_case) => {
+				return new UpdateCaseSuccessAction(updated_case);
+			});
+		});
+	
+	constructor(private actions$: Actions, private store$: Store<IAppState>, private casesService: CasesService){}
 
 }

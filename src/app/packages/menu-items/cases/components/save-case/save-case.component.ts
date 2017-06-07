31f8ffc5719a8f34fb7c70437cd174bf87ac95da
MarkShleifer <Mark.Shleifer@gmail.com>
2017-06-07 10:58:42 +0300

    casic save of default_case

--- src/app/packages/menu-items/cases/components/save-case/save-case.component.ts
+++ src/app/packages/menu-items/cases/components/save-case/save-case.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild, ElementRef, ChangeDetectionStrategy } from '@angular/core';
+import { transition, trigger, style, animate } from '@angular/animations';
+import { Observable } from 'rxjs/Observable';
+import { Case } from '../../models/case.model';
+import { Store } from '@ngrx/store';
+import { ICasesState } from '../../reducers/cases.reducer';
+import * as _ from "lodash";
+import { AddCaseAction, CloseModalAction } from '../../actions/cases.actions';
+
+const animations_during = '0.2s';
+
+const animations: any[] = [
+  trigger('modalContent', [
+    transition(":enter", [style({ 'backgroundColor': '#27b2cf', transform: 'translate(0, -100%)' }), animate(animations_during, style({ 'backgroundColor': 'white', transform: 'translate(0, 0)' }))]),
+    transition(":leave", [style({ 'backgroundColor': 'white', transform: 'translate(0, 0)' }), animate(animations_during, style({ 'backgroundColor': '#27b2cf', transform: 'translate(0, -100%)' }))]),
+  ])
+];
+
+const host = {
+  "[@modalContent]": "true",
+  "[style.display]": "'block'",
+  "[style.position]": "'absolute'",
+  "[style.width]": "'337px'",
+  "[style.top]": "'15px'",
+  "[style.left]": "'30px'"
+};
 
 @Component({
   selector: 'ansyn-save-case',
   templateUrl: './save-case.component.html',
-  styleUrls: ['./save-case.component.less']
+  styleUrls: ['./save-case.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations,
+  host
 })
 export class SaveCaseComponent implements OnInit {
 
-  constructor() { }
+  active_case$: Observable<Case> = this.store.select("cases")
+    .distinctUntilChanged(this.distinctUntilChangedActiveCase.bind(this))
+    .map(this.getCloneActiveCase.bind(this));
 
-  ngOnInit() {
+  case_model: Case;
+  on_edit_case = false;
+
+  @ViewChild("name_input") name_input: ElementRef;
+
+  @HostListener("@modalContent.done") selectText() {
+    this.name_input.nativeElement.select();
   }
 
+  constructor(private store: Store<ICasesState>) { }
+
+  distinctUntilChangedActiveCase(state_prev: ICasesState, state_current: ICasesState) {
+    return _.isEqual(state_prev.active_case_id, state_current.active_case_id);
+  }
+
+  getCloneActiveCase(case_state: ICasesState): Case {
+    let s_case: Case = case_state.selected_case;
+    return s_case;
+  }
+
+  ngOnInit(): void {
+    this.active_case$.subscribe((active_case: Case) => {
+      this.case_model = active_case;
+    });
+  }
+
+  close(): void {
+    this.store.dispatch(new CloseModalAction());
+  }
+
+  onSubmitCase() {
+    this.store.dispatch(new AddCaseAction(this.case_model));
+  }
 }
+

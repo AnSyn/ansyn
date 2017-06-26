d7a8b291c3cbd8d6fe0ac0b4eb18aa7a199e6288
asafMasa <asaf.masa@gmail.com>
2017-06-26 11:19:21 +0300

    add utils spinner logic to ImageryContainer

--- src/app/packages/map-facade/components/imagery-container/imagery-container.component.ts
+++ src/app/packages/map-facade/components/imagery-container/imagery-container.component.ts
@@ -1,25 +1,41 @@
-import { Component, ElementRef, Input, AfterViewInit, ViewChild, OnDestroy, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, ElementRef, Input, AfterViewInit, ViewChild, OnDestroy, OnInit } from '@angular/core';
 import { ImageryComponentSettings } from '@ansyn/imagery';
-import { ImageryCommunicatorService } from '../../../imagery/communicator-service/communicator.service';
-//import { SpinnerService, Spinner } from '@ansyn/loading-spinner';
+import { Spinner } from '@ansyn/core/utils/spinner';
 
 @Component({
 	selector: 'ansyn-imagery-container',
 	templateUrl: './imagery-container.component.html',
-	styleUrls: ['./imagery-container.component.less', '../../../loading-spinner/loading-spinner.less']
+	styleUrls: ['./imagery-container.component.less']
 })
 export class ImageryContainerComponent implements OnInit, AfterViewInit, OnDestroy {
 	@Input() mapComponentSettings: ImageryComponentSettings;
 	@Input() active: boolean;
 	@Input('show-status') showStatus: boolean;
 
-	// @ViewChild('spinnerLoader') spinnerLoaderElement: ElementRef;
-    //
-	// private spinner: Spinner;
-	@Input()
-	public showSpinner: boolean;
-	constructor(private cdRef: ChangeDetectorRef,
-				private imageryCommunicatorService: ImageryCommunicatorService/*private spinnerService: SpinnerService*/) {
+	@ViewChild('imageryViewContainer') imageryViewContainer: ElementRef;
+	private _showSpinner: boolean;
+
+	public get showSpinner(): boolean {
+		return this._showSpinner;
+	}
+
+	public set showSpinner(value: boolean) {
+		if (this._showSpinner === value) {
+			return;
+		}
+
+		this._showSpinner = value;
+		if (this._spinner) {
+			if (this._showSpinner) {
+				this._spinner.start("Loading...", 'transparent');//#EDEDED
+			} else {
+				this._spinner.stop();
+			}
+		}
+	}
+
+	private _spinner: Spinner;
+	constructor() {
 		this.showSpinner = false;
 	}
 
@@ -27,8 +43,13 @@ export class ImageryContainerComponent implements OnInit, AfterViewInit, OnDestr
 	}
 
 	ngAfterViewInit() {
+		this._spinner = new Spinner(this.imageryViewContainer.nativeElement);
 	}
 
 	ngOnDestroy(): void {
+		if (this._spinner) {
+			this._spinner.stop();
+			this._spinner = null;
+		}
 	}
 }

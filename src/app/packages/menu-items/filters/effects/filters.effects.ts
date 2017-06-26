45318b3f532c6add7ddc4cad0febcc48623645ab
MarkShleifer <Mark.Shleifer@gmail.com>
2017-06-26 17:23:06 +0300

    added file

---
+++ src/app/packages/menu-items/filters/effects/filters.effects.ts
@@ -0,0 +1,62 @@
+import { TypeContainerService } from '@ansyn/type-container';
+import { FilterMetadata } from '../models/metadata/filter-metadata.interface';
+import { FiltersActionTypes, InitializeFiltersAction, InitializeFiltersSuccessAction, InitializeSingleFilterAction } from '../actions/filters.actions';
+import { IFiltersState } from '../reducer/filters.reducer';
+import { FiltersService } from '../services/filters.service';
+import { Filter } from '../models/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/switchMap';
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Effect, Actions, toPayload } from '@ngrx/effects';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class FiltersEffects {
+
+    @Effect()
+    initializeFilters$: Observable<InitializeFiltersSuccessAction> = this.actions$
+        .ofType(FiltersActionTypes.INITIALIZE_FILTERS)
+        .switchMap((action: InitializeFiltersAction) => {
+            return this.filtersService.loadFilters().mergeMap((filters: Filter[]) => {
+                const actionsArray = [];
+                const filterMetadatas: Map<Filter, FilterMetadata> = new Map<Filter, FilterMetadata>();
+
+                filters.forEach((filter: Filter) => {
+                    const metadata: FilterMetadata = this.initializeMetadata(filter, action.payload.facets);
+
+                    actionsArray.push(new InitializeSingleFilterAction({ filter: filter, metadata: metadata }));
+
+                    action.payload.overlays.forEach((overlay: any) => {
+                        metadata.accumulateData(overlay[filter.modelName]);
+                    });
+                    
+                    filterMetadatas.set(filter, metadata);
+                });
+
+                actionsArray.push(new InitializeFiltersSuccessAction(filterMetadatas));
+                return Observable.from(actionsArray);
+            });
+        }).share();
+
+    constructor(private actions$: Actions,
+        private filtersService: FiltersService,
+        private store: Store<IFiltersState>,
+        private typeContainerService: TypeContainerService) { }
+
+    initializeMetadata(filter: Filter, facets: { filters: { fieldName: string, metadata: any }[] }): FilterMetadata {
+        const metaData: FilterMetadata = this.typeContainerService.resolve(FilterMetadata, filter.type);
+        const clonedMetadata: FilterMetadata = Object.assign(Object.create(metaData), metaData); //TODO: remove this when a non-singelton resolve will be available
+
+        const currentFilterInit = facets
+            && facets.filters.find(field => {
+                return field.fieldName === filter.modelName;
+            });
+
+        clonedMetadata.initializeFilter(currentFilterInit && currentFilterInit.metadata);
+
+        return clonedMetadata;
+    }
+}
+

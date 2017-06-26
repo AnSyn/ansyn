12ba8ed8f2efd1d276da263a77c1e544be516003
MarkShleifer <Mark.Shleifer@gmail.com>
2017-06-26 17:22:38 +0300

    moved some effects to facets

--- src/app/app-reducers/effects/filters.app.effects.ts
+++ src/app/app-reducers/effects/filters.app.effects.ts
@@ -20,30 +20,11 @@ export class FiltersAppEffects {
     initializeFilters$: Observable<any> = this.actions$
         .ofType(OverlaysActionTypes.LOAD_OVERLAYS_SUCCESS)
         .map(toPayload)
-        .withLatestFrom(this.store$.select('cases'))
-        .switchMap(([overlays, casesState]: [Overlay[], ICasesState]) => {
-            return this.filtersService.loadFilters().mergeMap((filters: Filter[]) => {
-                let actionsArray = [];
-                let filterMetadatas: Map<Filter, FilterMetadata> = new Map<Filter, FilterMetadata>();
-
-                filters.forEach((filter: Filter) => {
-                    const metadata: FilterMetadata = this.initializeMetadata(filter, casesState);
-
-                    actionsArray.push(new SetFilter({
-                        filteringParams: { key: filter.modelName, metadata: metadata },
-                        filterFunc: metadata.filterFunc
-                    }));
-
-                    overlays.forEach((overlay: Overlay) => {
-                        metadata.accumulateData(overlay[filter.modelName]);
-                    });
-
-                    filterMetadatas.set(filter, metadata);
-                });
-
-                actionsArray.push(new InitializeFiltersAction(filterMetadatas));
-                return Observable.from(actionsArray);
-            });
+        .withLatestFrom(this.store$.select('cases'), (overlays: Overlay[], casesState: ICasesState): any => {
+            return [overlays, casesState.selected_case.state.facets];
+        })
+        .map(([overlays, facets]: [Overlay[], any]) => {
+            return new InitializeFiltersAction({ overlays: overlays, facets: facets });
         }).share();
 
     @Effect()
@@ -68,8 +49,20 @@ export class FiltersAppEffects {
         .share();
 
     @Effect()
+    initializeSingleFilter$: Observable<any> = this.actions$
+        .ofType(FiltersActionTypes.INITIALIZE_SINGLE_FILTER)
+        .map(toPayload)
+        .map((payload: { filter: Filter, metadata: FilterMetadata }) => {
+            return new SetFilter({
+                filteringParams: { key: payload.filter.modelName, metadata: payload.metadata },
+                filterFunc: payload.metadata.filterFunc
+            });
+        })
+        .share();
+
+    @Effect()
     resetFilters$: Observable<any> = this.actions$
-        .ofType(CasesActionTypes.SELECT_CASE_BY_ID)
+        .ofType(OverlaysActionTypes.LOAD_OVERLAYS)
         .map((action) => {
             return new ResetFiltersAction();
         });
@@ -78,20 +71,6 @@ export class FiltersAppEffects {
         private store$: Store<IAppState>,
         private filtersService: FiltersService,
         private typeContainerService: TypeContainerService) { }
-
-    initializeMetadata(filter: Filter, casesState: ICasesState): FilterMetadata {
-        const metaData: FilterMetadata = this.typeContainerService.resolve(FilterMetadata, filter.type);
-        const clonedMetadata: FilterMetadata = Object.assign(Object.create(metaData), metaData); //TODO: remove this when a non-singelton resolve will be available
-
-        const currentFilterInit = casesState.selected_case.state.facets
-            && casesState.selected_case.state.facets.filters.find(field => {
-                return field.fieldName === filter.modelName;
-            });
-
-        clonedMetadata.initializeFilter(currentFilterInit && currentFilterInit.metadata);
-
-        return clonedMetadata;
-    }
 
     updateSelectedCase(filter: Filter, newMetadata: FilterMetadata, casesState: ICasesState): Case {
         const selected_case: Case = cloneDeep(casesState.selected_case);

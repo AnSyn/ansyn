fa2a53d94be99c2d09bef19a34ec64cb36cf4986
asafMasa <asaf.masa@gmail.com>
2017-07-03 14:47:59 +0300

    ImageryProviderService tests added

--- src/app/packages/imagery/provider-service/provider.service.spec.ts
+++ src/app/packages/imagery/provider-service/provider.service.spec.ts
@@ -19,4 +19,37 @@ describe('ImageryProviderService', () => {
 	it('should create "ImageryProviderService" service', () => {
 		expect(ImageryProviderService).toBeTruthy();
 	});
+
+	it('should register and provide a map', () => {
+
+		const mapComponent = { Id: "test"};
+
+		imageryProviderService.registerMapProvider("map1", mapComponent);
+		imageryProviderService.registerMapProvider("map2", { Id: "test2"});
+
+		expect(imageryProviderService.provideMap("map1")).toEqual(mapComponent);
+	});
+
+	class Plugin1 {
+		Id = "test1";
+	}
+
+	class Plugin2 {
+		Id = "test2";
+	}
+
+	class Plugin3 {
+		Id = "test3";
+	}
+	it('should register and createPlugins plugins by map type', () => {
+
+		imageryProviderService.registerPlugin("map1", "plugin1", Plugin1);
+		imageryProviderService.registerPlugin("map1", "plugin2", Plugin2);
+		imageryProviderService.registerPlugin("map2", "plugin3", Plugin3);
+
+		const pluggins = imageryProviderService.createPlugins("map1");
+		expect(pluggins.length).toEqual(2);
+		expect((<any>pluggins[0]).Id).toEqual(new Plugin1().Id);
+		expect((<any>pluggins[1]).Id).toEqual(new Plugin2().Id);
+	});
 });

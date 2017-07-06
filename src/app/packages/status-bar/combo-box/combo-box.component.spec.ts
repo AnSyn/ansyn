1a89987f5781d957adb6843e1e037e84a198b110
YairTawil <yairtawil@gmail.com>
2017-07-06 09:53:48 +0300

    Add unit testing for combo-box

--- src/app/packages/status-bar/combo-box/combo-box.component.spec.ts
+++ src/app/packages/status-bar/combo-box/combo-box.component.spec.ts
@@ -22,4 +22,53 @@ describe('ComboBoxComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+	it('should set selectIndex change value of _selectedIndex and call selectedIndexChange.emit with the new value', () => {
+		spyOn(component.selectedIndexChange, 'emit');
+		(<any>component)._selectedIndex = 0;
+		component.selectedIndex = 6;
+		expect(component.selectedIndexChange.emit).toHaveBeenCalledWith(6);
+		expect((<any>component)._selectedIndex).toEqual(6)
+	});
+
+	it('toggleShow should toggle "visibility" of optionsContainer("visible" and "hidden"). on "visible" focus() should have been call too', () => {
+		const optionsContainerStyle: CSSStyleDeclaration = component.optionsContainer.nativeElement.style;
+		spyOn(component.optionsContainer.nativeElement, 'focus');
+		optionsContainerStyle.visibility = 'hidden';
+		component.toggleShow();
+		expect(optionsContainerStyle.visibility).toEqual('visible');
+		expect(component.optionsContainer.nativeElement.focus).toHaveBeenCalled();
+		optionsContainerStyle.visibility = 'visible';
+		component.toggleShow();
+		expect(optionsContainerStyle.visibility).toEqual('hidden');
+	});
+
+	describe('onBlurOptionsContainer change "visibility" of optionsContainer to "hidden". only if relatedTarget is not "trigger" button', () => {
+		it('not "trigger" button', () => {
+			const $event = {
+				relatedTarget: {}
+			} as any;
+			const optionsContainerStyle: CSSStyleDeclaration = component.optionsContainer.nativeElement.style;
+			optionsContainerStyle.visibility = 'visible';
+			component.onBlurOptionsContainer($event);
+			expect(optionsContainerStyle.visibility).toEqual('hidden');
+		});
+
+		it('"trigger" button', () => {
+			const $event: FocusEvent = {
+				relatedTarget: component.optionsTrigger.nativeElement
+			} as any;
+			const optionsContainerStyle: CSSStyleDeclaration = component.optionsContainer.nativeElement.style;
+			optionsContainerStyle.visibility = 'visible';
+			component.onBlurOptionsContainer($event);
+			expect(optionsContainerStyle.visibility).toEqual('visible');
+		});
+	});
+
+	it('selectOption should get index, set the index on selectedIndex and change "visibility" of optionsContainer to "hidden".', () => {
+		component.selectOption(20);
+		expect(component.selectedIndex).toEqual(20);
+		expect(component.optionsContainer.nativeElement.style.visibility).toEqual('hidden');
+	});
+
 });

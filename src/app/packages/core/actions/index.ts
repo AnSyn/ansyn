242d0cf2ddbd0a8198426083b46fe2ec65217ff0
YairTawil <yairtawil@gmail.com>
2017-07-11 10:19:12 +0300

    general exports inside core index

---
+++ src/app/packages/core/actions/index.ts
@@ -0,0 +1,2 @@
+export { MenuActionTypes, AddMenuItemAction, SelectMenuItemAction, UnSelectMenuItemAction, AnimationStartAction, AnimationEndAction } from './menu.actions';
+export { EmptyAction, EMPTY_ACTION } from './empty.action';
--- src/app/packages/core/models/index.ts
+++ src/app/packages/core/models/index.ts
@@ -2,5 +2,4 @@ export { Overlay, OverlaysCriteria } from './overlay.model';
 export { Position } from './position.model';
 export { MenuItem } from './menu-item.model';
 export { IGeneralConfig } from './general.config';
-export { Case } from './case.model';
 export { Case, CaseState, CaseTimeState, CaseFacetsState, CaseMapsState, CaseMapState, defaultMapType } from './case.model';

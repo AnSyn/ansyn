b040fcb54f84219982d44f550827fa51067115a7
YairTawil <yairtawil@gmail.com>
2017-08-08 10:24:53 +0300

    Add app-loader files

---
+++ src/assets/app-loader/app-loader.js
@@ -0,0 +1,59 @@
+document.querySelector('app-root')
+	.innerHTML = `<div class="skeleton-wrapper">
+					<div class="skeleton-menu"></div>
+					<div class="skeleton-container">
+						<div class="skeleton-map">
+							<div class="skeleton-loader"></div>
+							<div class="skeleton-text">
+								<div class="wrapper">
+				
+									<div class="time-part-wrapper">
+				
+										<div class="time-part hundredths hun">
+											<div class="digit-wrapper">
+												<span class="digit">1</span>
+											</div>
+										</div>
+										<div class="time-part hundredths tens">
+											<div class="digit-wrapper">
+												<span class="digit">0</span>
+												<span class="digit">1</span>
+												<span class="digit">2</span>
+												<span class="digit">3</span>
+												<span class="digit">4</span>
+												<span class="digit">5</span>
+												<span class="digit">6</span>
+												<span class="digit">7</span>
+												<span class="digit">8</span>
+												<span class="digit">9</span>
+												<span class="digit">0</span>
+											</div>
+										</div>
+										<div class="time-part hundredths ones">
+											<div class="digit-wrapper">
+												<span class="digit">0</span>
+												<span class="digit">1</span>
+												<span class="digit">2</span>
+												<span class="digit">3</span>
+												<span class="digit">4</span>
+												<span class="digit">5</span>
+												<span class="digit">6</span>
+												<span class="digit">7</span>
+												<span class="digit">8</span>
+												<span class="digit">9</span>
+												<span class="digit">0</span>
+											</div>
+										</div>
+				
+										<div class="time-part static">
+											<span class="digit">%</span>
+										</div>
+				
+									</div>
+								</div>
+							</div>
+				
+						</div>
+						<div class="skeleton-timeline"></div>
+					</div>
+				</div>`;

1c243e250cd44d6b861e1d2654f3c9d5d79e69f1
YairTawil <yairtawil@gmail.com>
2017-08-16 10:43:00 +0300

    Fix menu-items Circular dependency warning

--- src/app/packages/menu-items/index.ts
+++ src/app/packages/menu-items/index.ts
@@ -1,9 +1,9 @@
-export { FiltersModule } from '@ansyn/menu-items/filters';
-export { CasesModule } from '@ansyn/menu-items/cases';
-export { LayersManagerModule } from '@ansyn/menu-items/layers-manager';
-export { AlgorithmsModule } from '@ansyn/menu-items/algorithms';
-export { SettingsModule } from '@ansyn/menu-items/settings';
-export { ToolsModule } from '@ansyn/menu-items/tools';
-export { ImagerySandBoxModule } from '@ansyn/menu-items/imagerySandBox';
-export { MenuItemsModule } from './menu-items.module';
-export { MenuItemsConfig } from './models/menu-items-config';
+export * from './filters';
+export * from './cases';
+export * from './layers-manager';
+export * from './algorithms';
+export * from './settings';
+export * from './tools';
+export * from './imagerySandBox';
+export * from './menu-items.module';
+export * from './models';

80c7ab787331e271c37ec342e6f0c0d3729a9708
Ohad Sadan <sadanoh@gmail.com>
2017-08-24 15:49:24 +0300

    add logo

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { configuration } from './configuration/configuration';
 
-import { TmpConfig } from './app/app.module';
-
 if (configuration.production) {
   enableProdMode();
 }
 
-fetch("/assets/config/config.json").then(response => {
-	return response.json();
-}).then(config => {
-	platformBrowserDynamic([
-		{ provide:TmpConfig, useValue:config }
-	]).bootstrapModule(AppModule);
-});
+platformBrowserDynamic().bootstrapModule(AppModule);
+
 
 

233c96e7862fb3efa4d05659bc4a78ec1ec74364
asafMasa <asaf.masa@gmail.com>
2017-08-24 15:38:20 +0300

    update context entity color and time format

--- src/app/packages/core/utils/time.ts
+++ src/app/packages/core/utils/time.ts
@@ -4,8 +4,16 @@ export class TimeDiff {
 	seconds: number;
 	minutes: number;
 	hours: number;
+	days: number;
+	months: number;
+	years: number;
 
-	constructor(hours: number, minutes: number, seconds: number, isNegative: boolean = false) {
+	constructor(years: number, months: number, days: number,
+				hours: number, minutes: number, seconds: number,
+				isNegative: boolean = false) {
+		this.years = years;
+		this.months = months;
+		this.days = days;
 		this.hours = hours;
 		this.minutes = minutes;
 		this.seconds = seconds;
@@ -20,17 +28,32 @@ export function getTimeDiff(date1: Date, date2: Date): TimeDiff {
 		isNegative = true;
 		millisecondsDiff *= -1;
 	}
-
+	const years = Math.floor(millisecondsDiff / 1000 / 60 / 60 / 24 / 30 / 12);
+	millisecondsDiff -= years * 1000 * 60 * 60 * 24 * 30 * 12;
+	const months = Math.floor(millisecondsDiff / 1000 / 60 / 60 / 24 / 30);
+	millisecondsDiff -= months * 1000 * 60 * 60 * 24 * 30;
+	const days = Math.floor(millisecondsDiff / 1000 / 60 / 60 / 24);
+	millisecondsDiff -= days * 1000 * 60 * 60 * 24;
 	const hours = Math.floor(millisecondsDiff / 1000 / 60 / 60);
 	millisecondsDiff -= hours * 1000 * 60 * 60;
 	const minutes = Math.floor(millisecondsDiff / 1000 / 60);
 	millisecondsDiff -= minutes * 1000 * 60;
 	const seconds = Math.floor(millisecondsDiff / 1000);
 	millisecondsDiff -= seconds * 1000;
-    const result = new TimeDiff(hours, minutes, seconds, isNegative);
+    const result = new TimeDiff(years, months, days, hours, minutes, seconds, isNegative);
 	return result;
 }
 
 export function getTimeDiffFormat(timeDiff: TimeDiff): string {
-	return `${timeDiff.isNegative ? '-' : ''} ${timeDiff.hours} : ${timeDiff.minutes} : ${timeDiff.seconds}`;
+	let result = `${timeDiff.isNegative ? '-' : ''} `;
+	if (timeDiff.years > 0) {
+		result += `${timeDiff.years} yr: ${timeDiff.months} mo: ${timeDiff.days} days`;
+	} else if (timeDiff.months > 0) {
+		result += `${timeDiff.months} mo: ${timeDiff.days} days: ${timeDiff.hours} hr`;
+	} else if (timeDiff.days > 0) {
+		result += `${timeDiff.days} days: ${timeDiff.hours} hr: ${timeDiff.minutes} min`;
+	} else {
+		result += `${timeDiff.hours} hr: ${timeDiff.minutes} min: ${timeDiff.seconds} sec`;
+	}
+	return result;
 }

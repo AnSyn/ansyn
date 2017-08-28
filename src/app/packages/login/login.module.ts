3b6e3055f1ab76eefe52a37a73dcc586daa45fce
Ohad Sadan <sadanoh@gmail.com>
2017-08-28 11:09:17 +0300

    fix the compiling bug

--- src/app/packages/login/login.module.ts
+++ src/app/packages/login/login.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent} from './login/login.component';
 import { AuthGuard } from './guard/auth.guard';
-import { Route, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 
 @NgModule({
-	imports: [ Router],
 	providers: [AuthGuard,AuthService],
 	declarations: [LoginComponent],
 	exports: [LoginComponent]

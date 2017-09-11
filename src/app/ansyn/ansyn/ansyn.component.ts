d70299f8d292915d82d94245b91ba3908251a155
YairTawil <yairtawil@gmail.com>
2017-09-11 11:00:28 +0300

    remove logs and toggleEditMode

--- src/app/ansyn/ansyn/ansyn.component.ts
+++ src/app/ansyn/ansyn/ansyn.component.ts
@@ -4,8 +4,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Case,CaseMapsState } from '@ansyn/menu-items/cases';
 import { isEqual as _isEqual, isNil as _isNil} from 'lodash';
-import { ActiveMapChangedAction } from '@ansyn/map-facade';
-import { UpdateMapSizeAction } from '@ansyn/map-facade/actions/map.actions';
 import "@ansyn/core/utils/clone-deep";
 import * as packageJson from '../../../../package.json';
 import { LoadContextsAction } from '@ansyn/menu-items/cases/actions/cases.actions';
@@ -66,14 +64,8 @@ export class AnsynComponent implements OnInit{
 
 	ngOnInit(): void {
 		this.store.dispatch(new LoadContextsAction());
-		this.selected_case$.subscribe(() => {
-			console.log("selected_case$! changessssssssssssss!");
-		});
 		this.selectedCaseName$.subscribe(_selectedCaseName => this.selectedCaseName = _selectedCaseName);
-		this.maps$.subscribe(maps => {
-			console.log("maps$! changessssssssssssss!");
-			this.maps = maps
-		});
+		this.maps$.subscribe(maps => {this.maps = maps});
 		this.displayedOverlay$.subscribe((_displayedOverlay: Overlay) => { this.displayedOverlay = _displayedOverlay});
 		this.isFavoriteOverlay$.subscribe((isFavoriteOverlay: boolean) => { this.isFavoriteOverlay = isFavoriteOverlay});
 		this.pinLocation$.subscribe( _pinLocation => this.pinLocation = _pinLocation);

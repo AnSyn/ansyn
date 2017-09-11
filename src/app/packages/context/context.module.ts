4206657e2b6717a2a99fec8057003c18983dd8d7
Amit Moryossef <amitmoryossef@gmail.com>
2017-09-11 16:33:28 +0300

    refactor(context): change the config

--- src/app/packages/context/context.module.ts
+++ src/app/packages/context/context.module.ts
@@ -1,22 +1,14 @@
-import { Inject, InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './container/container.component';
 import { FormsModule } from '@angular/forms';
 import { ContextProviderService } from './providers/context-provider.service';
-import { Http, HttpModule } from '@angular/http';
-import { IContextSource, IContextSourceConfig } from './context.interface';
+import { HttpModule } from '@angular/http';
 
 // this must be here in order to use it in InjectionToken
-export interface IContextConfig {
-	contextSources: IContextSourceConfig[];
-}
-
-export interface IContextSources {
-	sources: any;
-}
+export type IContextConfig  = any;
 
 export const ContextConfig: InjectionToken<IContextConfig> = new InjectionToken('ContextConfig');
-export const ContextSources: InjectionToken<IContextSources> = new InjectionToken('ContextSources');
 
 @NgModule({
 	imports: [
@@ -25,45 +17,16 @@ export const ContextSources: InjectionToken<IContextSources> = new InjectionToke
 		HttpModule
 	],
 	exports: [ContainerComponent],
-	declarations: [ContainerComponent],
-	providers: [
-		ContextProviderService
-	]
+	declarations: [ContainerComponent]
 })
 export class ContextModule {
-	static forRoot(config: IContextConfig, sources: IContextSources): ModuleWithProviders {
+	static forRoot(config: IContextConfig): ModuleWithProviders {
 		return {
 			ngModule: ContextModule,
 			providers: [
 				ContextProviderService,
-				{provide: ContextConfig, useValue: config},
-				{provide: ContextSources, useValue: sources}
+				{ provide: ContextConfig, useValue: config }
 			]
 		};
-	}
-
-	constructor(public contextProviderService: ContextProviderService, @Inject(ContextSources)sourcesHandler, @Inject(ContextConfig)config: IContextConfig, private http: Http) {
-		this.register(config, sourcesHandler);
-
-	}
-
-	register(config, sourcesHandler) {
-		config.contextSources.forEach((itemConfig: IContextSourceConfig) => {
-			if (!itemConfig.available) {
-				return;
-			}
-			const object: IContextSource = sourcesHandler[itemConfig.type];
-			const gateway = this.getGateway(itemConfig.apiObject);
-			const instance: IContextSource = new (<any>object)(itemConfig, gateway);
-			this.contextProviderService.register(itemConfig.type, instance);
-		});
-	}
-
-	getGateway(apiObject) {
-		if (apiObject === 'Http') {
-			return this.http;
-		}
-		return undefined;
-
 	}
 }

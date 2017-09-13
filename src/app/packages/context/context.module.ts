7be9ddccd55f74a8e8a47ea3c1260518d251efa8
Amit Moryossef <amitmoryossef@gmail.com>
2017-09-13 15:59:48 +0300

    tests(context): add tests

---
+++ src/app/packages/context/context.module.ts
@@ -0,0 +1,28 @@
+import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from './container/container.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { IContextConfig } from './context.interface';
+
+export const ContextConfig: InjectionToken<IContextConfig> = new InjectionToken('ContextConfig');
+
+@NgModule({
+	imports: [
+		CommonModule,
+		FormsModule,
+		HttpModule
+	],
+	exports: [ContainerComponent],
+	declarations: [ContainerComponent]
+})
+export class ContextModule {
+	static forRoot(config: IContextConfig): ModuleWithProviders {
+		return {
+			ngModule: ContextModule,
+			providers: [
+				{ provide: ContextConfig, useValue: config }
+			]
+		};
+	}
+}

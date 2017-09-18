d760a61375dce5d649694c06345a1a7695e83d2b
Ohad Sadan <sadanoh@gmail.com>
2017-09-18 09:17:42 +0300

    fix(filters does not works from link on default case)

---
+++ src/app/app-reducers/effects/filters.app.effects.spec.ts
@@ -0,0 +1,31 @@
+// @TODO add  tests
+
+
+
+describe('Filters app effects', () => {
+
+	it('updateOverlayFilters$ effect', () => {
+
+	});
+
+	it('updateCaseFacets$ effect', () => {
+
+	});
+
+	it('initializeFilters$ effect', () => {
+
+	});
+	
+	it('resetFilters$ effect', () => {
+
+	})
+
+	it('updateCaseFacets function', () => {
+
+	})
+
+	it('isMetadataEmpty', () => {
+
+	})
+
+});

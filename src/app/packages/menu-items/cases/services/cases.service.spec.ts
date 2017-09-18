c77e665512a7afa1e0f7b63e53260020dff7e2f3
Ohad Sadan <sadanoh@gmail.com>
2017-09-18 09:27:37 +0300

    fix(linter) fix some type checking

--- src/app/packages/menu-items/cases/services/cases.service.spec.ts
+++ src/app/packages/menu-items/cases/services/cases.service.spec.ts
@@ -93,6 +93,8 @@ describe('CasesService', () => {
 		let fake_response = { selected_case };
 		spyOn(http, 'post').and.callFake(() => ({ map: (callBack) => callBack(fake_response) }));
 		casesService.createCase(selected_case);
+		let fake_response = { json: () => selected_case };
+		spyOn(http, 'post').and.callFake(() => ({ map: (callBack) => callBack(fake_response) }));
 		expect(http.post).toHaveBeenCalledWith(`${casesService.base_url}`, selected_case);
 	});
 

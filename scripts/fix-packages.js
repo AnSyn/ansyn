331eeb3afc60ea54cd0e091a06e0f03083a1e7fb
Amit Moryossef <amitmoryossef@gmail.com>
2017-09-18 15:06:40 +0300

    fix(): revert outside tsconfig, create backwards

--- scripts/fix-packages.js
+++ scripts/fix-packages.js
@@ -1,9 +1,8 @@
 const replace = require('replace');
 
 replace({
-	// Note that using + instead of * does not reinforce an edge case...
-	regex: '(\\.\\.\\/)*\\.\\.\\/packages', // any amount of (../) followed by `packages`
-	replacement: '@ansyn',
+	regex: '\'(.*?)\\/packages\\/', // any amount of (../) followed by `packages`
+	replacement: '\'@ansyn/',
 	paths: ['src/'],
 	recursive: true,
 	silent: false

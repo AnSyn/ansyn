397d5933142c40fe42d7c227fb5e8e45d128ae7d
YairTawil <yairtawil@gmail.com>
2017-09-19 10:23:56 +0300

    Change menuItems to Map Object(instead of Array)

--- src/app/packages/menu/reducers/menu.reducer.ts
+++ src/app/packages/menu/reducers/menu.reducer.ts
@@ -8,14 +8,14 @@ import {
 import { SetBadgeAction } from '../actions/menu.actions';
 
 export interface IMenuState {
-	menu_items: MenuItem[];
-	selected_menu_item_index: number;
+	menuItems: Map<string, MenuItem>;
+	selectedMenuItem: string;
 	animation: boolean;
 }
 
 export const initialMenuState: IMenuState = {
-	menu_items: [],
-	selected_menu_item_index: -1,
+	menuItems: new Map(),
+	selectedMenuItem: '',
 	animation: false
 };
 
@@ -25,35 +25,29 @@ export function MenuReducer(state: IMenuState = initialMenuState, action: MenuAc
 
 	switch (action.type) {
 		case MenuActionTypes.ADD_MENU_ITEM:
-			let menu_items: MenuItem[] = [
-				...state.menu_items,
-				action.payload,
-			];
-			return Object.assign({}, state, { menu_items });
+			const menuItems = new Map(state.menuItems);
+			menuItems.set(action.payload.name, action.payload);
+			return { ...state, menuItems };
 
 		case MenuActionTypes.SELECT_MENU_ITEM:
-			return Object.assign({}, state, { selected_menu_item_index: action.payload, animation: true });
+			return { ...state, selectedMenuItem: action.payload, animation: true };
 
 		case MenuActionTypes.UNSELECT_MENU_ITEM:
-			return Object.assign({}, state, { selected_menu_item_index: -1, animation: true });
+			return { ...state, selectedMenuItem: '', animation: true };
 
 		case MenuActionTypes.ANIMATION_START:
-			return Object.assign({}, state, { animation: true });
+			return { ...state, animation: true };
 
 		case MenuActionTypes.ANIMATION_END:
-			return Object.assign({}, state, { animation: false });
+			return { ...state, animation: false };
 
 		case MenuActionTypes.SET_BADGE: {
-			const { index, badge } = action.payload ;
-			const menuItem = state.menu_items[index];
+			const { key, badge } = action.payload ;
+			const menuItems = new Map(state.menuItems);
+			const menuItem = menuItems.get(key);
 			menuItem.badge = badge;
-			const size = state.menu_items.length;
-			const menu_items = [
-				state.menu_items.slice(0, index),
-				menuItem,
-				state.menu_items.slice(index + 1, size),
-			];
-			return {...state, menu_items};
+			menuItems.set(key, menuItem);
+			return { ...state, menuItems };
 		}
 
 		default:

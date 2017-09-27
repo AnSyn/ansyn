3db5327dc5b045f095774ee1f89cb2c50da4c95c
Ohad Sadan <sadanoh@gmail.com>
2017-09-27 17:34:47 +0300

    hotfix

--- src/app/packages/menu-items/tools/models/tools-config.ts
+++ src/app/packages/menu-items/tools/models/tools-config.ts
@@ -1,12 +1,14 @@
 import { CoordinatesSystem } from '@ansyn/core/utils/covert-projections';
 import { InjectionToken } from '@angular/core';
 
-export const toolsConfig: InjectionToken<IToolsConfig> = new InjectionToken('toolsConfig');
-
 export interface IToolsConfig {
 	GoTo: {
 		from: CoordinatesSystem;
 		to: CoordinatesSystem;
 	}
 }
+
+export const toolsConfig: InjectionToken<IToolsConfig> = new InjectionToken('toolsConfig');
+
+
 

ae756e41af8fe47dfe79b56205b4c0ed8300bf64
YairTawil <yairtawil@gmail.com>
2017-10-04 10:07:31 +0300

    lint(): master lint

--- src/app/packages/menu-items/tools/tools/tools.component.ts
+++ src/app/packages/menu-items/tools/tools/tools.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
 	AnnotationVisualizerAgentAction,
 	GoToExpandAction,

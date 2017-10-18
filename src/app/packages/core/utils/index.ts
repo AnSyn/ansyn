ad3d1d31be7fcb5b9f889afb528cefef23b3d0a5
Amit Moryossef <amitmoryossef@gmail.com>
2017-10-18 09:18:54 +0300

    test(map-app-effects): remove irrelevant tests

--- src/app/packages/core/utils/index.ts
+++ src/app/packages/core/utils/index.ts
@@ -1,7 +1,7 @@
 export { type } from './type';
 export { copyFromContent } from './clipboard';
 export { Spinner } from './spinner';
-export { calcGeoJSONExtent, isExtentContainedInPolygon } from './calc-extent';
+export * from './calc-extent';
 export * from './geo';
 export * from './store-helpers';
 export * from './covert-projections';

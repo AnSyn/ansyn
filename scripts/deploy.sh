c473d878e7a18defa84c610c0fc2e2b15020780f
Amit Moryossef <amitmoryossef@gmail.com>
2017-11-01 10:58:23 +0200

    feat(travis): add deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+target=$1
+echo "target $target"
+if [ -z "$target" ]
+then
+	echo "Missing deployment target"
+	exit 1
+fi
+
+version=$2
+echo "version $version"
+if [ -z "$target" ]
+then
+	echo "Missing deployment target"
+	exit 1
+fi
+
+echo "start deploying version $version on target $target"
+
+eval $(aws ecr get-login --no-include-email --region us-west-2)
+
+docker build -t "$target:$version" .
+
+docker tag "$target:$version" "$target:latest"
+
+docker tag "$target:latest" 223455578796.dkr.ecr.us-west-2.amazonaws.com/ansyn/client:latest
+
+docker tag "$target:latest" "223455578796.dkr.ecr.us-west-2.amazonaws.com/$target:$version"
+
+docker push "223455578796.dkr.ecr.us-west-2.amazonaws.com/$target:latest"
+
+docker push "223455578796.dkr.ecr.us-west-2.amazonaws.com/$target:$version"
+
+echo "deployment succeeded";

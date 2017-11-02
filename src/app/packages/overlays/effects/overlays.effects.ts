f4a524c15341fd1b92195c26b88fef63d1639854
YairTawil <yairtawil@gmail.com>
2017-11-02 10:14:12 +0200

    fix(rxjs): add imports for operators

--- src/app/packages/overlays/effects/overlays.effects.ts
+++ src/app/packages/overlays/effects/overlays.effects.ts
@@ -1,6 +1,8 @@
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/pluck';
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
 import { Observable } from 'rxjs/Observable';

83c7ed8f54f56a5553cd62e39f4accc6319c4546
YairTawil <yairtawil@gmail.com>
2017-11-06 08:50:38 +0200

    fix(packages): remove unnecessary type-container

--- src/app/packages/type-container/index.ts
+++
@@ -1,3 +0,0 @@
-export { TypeContainerService, ITypeContainer } from './services/type-container.service';
-export { TypeContainerModule } from './type-container.module';
-export { RegisterOptions } from './models/register-options.model';

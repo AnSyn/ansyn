15efb30ef887015576a97d7e107486508702f6b3
asafMasa <asaf.masa@gmail.com>
2017-11-08 14:35:04 +0200

    fix raster setOperation Nullreference

--- src/app/app/app-providers/overlay-source-providers/idaho-source-provider.ts
+++ src/app/app/app-providers/overlay-source-providers/idaho-source-provider.ts
@@ -101,7 +101,7 @@ export class IdahoSourceProvider extends BaseOverlaySourceProvider {
 		overlay.azimuth = 0;
 		overlay.sourceType = IdahoOverlaySourceType;
 		overlay.isFullOverlay = true;
-		overlay.isGeoRegistered = true;
+		overlay.isGeoRegistered = false;
 
 		return overlay;
 	}

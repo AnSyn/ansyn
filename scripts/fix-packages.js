7f7aae94079ecdd50d7d8f0becfa40ba04566567
YairTawil <yairtawil@gmail.com>
2017-11-09 14:12:29 +0200

    fix(fix-packages): fix ansyn/app packages too

--- scripts/fix-packages.js
+++ scripts/fix-packages.js
@@ -1,7 +1,7 @@
 const replace = require('replace');
 
 replace({
-	regex: '\'(.*?)\\/packages\\/', // any amount of (../) followed by `packages`
+	regex: '(\'(.*?)\\/packages\\/|\'(.*?)\\/app\\/)', // any amount of (../) followed by `packages`
 	replacement: '\'@ansyn/',
 	paths: ['src/'],
 	recursive: true,

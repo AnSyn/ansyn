9c3410e66c4a834cf670ca52f62cf7ee328d6ed2
YairTawil <yairtawil@gmail.com>
2017-11-09 14:13:00 +0200

    fix(packages)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppAnsynModule } from '@ansyn/app';
-import { fetchConfigProviders } from './app/app/app-providers';
+import { fetchConfigProviders } from '@ansyn/app/app-providers';
 
 fetchConfigProviders.then(providers => platformBrowserDynamic(providers).bootstrapModule(AppAnsynModule));

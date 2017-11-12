c2e6fa298ca52eb66ea85635348a4a591ded2a84
Amit Moryossef <amitmoryossef@gmail.com>
2017-11-12 16:36:38 +0200

    feat(multiple-source-provider): remove unnecessary code

--- src/app/app/app-providers/overlay-source-providers/idaho-source-provider2.ts
+++ src/app/app/app-providers/overlay-source-providers/idaho-source-provider2.ts
@@ -1,8 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IdahoOverlaysSourceConfig, IdahoSourceProvider, IIdahoOverlaySourceConfig } from './idaho-source-provider';
-import { IFetchParams } from '@ansyn/overlays/models/base-overlay-source-provider.model';
-import { Overlay } from '@ansyn/core/models/overlay.model';
 
 export const IdahoOverlaySourceType2 = 'IDAHO2';
 
@@ -12,9 +10,5 @@ export class IdahoSourceProvider2 extends IdahoSourceProvider {
 
 	constructor(http: HttpClient, @Inject(IdahoOverlaysSourceConfig) _overlaySourceConfig: IIdahoOverlaySourceConfig) {
 		super(http, _overlaySourceConfig);
-	}
-
-	public fetch(fetchParams: IFetchParams) {
-		return super.fetch(fetchParams);
 	}
 }

a3dcc5053bb76ece8da5aab86b94f4e191b49da2
Amit Moryossef <amitmoryossef@gmail.com>
2017-11-22 09:22:21 +0200

    fix(test): toast tests work

--- src/app/packages/core/components/toast/toast.component.spec.ts
+++ src/app/packages/core/components/toast/toast.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToastComponent } from './toast.component';
+import { CoreModule } from '../../core.module';
+import { StoreModule } from '@ngrx/store';
 
 describe('ToastComponent', () => {
 	let component: ToastComponent;
@@ -8,9 +10,11 @@ describe('ToastComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ToastComponent]
-		})
-			.compileComponents();
+			imports: [
+				StoreModule.forRoot({}),
+				CoreModule
+			]
+		}).compileComponents();
 	}));
 
 	beforeEach(() => {
@@ -22,38 +26,4 @@ describe('ToastComponent', () => {
 	it('should be created', () => {
 		expect(component).toBeTruthy();
 	});
-	describe('set showToast', () => {
-		beforeEach(() => {
-			spyOn(window, 'setTimeout');
-			spyOn(window, 'clearTimeout');
-			spyOn(component.showToastChange, 'emit');
-		});
-
-		it('should: call showToastChange.emit with new value', () => {
-			component.showToast = true;
-			expect(component.showToastChange.emit).toHaveBeenCalledWith(true);
-		});
-		it('should: set Timeout if value is "true"', () => {
-			component.showToast = false;
-			expect(window.setTimeout).not.toHaveBeenCalled();
-			component.showToast = true;
-			expect(window.setTimeout).toHaveBeenCalled();
-		});
-
-		it('should: clear Timeout if value is "false"', () => {
-			component.showToast = true;
-			expect(window.clearTimeout).not.toHaveBeenCalled();
-			component.showToast = false;
-			expect(window.clearTimeout).toHaveBeenCalledWith(component.timeoutRef);
-		});
-
-
-		it('should: change _showToast value ', () => {
-			component.showToast = true;
-			expect((<any>component)._showToast).toBeTruthy();
-			component.showToast = false;
-			expect((<any>component)._showToast).toBeFalsy();
-		});
-	});
-
 });

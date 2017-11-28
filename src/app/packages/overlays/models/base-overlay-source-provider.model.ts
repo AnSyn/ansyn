57735c54c8d813c5b29d290723fad69d8e1cd6b5
Amit Moryossef <amitmoryossef@gmail.com>
2017-11-28 08:45:39 +0200

    patch(types): fix failing travis build

--- src/app/packages/overlays/models/base-overlay-source-provider.model.ts
+++ src/app/packages/overlays/models/base-overlay-source-provider.model.ts
@@ -10,7 +10,7 @@ export interface DateRange {
 }
 
 export interface IFetchParams {
-	region: any; // GeoJSON.DirectGeometryObject;
+	region: GeoJSON.DirectGeometryObject;
 	sensors?: string[];
 	timeRange: DateRange;
 }

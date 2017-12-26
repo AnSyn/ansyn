2e8c3cd13978ad112ac9ef11f620ba29d4c988cc
YairTawil <yairtawil@gmail.com>
2017-12-26 14:26:57 +0200

    feat(scripts): fix-packages/fix-package-json add to git by flag

--- scripts/fix-packages.js
+++ scripts/fix-packages.js
@@ -2,12 +2,15 @@ const replace = require('replace');
 const packages = require('./packages-list.json');
 const findInFiles = require('find-in-files');
 const gitAdd = require('./git-add');
+const addToGit = process.argv[2] === '--add' || process.argv[2] === '-a';
 
 function replaceAdd(options) {
 	Promise.all(options.paths.map(path => findInFiles.find(options.regex, path, '.ts$'))).then(results => {
 		const dirty = results.map(res => Object.keys(res)).reduce((a, b) => a.concat(b), []);
 		replace(options);
-		dirty.forEach(gitAdd);
+		if(addToGit) {
+			dirty.forEach(gitAdd);
+		}
 	});
 }
 

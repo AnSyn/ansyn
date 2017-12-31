dfc27de5a6b1efedd5c701e4e6ad384b0ad10960
yuvalbl <yuval.bar.levi@gmail.com>
2017-12-31 13:10:59 +0200

    code review changes

--- src/app/packages/imagery/index.ts
+++ src/app/packages/imagery/index.ts
@@ -1,6 +1,7 @@
 export * from './model';
 export { CommunicatorEntity } from './communicator-service/communicator.entity';
 export { ImageryProviderService } from './provider-service/provider.service';
+export { ImageryComponentManager } from './imagery-component/manager/imagery.component.manager';
 export { ImageryModule } from './imagery.module';
 export { ImageryCommunicatorService } from './communicator-service/communicator.service';
 export { ConfigurationToken } from './configuration.token';

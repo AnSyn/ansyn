a6e9e5b29b344405358d70f0e43ac22ea4c69633
shaharido1 <shaharido1@gmail.com>
2018-01-15 11:32:36 +0200

    task(1018)configurable-indications: tooltip of combo box

---
+++ src/app/packages/status-bar/models/statusBar-config.model.ts
@@ -0,0 +1,18 @@
+export interface IToolTipsConfig {
+	orientation?: string,
+	geoFilter?: string,
+	geoFilterEdit?: string,
+	geoFilterShow?: string,
+	timeFilter ?: string,
+	timeFilterEdit?: string,
+	screenNumber?: string,
+	imageCount?: string,
+	backwards?: string,
+	forward?: string,
+	reset?: string,
+	enlarge?: string
+}
+
+export interface IStatusBarConfig {
+	toolTip: IToolTipsConfig
+}

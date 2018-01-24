14e60aeaeb76eb42fb78050533bb4a6a5333865d
shaharido1 <shaharido1@gmail.com>
2018-01-24 13:27:59 +0200

    fix(tools): Manual image enhancement - Reset all #1019

--- src/app/packages/menu-items/tools/models/tools-config.ts
+++ src/app/packages/menu-items/tools/models/tools-config.ts
@@ -9,7 +9,15 @@ export interface IToolsConfig {
 	Proj4: {
 		ed50: string;
 		ed50Customized: string;
-	}
+	},
+	ImageProcParams : Array<IImageProcParam>
+}
+
+export interface IImageProcParam {
+	name: string,
+	defaultValue: number,
+	min: number,
+	max: number
 }
 
 export const toolsConfig: InjectionToken<IToolsConfig> = new InjectionToken('toolsConfig');

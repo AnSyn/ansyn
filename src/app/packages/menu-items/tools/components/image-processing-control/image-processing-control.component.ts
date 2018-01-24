c39947e9a76873234ee1f85441b322db81ab9b70
shaharido1 <shaharido1@gmail.com>
2018-01-24 13:27:59 +0200

    fix(tools): Manual image enhancement - Reset all #1019

--- src/app/packages/menu-items/tools/components/image-processing-control/image-processing-control.component.ts
+++ src/app/packages/menu-items/tools/components/image-processing-control/image-processing-control.component.ts
@@ -1,8 +1,17 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, HostBinding, Input, Inject } from '@angular/core';
 import { IToolsState } from '../../reducers/tools.reducer';
 import { Store } from '@ngrx/store';
 import { SetManualImageProcessing } from '../../actions/tools.actions';
 import { isEqual, throttle } from 'lodash';
+import { IToolsConfig, toolsConfig } from '@ansyn/menu-items/tools/models';
+import { IImageProcParam } from '@ansyn/menu-items/tools/models/tools-config';
+
+
+interface IImageProcParamComp extends  IImageProcParam {
+	show: boolean,
+	value: number,
+}
+
 
 @Component({
 	selector: 'ansyn-image-processing-control',
@@ -11,14 +20,6 @@ import { isEqual, throttle } from 'lodash';
 })
 export class ImageProcessingControlComponent {
 	private _isExpended: boolean;
-	private _imgProcessParamDefaults = {
-		Contrast: 0,
-		Brightness: 0,
-		Gamma: 100,
-		Saturation: 100,
-		Sharpness: 0
-	};
-	public imgProcessParams: any;	// instead of Object, for Travis testing
 	public imgProcessActive: boolean;	// instead of Object, for Travis testing
 	public throttledManualImageProcess: Function;	// throttled function
 
@@ -31,30 +32,53 @@ export class ImageProcessingControlComponent {
 		return this._isExpended;
 	}
 
-	constructor(public store$: Store<IToolsState>) {
+	params : Array<IImageProcParamComp> = [];
+
+	constructor(public store$: Store<IToolsState>, @Inject(toolsConfig) protected config: IToolsConfig) {
 		this.resetAllParams();
+		this.params = config.ImageProcParams.map(param => {
+			return Object.assign({}, param, {show : false}, {value: 0})
+		})
 		// limit to once every 200 ms
 		this.throttledManualImageProcess = throttle(this.manualImageProcess, 200);
 	}
 
+
 	resetAllParams() {
-		this.imgProcessParams = Object.assign({}, this._imgProcessParamDefaults);
+		this.params.forEach(param => {
+			param.value = param.defaultValue;
+		});
 		this.imgProcessActive = false;
 	}
 
 	manualImageProcess() {
-		this.imgProcessActive = !isEqual(this.imgProcessParams, this._imgProcessParamDefaults);
-		let dispatchValue = (this.imgProcessActive) ? this.imgProcessParams : undefined;
+		this.imgProcessActive = !isEqual(
+			this.params.map(param => {
+				return param.value;
+			}),
+			this.params.map(param => {
+				return param.defaultValue;
+			})
+		);
+		let dispatchValue = {};
+		if (this.imgProcessActive) {
+			this.params.forEach(param => {
+				dispatchValue[param.name] = param.value
+			})
+		}
+		else {
+			dispatchValue = undefined;
+		}
 		this.store$.dispatch(new SetManualImageProcessing({ processingParams: dispatchValue }));
 	}
 
-	backToDefault(processParam) {
-		this.imgProcessParams[processParam] = this._imgProcessParamDefaults[processParam];
+	resetOne(param) {
+		param.value = param.defaultValue;
 		this.throttledManualImageProcess();
 	}
 
 	resetAll() {
-		this.resetAllParams()
+		this.resetAllParams();
 		this.throttledManualImageProcess();
 
 	}

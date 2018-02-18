35d20ad66a1cc9393dd32e158ac7c7559137a4ab
YairTawil <yairtawil@gmail.com>
2018-02-18 14:16:34 +0200

    fix(): CR request changes

--- src/app/packages/map-facade/components/imagery-tile-progress/imagery-tile-progress.component.ts
+++ src/app/packages/map-facade/components/imagery-tile-progress/imagery-tile-progress.component.ts
@@ -1,37 +1,27 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IMapState, MapsProgress, mapStateSelector } from '@ansyn/map-facade/reducers/map.reducer';
 import { Store } from '@ngrx/store';
-import { SetProgressBarAction } from '@ansyn/map-facade';
 
 @Component({
 	selector: 'ansyn-imagery-tile-progress',
 	templateUrl: './imagery-tile-progress.component.html',
 	styleUrls: ['./imagery-tile-progress.component.less']
 })
-export class ImageryTileProgressComponent implements OnInit, OnDestroy {
+export class ImageryTileProgressComponent implements OnInit {
 	@Input() mapId;
 	@Input() lowered;
 
-	mapsProgress$ = this.store$.select(mapStateSelector)
+	progress$ = this.store$.select(mapStateSelector)
 		.pluck<IMapState, MapsProgress>('mapsProgress')
-		.distinctUntilChanged();
+		.distinctUntilChanged()
+		.map((mapsProgress: MapsProgress): number => mapsProgress[this.mapId]);
 
-	mapsProgress;
 	progress;
 
 	constructor(public store$: Store<IMapState>) {
 	}
 
 	ngOnInit() {
-		this.mapsProgress$.subscribe((mapsProgress: MapsProgress) => {
-			this.mapsProgress = mapsProgress;
-			this.progress = mapsProgress[this.mapId]
-		});
-	}
-
-	ngOnDestroy() {
-		const updatedMapProgress = { ...this.mapsProgress };
-		delete updatedMapProgress[this.mapId];
-		this.store$.dispatch(new SetProgressBarAction(updatedMapProgress));
+		this.progress$.subscribe((progress) => this.progress = progress);
 	}
 }

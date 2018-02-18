f4f36eb3895f2e8b173966f1ffde03574d697ede
YairTawil <yairtawil@gmail.com>
2018-02-18 12:06:00 +0200

    test(map-facade): unit test for imagery loader

--- src/app/packages/map-facade/components/imagery-tile-progress/imagery-tile-progress.component.spec.ts
+++ src/app/packages/map-facade/components/imagery-tile-progress/imagery-tile-progress.component.spec.ts
@@ -1,11 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ImageryTileProgressComponent } from './imagery-tile-progress.component';
 import { StoreModule } from '@ngrx/store';
-import { mapFeatureKey, MapReducer } from '@ansyn/map-facade';
+import { mapFeatureKey, MapReducer } from '../../reducers/map.reducer';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('ImageryTileProgressComponent', () => {
 	let component: ImageryTileProgressComponent;
 	let fixture: ComponentFixture<ImageryTileProgressComponent>;
+	let mapLoader: DebugElement;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -19,9 +22,24 @@ describe('ImageryTileProgressComponent', () => {
 		fixture = TestBed.createComponent(ImageryTileProgressComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
+		mapLoader = fixture.debugElement.query(By.css('.map-loader'));
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('progress percent should effect loader width', () => {
+		component.progress = 70;
+		fixture.detectChanges();
+		expect(mapLoader.styles.width).toEqual('70%');
+	});
+
+	it('progress percent on 100 should hide the loader', () => {
+		component.progress = 100;
+		fixture.detectChanges();
+		expect(mapLoader.properties.hidden).toBeTruthy();
+	});
+
+
 });

1e87910360344ccd5c677447159ab8dc54de6065
unknown <dandik.o12@gmail.com>
2018-02-28 17:35:12 +0200

    feat (new provider): added new provider files, converting to open aerial

---
+++ src/app/app/app-providers/map-source-providers/open-layers-open-aerial-source.provider.ts
@@ -0,0 +1,45 @@
+import { BaseMapSourceProvider } from '@ansyn/imagery';
+import XYZ from 'ol/source/xyz';
+import ImageLayer from 'ol/layer/image';
+import { ProjectableRaster } from '@ansyn/open-layers-map';
+import { Injectable } from '@angular/core';
+import { Overlay } from '@ansyn/core';
+import { extentFromGeojson } from '@ansyn/core/utils';
+import proj from 'ol/proj';
+
+export const OpenLayerOpenAerialSourceProviderMapType = 'openLayersMap';
+export const OpenLayerOpenAerialSourceProviderSourceType = 'OPEN_AERIAL';
+
+@Injectable()
+export class OpenLayersOpenAerialSourceProvider extends BaseMapSourceProvider {
+
+	public mapType = OpenLayerOpenAerialSourceProviderMapType;
+	public sourceType = OpenLayerOpenAerialSourceProviderSourceType;
+
+	create(metaData: Overlay, mapId: string): any {
+		const source = new XYZ({
+			url: metaData.imageUrl,
+			crossOrigin: 'Anonymous',
+			projection: 'EPSG:3857' // Check correct EPSG?
+		});
+
+		this.monitorSource(source, mapId);
+		let [x, y, x1, y1] = extentFromGeojson(metaData.footprint);
+		[x, y] = proj.transform([x, y], 'EPSG:4326', 'EPSG:3857'); // Check correct EPSG?do i even need a transformation?
+		[x1, y1] = proj.transform([x1, y1], 'EPSG:4326', 'EPSG:3857'); // Check correct EPSG?do i even need a transformation?
+
+		return new ImageLayer({
+			source: new ProjectableRaster({
+				sources: [source],
+				operation: (pixels) => pixels[0],
+				operationType: 'image'
+			}),
+			extent: [x, y, x1, y1]
+		});
+	}
+
+	createAsync(metaData: any, mapId: string): Promise<any> {
+		let layer = this.create(metaData, mapId);
+		return Promise.resolve(layer);
+	}
+}

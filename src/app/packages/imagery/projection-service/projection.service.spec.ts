ae7ccc949ea864faaa1ae03ae724c37d85324abd
Lior Zeno <liorzino@gmail.com>
2018-03-07 15:39:12 +0200

    Remove ProjectionService tests

--- src/app/packages/imagery/projection-service/projection.service.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ProjectionService } from 'app/packages/imagery/projection-service/projection.service';
-
-describe('ProjectionService', () => {
-	let projectionService: ProjectionService;
-
-	it('should be created', () => {
-		expect(projectionService).toBeTruthy();
-	});
-});

6679e8568eb7ef2b6cf5bc517b4474a2b9263f0a
Lior Zeno <liorzino@gmail.com>
2018-03-07 14:48:17 +0200

    Add an abstract ProjectionService API

---
+++ src/app/packages/imagery/projection-service/projection.service.spec.ts
@@ -0,0 +1,9 @@
+import { ProjectionService } from 'app/packages/imagery/projection-service/projection.service';
+
+describe('ProjectionService', () => {
+	let projectionService: ProjectionService;
+
+	it('should be created', () => {
+		expect(projectionService).toBeTruthy();
+	});
+});

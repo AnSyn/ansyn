6f5b861f0f7e2ab6b783951ad96a5123c7830581
Lior Zeno <liorzino@gmail.com>
2018-03-07 16:59:15 +0200

    openlayers-map: fix a typo in import

--- src/app/packages/open-layers-map/openlayers-map/openlayers-map.ts
+++ src/app/packages/open-layers-map/openlayers-map/openlayers-map.ts
@@ -17,7 +17,7 @@ import TileLayer from 'ol/layer/tile';
 import ImageLayer from 'ol/layer/image';
 import VectorLayer from 'ol/layer/vector';
 import MousePosition from 'ol/control/mouseposition'
-import Coordinate from 'ol/Coordinate';
+import Coordinate from 'ol/coordinate';
 
 import * as GeoJSON from 'geojson';
 

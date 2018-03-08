9b9ba0e72fe9d0095b3a8b33672f6d6be790f80b
unknown <dandik.o12@gmail.com>
2018-03-08 10:51:06 +0200

    feat(new provider): added open aerial overlay provider

--- src/app/@ansyn/ansyn/app-providers/map-source-providers/index.ts
+++ src/app/@ansyn/ansyn/app-providers/map-source-providers/index.ts
@@ -5,5 +5,6 @@ export * from './open-layers-MapBox-source-provider';
 export * from './open-layers-OSM-source-provider';
 export * from './open-layers-TileWMS-source-provider';
 export * from './open-layers-ESRI-4326-source-provider';
+export * from './open-layers-open-aerial-source-provider';
 
 

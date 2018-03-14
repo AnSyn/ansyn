d99794713edf8665d7ddcfc3703e38f70ef6359d
Lior Zeno <liorzino@gmail.com>
2018-03-14 16:41:12 +0200

    mouse-shadow.visualizer: override addOrUpdateEntities() with an approximated projection method

--- src/app/@ansyn/plugins/openlayers/open-layer-visualizers/mouse-shadow.visualizer.ts
+++ src/app/@ansyn/plugins/openlayers/open-layer-visualizers/mouse-shadow.visualizer.ts
@@ -2,6 +2,8 @@ import { EntitiesVisualizer } from './entities-visualizer';
 import Feature from 'ol/feature';
 import Icon from 'ol/style/icon';
 import Style from 'ol/style/style';
+import { Observable } from 'rxjs/Observable';
+import { IVisualizerEntity } from '@ansyn/imagery';
 
 export const MouseShadowVisualizerType = 'MouseShadowVisualizer';
 
@@ -19,6 +21,37 @@ export class MouseShadowVisualizer extends EntitiesVisualizer {
 			}),
 			zIndex: 200
 		});
+	}
+
+	addOrUpdateEntities(logicalEntities: IVisualizerEntity[]): Observable<boolean> {
+		if (logicalEntities.length <= 0) {
+			return Observable.of(true);
+		}
+
+		const logicalEntitiesCopy = [...logicalEntities];
+
+		const featuresCollectionToAdd: GeoJSON.FeatureCollection<any> = {
+			type: 'FeatureCollection',
+			features: []
+		};
+
+		logicalEntitiesCopy.forEach((entity: IVisualizerEntity) => {
+			this.removeEntity(entity.id);
+			const clonedFeatureJson: any = { ...entity.featureJson, id: entity.id };
+			featuresCollectionToAdd.features.push(clonedFeatureJson);
+			this.idToEntity.set(entity.id, { originalEntity: entity, feature: null });
+		});
+
+		return this.iMap.projectionService.projectCollectionApproximatelyToImage<Feature>(featuresCollectionToAdd, this.iMap)
+			.map((features: Feature[]) => {
+				features.forEach((feature: Feature) => {
+					const id: string = <string>feature.getId();
+					const existingEntity = this.idToEntity.get(id);
+					this.idToEntity.set(id, { ...existingEntity, feature: feature });
+				});
+				this.source.addFeatures(features);
+				return true;
+			});
 	}
 
 	featureStyle(feature: Feature, resolution) {

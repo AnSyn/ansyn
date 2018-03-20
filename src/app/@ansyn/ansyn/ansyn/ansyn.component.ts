ea5749c2e15414b4ff166214ce82cce8f6026221
shaharido1 <shaharido1@gmail.com>
2018-03-20 15:07:38 +0200

    remove version from html

--- src/app/@ansyn/ansyn/ansyn/ansyn.component.ts
+++ src/app/@ansyn/ansyn/ansyn/ansyn.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Case } from '@ansyn/menu-items/cases';
 import '@ansyn/core/utils/clone-deep';
-import * as packageJson from '../../../../../package.json';
 import 'rxjs/add/operator/distinctUntilChanged';
 import { CaseMapState } from '@ansyn/core/models/case.model';
 import { MapFacadeService } from '@ansyn/map-facade/services/map-facade.service';
@@ -37,7 +36,6 @@ export class AnsynComponent implements OnInit {
 
 	selectedCaseName$: Observable<string> = this.selectedCase$.pluck('name');
 	selectedCaseName: string;
-	version = (<any>packageJson).version;
 	isPinnedClass: string;
 
 	constructor(protected store$: Store<IAppState>) {

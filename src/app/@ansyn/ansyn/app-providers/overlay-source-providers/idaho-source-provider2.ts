e652422e93ae4f7de185abc58f74b37d2751d0eb
shaharido1 <shaharido1@gmail.com>
2018-03-20 18:09:24 +0200

    problem inheirit dependencies

--- src/app/@ansyn/ansyn/app-providers/overlay-source-providers/idaho-source-provider2.ts
+++ src/app/@ansyn/ansyn/app-providers/overlay-source-providers/idaho-source-provider2.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { IdahoSourceProvider } from './idaho-source-provider';
-import { Overlay } from '@ansyn/core';
+import { ErrorHandlerService, Overlay } from '@ansyn/core';
+import { HttpClient } from '@angular/common/http';
+import { IdahoOverlaysSourceConfig, IIdahoOverlaySourceConfig } from '@ansyn/ansyn/app-providers/overlay-source-providers/idaho-source-provider';
 
 export const IdahoOverlaySourceType2 = 'IDAHO2';
 
 @Injectable()
 export class IdahoSourceProvider2 extends IdahoSourceProvider {
 	sourceType = IdahoOverlaySourceType2;
-
+	constructor(public errorHandlerService: ErrorHandlerService, protected http: HttpClient, @Inject(IdahoOverlaysSourceConfig) protected _overlaySourceConfig: IIdahoOverlaySourceConfig) {
+		super(errorHandlerService, http, _overlaySourceConfig)
+		// problem to inherit the dependencies
+	}
 	protected parseData(idahoElement: any, token: string): Overlay {
 		const result = super.parseData(idahoElement, token);
 		result.isGeoRegistered = false;

f24d8f6d43eb24cef77e96514b0af8962a35d38d
ramtob <ramtob@gmail.com>
2018-03-22 16:19:03 +0200

    fix(imagery-tile-progress.component.ts): adding unsubscribe() in onDestroy()

--- src/app/@ansyn/map-facade/components/imagery-tile-progress/imagery-tile-progress.component.ts
+++ src/app/@ansyn/map-facade/components/imagery-tile-progress/imagery-tile-progress.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { IMapState, MapsProgress, mapStateSelector } from '@ansyn/map-facade/reducers/map.reducer';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
 	selector: 'ansyn-imagery-tile-progress',
 	templateUrl: './imagery-tile-progress.component.html',
 	styleUrls: ['./imagery-tile-progress.component.less']
 })
-export class ImageryTileProgressComponent implements OnInit {
+export class ImageryTileProgressComponent implements OnInit, OnDestroy {
 	@Input() mapId;
 	@Input() lowered;
 
+	private _subscriptions: Subscription[] = [];
 	progress$ = this.store$.select(mapStateSelector)
 		.pluck<IMapState, MapsProgress>('mapsProgress')
 		.distinctUntilChanged()
@@ -22,6 +24,12 @@ export class ImageryTileProgressComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		this.progress$.subscribe((progress) => this.progress = progress);
+		this._subscriptions.push(
+			this.progress$.subscribe((progress) => this.progress = progress)
+		);
+	}
+
+	ngOnDestroy(): void {
+		this._subscriptions.forEach(observable$ => observable$.unsubscribe());
 	}
 }

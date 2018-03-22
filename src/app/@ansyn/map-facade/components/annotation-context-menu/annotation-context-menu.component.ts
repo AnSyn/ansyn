0d370ab270ec82acc20d500dc920387725b1e1c9
ramtob <ramtob@gmail.com>
2018-03-22 16:50:13 +0200

    fix(annotation-context-menu.component.ts): adding unsubscribe() in onDestroy()

--- src/app/@ansyn/map-facade/components/annotation-context-menu/annotation-context-menu.component.ts
+++ src/app/@ansyn/map-facade/components/annotation-context-menu/annotation-context-menu.component.ts
@@ -1,17 +1,19 @@
-import { Component, ElementRef, HostBinding, HostListener, Input } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
 import { MapEffects } from '../../effects/map.effects';
 import { IMapState } from '../../reducers/map.reducer';
 import { Store } from '@ngrx/store';
 import { AnnotationContextMenuTriggerAction, AnnotationRemoveFeature } from '../../actions/map.actions';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
 	selector: 'ansyn-annotations-context-menu',
 	templateUrl: './annotation-context-menu.component.html',
 	styleUrls: ['./annotation-context-menu.component.less']
 })
-export class AnnotationContextMenuComponent {
+export class AnnotationContextMenuComponent implements OnInit, OnDestroy {
 	action: AnnotationContextMenuTriggerAction;
 	contextMenuWrapperStyle;
+	private _subscriptions: Subscription[] = [];
 
 	@Input('mapId') mapId;
 
@@ -29,31 +31,38 @@ export class AnnotationContextMenuComponent {
 	}
 
 	constructor(public store: Store<IMapState>, public mapEffect: MapEffects, public host: ElementRef) {
+	}
 
-		this.mapEffect.annotationContextMenuTrigger$
-			.filter(({ payload }) => payload.mapId === this.mapId)
-			.subscribe((action: AnnotationContextMenuTriggerAction) => {
-				this.action = action;
-				const { boundingRect } = <any> this.action.payload;
-				this.contextMenuWrapperStyle = {
-					top: `${boundingRect.top}px`,
-					left: `${boundingRect.left}px`,
-					width: `${boundingRect.width}px`,
-					height: `${boundingRect.height}px`,
-					transform: `rotate(${boundingRect.rotation}deg)`
-				};
+	ngOnInit() {
+		this._subscriptions.push(
+			this.mapEffect.annotationContextMenuTrigger$
+				.filter(({payload}) => payload.mapId === this.mapId)
+				.subscribe((action: AnnotationContextMenuTriggerAction) => {
+					this.action = action;
+					const {boundingRect} = <any> this.action.payload;
+					this.contextMenuWrapperStyle = {
+						top: `${boundingRect.top}px`,
+						left: `${boundingRect.left}px`,
+						width: `${boundingRect.width}px`,
+						height: `${boundingRect.height}px`,
+						transform: `rotate(${boundingRect.rotation}deg)`
+					};
+					this.host.nativeElement.focus();
+				}),
 
-				this.host.nativeElement.focus();
-			});
+			this.mapEffect.positionChanged$.subscribe(() => {
+				this.host.nativeElement.blur();
+			})
+		);
+	}
 
-		this.mapEffect.positionChanged$.subscribe(() => {
-			this.host.nativeElement.blur();
-		});
+	ngOnDestroy(): void {
+		this._subscriptions.forEach(observable$ => observable$.unsubscribe());
 	}
 
 	removeFeature($event) {
 		$event.stopPropagation();
-		const { featureId } = this.action.payload;
+		const {featureId} = this.action.payload;
 		this.store.dispatch(new AnnotationRemoveFeature(featureId));
 	}
 }

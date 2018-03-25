ca33ee069477c0c57a0d73dd4553def4d6d80eac
Lior Zeno <liorzino@gmail.com>
2018-03-25 18:22:30 +0300

    overlay.model: Add Planet models

--- src/app/@ansyn/core/models/overlay.model.ts
+++ src/app/@ansyn/core/models/overlay.model.ts
@@ -1,19 +1,16 @@
 import { LimitedArray } from '../utils/limited-array';
 import { CaseRegionState, CaseTimeState } from '@ansyn/core';
-import { GeoJsonObject } from "geojson";
 
-export interface OverlaysOpenAerialFetchData extends LimitedArray{
-	meta: {
-		provided_by: string,
-		license: string,
-		website: string,
-		page: number,
-		limit: number,
-		found: number
+export interface OverlaysPlanetFetchData extends LimitedArray {
+	_links: {
+		_first: string,
+		_next: string,
+		_self: string,
 	},
-	results: OpenAerialOverlay[]
+	features: PlanetOverlay[],
+	type: string
 }
-export interface OverlaysFetchData{
+export interface OverlaysFetchData {
 	data: Overlay[],
 	// number of overlays removed from total overlays (according to config)
 	limited: number
@@ -39,29 +36,40 @@ export class Overlay {
 	isGeoRegistered: boolean;
 }
 
-export class OpenAerialOverlay {
-	_id: string;
-	uuid: string;
-	_v: number;
-	title: string;
-	projection: string;
-	footprint: string;
-	gsd: number;
-	file_size: number;
-	acquisition_start: string;
-	acquisition_end: string;
-	platform: string;
-	provider: string;
-	contact: string;
-	imageUrl?: string;
-	properties: {
-		thumbnail?: string;
-		tms?: string;
-		wmts?: string;
-		sensor?: string;
+export class PlanetOverlay {
+	_links: {
+		_self: string,
+		assets: string,
+		thumbnail: string
 	};
-	meta_uri: string;
-	geojson: GeoJsonObject;
+	_permissions: string[];
+	geometry: GeoJSON.GeometryObject;
+	id: string;
+	properties: {
+		acquired: string,
+		anomalous_pixels: number,
+		black_fill: number,
+		cloud_cover: number,
+		columns: number,
+		epsg_code: number,
+		grid_cell: string,
+		ground_control: boolean,
+		gsd: number,
+		item_type: string,
+		origin_x: number,
+		origin_y: number,
+		pixel_resolution: number,
+		provider: string,
+		published: string,
+		rows: number,
+		satellite_id: string,
+		strip_id: string,
+		sun_azimuth: number,
+		sun_elevation: number,
+		updated: string,
+		usable_data: number,
+		view_angle: number
+	};
 }
 
 export interface OverlaysCriteria {

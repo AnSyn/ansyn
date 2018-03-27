4891ef1a1c69f44053bba44de1e8f2792dc07d27
Lior Zeno <liorzino@gmail.com>
2018-03-27 18:38:46 +0300

    travis: build Docker image only once

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+target=$1
+echo "target $target"
+if [ -z "$target" ]
+then
+	echo "Missing deployment target"
+	exit 1
+fi
+
+version=$2
+echo "version $version"
+if [ -z "$version" ]
+then
+	echo "Missing deployment version"
+	exit 1
+fi
+
+docker build -t "$target:$version" .

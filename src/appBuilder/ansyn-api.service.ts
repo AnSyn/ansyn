3e1c98b77948b7d4ebfffeeb810f2b7aaf7f6d65
shaharido1 <shaharido1@gmail.com>
2018-03-27 17:06:59 +0300

    drop mouse shadow effect

--- src/appBuilder/ansyn-api.service.ts
+++ src/appBuilder/ansyn-api.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { SetLayoutAction, SetWindowLayout, WindowLayout } from '@ansyn/core';
-import { Actions, Effect } from '@ngrx/effects';
-import { ToolsActionsTypes } from '@ansyn/menu-items/tools/actions/tools.actions';
-import { EmitMouseShadowProducers } from '@ansyn/menu-items';
-import { Observable } from 'rxjs/Observable';
+import { Actions } from '@ngrx/effects';
 import { Subject } from 'rxjs/Subject';
 import { Overlay } from '@ansyn/core/models';
 import { LoadOverlaysSuccessAction } from '@ansyn/overlays';
@@ -15,20 +12,20 @@ export class AnsynApi {
 
 	shadowMouseProducer$ = new Subject();
 
-	@Effect()
-	onStartMapShadow$: Observable<any> = this.actions$
-		.ofType<EmitMouseShadowProducers>(ToolsActionsTypes.EMIT_MOUSESHADOW_PRODUCER)
-		.take(1)
-		.do(({ payload }: EmitMouseShadowProducers) => {
-			payload.shadowMouseProducer.subscribe(content => {
-					console.log(content)
-					this.shadowMouseProducer$.next(content)
-			});
-			console.log(payload.shadowMouseProducer)
-		});
+	// @Effect()
+	// onStartMapShadow$: Observable<any> = this.actions$
+	// 	.ofType<EmitMouseShadowProducers>(ToolsActionsTypes.EMIT_MOUSESHADOW_PRODUCER)
+	// 	.take(1)
+	// 	.do(({ payload }: EmitMouseShadowProducers) => {
+	// 		payload.shadowMouseProducer.subscribe(content => {
+	// 				console.log(content)
+	// 				this.shadowMouseProducer$.next(content)
+	// 		});
+	// 		console.log(payload.shadowMouseProducer)
+	// 	});
 
 	constructor(public store: Store<any>, protected actions$: Actions) {
-			// this.store.dispatch(new LoadDefaultCaseAction());
+		// this.store.dispatch(new LoadDefaultCaseAction());
 	}
 
 
@@ -49,7 +46,7 @@ export class AnsynApi {
 			date: new Date(Date.now()),
 			azimuth: 3.2,
 			isGeoRegistered: false
-		}
+		};
 
 		this.store.dispatch(new LoadOverlaysSuccessAction([fakeOverlay]));
 	}

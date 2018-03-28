d10015fc6564d1fe6f7d1e78c8e0460c8bc24ca3
Lior Zeno <liorzino@gmail.com>
2018-03-28 11:29:09 +0300

    Revert "openlayers-map: null check is not enough, check that dispose is defined."
    This reverts commit 5e5202901d007910ad4d961c15db0175b7a58b6b.

--- src/app/@ansyn/plugins/openlayers/open-layers-map/openlayers-map/openlayers-map.component.ts
+++ src/app/@ansyn/plugins/openlayers/open-layers-map/openlayers-map/openlayers-map.component.ts
@@ -36,7 +36,7 @@ export class OpenlayersMapComponent implements OnInit, OnDestroy, IMapComponent 
 	}
 
 	ngOnDestroy(): void {
-		if (this._map && typeof this._map.dispose === 'function') {
+		if (this._map) {
 			this._map.dispose();
 		}
 	}

f9f4135f5f6d82bef2a8ee6f188b61ca11e40d17
YairTawil <yairtawil@gmail.com>
2018-03-29 10:56:15 +0300

    fix(imagery-plugins): factory to separated exported function

--- src/app/@ansyn/imagery/imagery/providers/imagery.providers.ts
+++ src/app/@ansyn/imagery/imagery/providers/imagery.providers.ts
@@ -1,13 +1,10 @@
 import { InjectionToken, Injector } from '@angular/core';
 import { BaseImageryPlugin } from '../../model/base-imagery-plugin';
 import { ImageryPluginProvider, PLUGINS_COLLECTION } from '../../model/plugins-collection';
-import { OpenlayersMapName } from '@ansyn/plugins/openlayers/open-layers-map/openlayers-map/openlayers-map';
 
 export const MAP_NAME: InjectionToken<string> = new InjectionToken('MAP_NAME');
 
-export const BaseImageryPluginProvider = {
-	provide: BaseImageryPlugin,
-	useFactory(pluginsCollection: Array<ImageryPluginProvider[]>, parent: Injector, mapName: string) {
+export function BaseImageryPluginProviderFactory(pluginsCollection: Array<ImageryPluginProvider[]>, parent: Injector, mapName: string) {
 		const providers = pluginsCollection
 			.reduce((v, i) => [...v, ...i], [])
 			.filter(({ provide, useClass }: ImageryPluginProvider) => provide === BaseImageryPlugin && useClass.supported.includes(mapName));
@@ -17,7 +14,11 @@ export const BaseImageryPluginProvider = {
 		}
 		const childInjector = Injector.create(providers, parent);
 		return childInjector.get(BaseImageryPlugin);
-	},
+}
+
+export const BaseImageryPluginProvider = {
+	provide: BaseImageryPlugin,
+	useFactory: BaseImageryPluginProviderFactory,
 	deps: [PLUGINS_COLLECTION, Injector, MAP_NAME]
 };
 

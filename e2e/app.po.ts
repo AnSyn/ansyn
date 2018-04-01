9725d38f3a6556802bfded79f4cffc7153341a3e
ramtob <ramtob@gmail.com>
2018-04-01 16:51:49 +0300

    feat(e2e): read the password from an environment variable

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,5 +1,7 @@
 import { browser, by, element } from 'protractor';
 
+let myConfig = require('../src/app/@ansyn/assets/config/app.config.json');
+
 export class AppPage {
 	private _LOGIN_COMPONENT_SELECTOR = 'ansyn-login';
 	loginComponent = element(by.css(this._LOGIN_COMPONENT_SELECTOR));
@@ -7,11 +9,16 @@ export class AppPage {
 	private _usernameInput = element(by.id('username'));
 	private _passwordInput = element(by.id('password'));
 	private _CORRECT_USERNAME = 'admin@ansyn.com';
-	private _CORRECT_PASSWORD = '123789';
+	private _CORRECT_PASSWORD;
 	loginButton = element(by.css('button'));
 
 	private _MAIN_COMPONENT_SELECTOR = 'ansyn-app';
 	mainComponent = element(by.css(this._MAIN_COMPONENT_SELECTOR));
+
+	constructor() {
+		this._CORRECT_PASSWORD = myConfig.e2eTestConfig.pass;
+		// console.log(this._CORRECT_PASSWORD);
+	}
 
 	navigateTo() {
 		return browser.get('/');

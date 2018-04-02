5005145d24d7573d2b4b5f33d8300d7e480ef4d6
ramtob <ramtob@gmail.com>
2018-04-02 15:42:31 +0300

    Rewinding (cancelling) all use of the app config files. The e2e test does not need them. It can read an environment variable directly.

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -9,16 +9,11 @@ export class AppPage {
 	private _usernameInput = element(by.id('username'));
 	private _passwordInput = element(by.id('password'));
 	private _CORRECT_USERNAME = 'admin@ansyn.com';
-	private _CORRECT_PASSWORD;
+	private _CORRECT_PASSWORD = process.env.ANSYN_E2E_PASS;
 	loginButton = element(by.css('button'));
 
 	private _MAIN_COMPONENT_SELECTOR = 'ansyn-app';
 	mainComponent = element(by.css(this._MAIN_COMPONENT_SELECTOR));
-
-	constructor() {
-		this._CORRECT_PASSWORD = myConfig.e2eTestConfig.pass;
-		// console.log(this._CORRECT_PASSWORD);
-	}
 
 	navigateTo() {
 		return browser.get('/');

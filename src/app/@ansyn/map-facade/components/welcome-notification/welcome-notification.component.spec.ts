2a91a58bd0d3b6c6c6e5c9ea3362fee054671d75
ramtob <ramtob@gmail.com>
2018-04-03 16:49:54 +0300

    feat (imageries-manager.component): adding new sub component welcome-notification

---
+++ src/app/@ansyn/map-facade/components/welcome-notification/welcome-notification.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { WelcomeNotificationComponent } from './welcome-notification.component';
+import { StoreModule } from '@ngrx/store';
+import { mapFeatureKey, MapReducer } from '../../reducers/map.reducer';
+
+fdescribe('WelcomeNotificationComponent', () => {
+	let component: WelcomeNotificationComponent;
+	let fixture: ComponentFixture<WelcomeNotificationComponent>;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [StoreModule.forRoot({ [mapFeatureKey]: MapReducer })],
+			declarations: [WelcomeNotificationComponent]
+		})
+			.compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(WelcomeNotificationComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+});

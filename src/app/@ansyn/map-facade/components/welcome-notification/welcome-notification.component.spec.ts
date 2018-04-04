417b1301ae3301fea6e749c4cd24b2c939697e6b
ramtob <ramtob@gmail.com>
2018-04-04 16:53:17 +0300

    feat (welcome-notification.spec): test with the store (not finished)

--- src/app/@ansyn/map-facade/components/welcome-notification/welcome-notification.component.spec.ts
+++ src/app/@ansyn/map-facade/components/welcome-notification/welcome-notification.component.spec.ts
@@ -1,14 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { WelcomeNotificationComponent } from './welcome-notification.component';
-import { StoreModule } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
 import { IMapFacadeConfig } from '@ansyn/map-facade/models/map-config.model';
 import { mapFacadeConfig } from '@ansyn/map-facade/models/map-facade.config';
 import { CoreModule } from '@ansyn/core/core.module';
 import { EffectsModule } from '@ngrx/effects';
+import { ICoreState } from '@ansyn/core/reducers/core.reducer';
+import { SetIsAfterLoginFlagAction } from '@ansyn/core';
 
 fdescribe('WelcomeNotificationComponent', () => {
 	let component: WelcomeNotificationComponent;
 	let fixture: ComponentFixture<WelcomeNotificationComponent>;
+	let store: Store<any>;
+
+	beforeEach(inject([Store], (_store: Store<ICoreState>) => {
+		store = _store;
+		store.next(new SetIsAfterLoginFlagAction(true));
+	}));
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -39,5 +47,15 @@ fdescribe('WelcomeNotificationComponent', () => {
 	it('tabindex attribute should return zero', () => {
 		expect(component.tabindex).toEqual(0);
 	});
+
+	it('element should be focused', () => {
+
+	});
+
+	// it('if isAfterLogin is true, bring the element to focus', () => {
+	// 	spyOn(component.elem.nativeElement, 'focus');
+	// 	(<Store<ICoreState>>component.store$.select(coreStateSelector)).next(new SetIsAfterLoginFlagAction(true));
+	// 	expect(component.elem.nativeElement.focus).toHaveBeenCalled();
+	// });
 
 });

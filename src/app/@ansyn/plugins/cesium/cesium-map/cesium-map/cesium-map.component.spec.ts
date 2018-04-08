1973620b49a6fbb030c692afff1e359fe1633bb0
YairTawil <yairtawil@gmail.com>
2018-04-08 14:02:45 +0300

    test: for each imagery map components

--- src/app/@ansyn/plugins/cesium/cesium-map/cesium-map/cesium-map.component.spec.ts
+++ src/app/@ansyn/plugins/cesium/cesium-map/cesium-map/cesium-map.component.spec.ts
@@ -1,10 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CesiumMapComponent } from './cesium-map.component';
 import { PLUGINS_COLLECTIONS } from '@ansyn/imagery';
+import { cold, hot } from 'jasmine-marbles';
+import { Observable } from 'rxjs/Observable';
+import { OpenLayersMap } from '@ansyn/plugins/openlayers/open-layers-map';
+import { CesiumMap } from '@ansyn/plugins/cesium/cesium-map';
 
 describe('CesiumMapComponent', () => {
 	let component: CesiumMapComponent;
 	let fixture: ComponentFixture<CesiumMapComponent>;
+	let map: CesiumMap;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -17,17 +22,32 @@ describe('CesiumMapComponent', () => {
 		fixture = TestBed.createComponent(CesiumMapComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
+		map = (<any>component).map;
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
 
-	it('createMap should raise mapCreated event', () => {
+	describe('createMap should raise map via success', () => {
+		let success: boolean;
 
-		spyOn(component.mapCreated, 'emit');
-		component.createMap([]);
-		// Implementation of Cesium is missing.
-		expect(component.mapCreated.emit).not.toHaveBeenCalled();
+		beforeEach(() => {
+			spyOn(map, 'initMap').and.callFake(() => Observable.of(success));
+		});
+
+		it('on success', () => {
+			success = true;
+			const expectedResult = cold('(b|)', { b: map });
+			expect(component.createMap([])).toBeObservable(expectedResult);
+
+		});
+
+		it('on failed', () => {
+			success = false;
+			const expectedResult = cold('|');
+			expect(component.createMap([])).toBeObservable(expectedResult);
+		})
+
 	});
 });

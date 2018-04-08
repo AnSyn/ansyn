1714fdd051fb1a4b88c122729e5df246a9267361
YairTawil <yairtawil@gmail.com>
2018-04-08 09:02:27 +0300

    fix(monitor-plugin): kill old events

--- src/app/@ansyn/plugins/openlayers/monitor/monitor.plugin.ts
+++ src/app/@ansyn/plugins/openlayers/monitor/monitor.plugin.ts
@@ -6,10 +6,21 @@ import { Store } from '@ngrx/store';
 import { OpenlayersMapName } from '@ansyn/plugins/openlayers/open-layers-map';
 import { ProjectableRaster } from '@ansyn/plugins/openlayers/open-layers-map/models/projectable-raster';
 import { DisabledOpenLayersMapName } from '@ansyn/plugins/openlayers/open-layers-map/openlayers-disabled-map/openlayers-disabled-map';
+import TileSource from 'ol/source/tile';
 
 @Injectable()
 export class MonitorPlugin extends BaseImageryPlugin {
 	static supported = [OpenlayersMapName, DisabledOpenLayersMapName];
+
+	source: TileSource;
+
+	isFirstLoad: boolean;
+
+	tilesCounter = {
+		total: 0,
+		success: 0,
+		error: 0
+	};
 
 	messages = {
 		all: 'Failed to load overlay',
@@ -30,7 +41,7 @@ export class MonitorPlugin extends BaseImageryPlugin {
 			.do(this.monitorSource.bind(this));
 	}
 
-	getMainSource() {
+	getMainSource(): TileSource {
 		const layer = this.communicator.ActiveMap.mapObject.getLayers()
 			.getArray().find(layer => layer.get('name') === 'main');
 
@@ -41,66 +52,87 @@ export class MonitorPlugin extends BaseImageryPlugin {
 		let source = layer.getSource();
 
 		if (source instanceof ProjectableRaster) {
-			return (<ProjectableRaster>source).sources[0];
+			return <TileSource> (<ProjectableRaster>source).sources[0];
 		}
 
-		return source;
+		return <TileSource> source;
 	}
 
 	monitorSource() {
-		const source = this.getMainSource(), mapId = this.mapId;
-		let isFirstLoad = true;
-
-		let tilesCounter = {
-			total: 0,
-			success: 0,
-			error: 0
-		};
-
-		// For it to be reset after every zoom load
-		const resetCounterWhenDone = () => {
-			if (tilesCounter.total === tilesCounter.success + tilesCounter.error) {
-				tilesCounter.total = 0;
-				tilesCounter.success = 0;
-				tilesCounter.error = 0;
-				isFirstLoad = false;
-				this.communicator.ActiveMap.mapObject.renderSync();
-			}
-
-			const progress = tilesCounter.total ? (tilesCounter.success + tilesCounter.error) / tilesCounter.total : 1;
-
-			this.store$.dispatch(new SetProgressBarAction({ progress: progress * 100, mapId }));
-		};
-
-		// source.once('tileloadstart', () => startTimingLog(this.mapType));
-
-		source.on('tileloadstart', () => tilesCounter.total++);
-
-		source.on('tileloadend', () => {
-			tilesCounter.success++;
-			resetCounterWhenDone();
-		});
-
-		source.on('tileloaderror', () => {
-			tilesCounter.error++;
-
-			let message;
-
-			if (isFirstLoad && tilesCounter.error === tilesCounter.total) { // All of em, on first load
-				message = this.messages.all;
-			} else if (tilesCounter.error === 1) { // Only 1
-				message = this.messages.one;
-			} else { // More than 1, but not all
-				message = this.messages.partial.replace('{{amount}}', String(tilesCounter.error));
-			}
-
-			this.store$.dispatch(new SetToastMessageAction({
-				toastText: message,
-				showWarningIcon: true
-			}));
-
-			resetCounterWhenDone();
-		});
+		this.killMonitorEvents();
+		this.initMonitor();
+		this.setMonitorEvents();
 	}
 
+	resetCounterWhenDone = () => {
+		if (this.tilesCounter.total === this.tilesCounter.success + this.tilesCounter.error) {
+			this.tilesCounter.total = 0;
+			this.tilesCounter.success = 0;
+			this.tilesCounter.error = 0;
+			this.isFirstLoad = false;
+			this.communicator.ActiveMap.mapObject.renderSync();
+		}
+
+		const progress = this.tilesCounter.total ? (this.tilesCounter.success + this.tilesCounter.error) / this.tilesCounter.total : 1;
+
+		this.store$.dispatch(new SetProgressBarAction({ progress: progress * 100, mapId: this.mapId }));
+	};
+
+	tileLoadStart() {
+		this.tilesCounter.total++;
+	}
+
+	tileLoadEnd() {
+		this.tilesCounter.success++;
+		this.resetCounterWhenDone();
+	}
+
+	tileLoadError() {
+		this.tilesCounter.error++;
+
+		let message;
+
+		if (this.isFirstLoad && this.tilesCounter.error === this.tilesCounter.total) { // All of em, on first load
+			message = this.messages.all;
+		} else if (this.tilesCounter.error === 1) { // Only 1
+			message = this.messages.one;
+		} else { // More than 1, but not all
+			message = this.messages.partial.replace('{{amount}}', String(this.tilesCounter.error));
+		}
+
+		this.store$.dispatch(new SetToastMessageAction({
+			toastText: message,
+			showWarningIcon: true
+		}));
+
+		this.resetCounterWhenDone();
+	}
+
+	initMonitor() {
+		this.source = this.getMainSource();
+		this.isFirstLoad = true;
+		const total = 0, success = 0, error = 0;
+		this.tilesCounter = { total, success, error };
+	}
+
+	setMonitorEvents() {
+		if (this.source) {
+			this.source.on('tileloadstart', this.tileLoadStart, this);
+			this.source.on('tileloadend', this.tileLoadEnd, this);
+			this.source.on('tileloaderror', this.tileLoadError, this);
+		}
+	}
+
+	killMonitorEvents() {
+		if (this.source) {
+			this.source.un('tileloadstart', this.tileLoadStart, this);
+			this.source.un('tileloadend', this.tileLoadEnd, this);
+			this.source.un('tileloaderror', this.tileLoadError, this);
+		}
+	}
+
+	dispose() {
+		this.killMonitorEvents();
+		super.dispose();
+	}
 }

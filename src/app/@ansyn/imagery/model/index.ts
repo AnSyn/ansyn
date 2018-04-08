a01dc2125ea783d8fc6b95c008f0270f65a0d887
YairTawil <yairtawil@gmail.com>
2018-04-08 11:42:22 +0300

    refactor(imagery): rename map component file

--- src/app/@ansyn/imagery/model/index.ts
+++ src/app/@ansyn/imagery/model/index.ts
@@ -2,5 +2,5 @@ export * from './iimagery-config';
 export * from './imap';
 export * from './base-source-provider.model';
 export * from './base-imagery-plugin';
-export * from './imap-component';
+export * from './imagery-map-component';
 export * from './base-imagery-visualizer';

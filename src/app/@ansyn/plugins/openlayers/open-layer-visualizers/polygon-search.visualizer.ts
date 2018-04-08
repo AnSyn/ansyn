20982cef35b0caaa76d6d2d25eebf3b0e6c4bae6
DESKTOP-94M6T0L\user <shai.gm@gmail.com>
2018-04-08 19:40:14 +0300

    manage twicks between pinpoint and polygon search 1

--- src/app/@ansyn/plugins/openlayers/open-layer-visualizers/polygon-search.visualizer.ts
+++ src/app/@ansyn/plugins/openlayers/open-layer-visualizers/polygon-search.visualizer.ts
@@ -1,47 +1,64 @@
 import { EntitiesVisualizer } from './entities-visualizer';
-import Feature from 'ol/feature';
-import Icon from 'ol/style/icon';
 import Style from 'ol/style/style';
 import proj from 'ol/proj';
+import GeomPolygon from "ol/geom/polygon";
+import Draw from "ol/interaction/draw";
 import { StatusBarActionsTypes, statusBarFlagsItems, UpdateStatusFlagsAction } from "@ansyn/status-bar";
 import { Observable } from "rxjs/Observable";
-import Draw from "ol/interaction/draw";
-import * as ol from "openlayers";
 import { VisualizerInteractions } from "@ansyn/imagery/model/base-imagery-visualizer";
-import { CommunicatorEntity, IMap, IVisualizerEntity } from "@ansyn/imagery";
+import { CommunicatorEntity } from "@ansyn/imagery";
 import { Actions } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import LineString from "ol/geom/linestring";
-import MultiLineString from "ol/geom/multilinestring";
 import { cloneDeep, remove } from "lodash";
 import { FeatureCollection, GeometryObject } from "geojson";
 import { SetAnnotationsLayer } from "@ansyn/menu-items/layers-manager/actions/layers.actions";
 import { ILayerState, layersStateSelector } from "@ansyn/menu-items/layers-manager/reducers/layers.reducer";
 import { OverlaysCriteria, SetOverlaysCriteriaAction } from "@ansyn/core";
-import GeomPolygon from "ol/geom/polygon";
+import { StatusBarFlag } from "@ansyn/status-bar/models";
+import { statusBarStateSelector } from "@ansyn/status-bar/reducers/status-bar.reducer";
 
 export class PolygonSearchVisualizer extends EntitiesVisualizer {
 	static fillAlpha = 0.4;
 	static lastPolygonSearchId: any = '';
-	_iconSrc: Style;
 	public annotationsLayer;
+	flags: Map<StatusBarFlag, boolean> = new Map<StatusBarFlag, boolean>();
 
 	get drawInteractionHandler() {
 		return this.interactions.get(VisualizerInteractions.drawInteractionHandler);
 	}
 
+	flags$ = this.store$.select(statusBarStateSelector).pluck('flags').distinctUntilChanged();
+
 	polygonSearch$: Observable<any> = this.actions$
 		.ofType<UpdateStatusFlagsAction>(StatusBarActionsTypes.UPDATE_STATUS_FLAGS)
 		.filter(action => action.payload.key === statusBarFlagsItems.polygonSearch)
 		.do(() => {
+			const isPolygonSearch = this.flags.get('POLYGON_SEARCH');
 			const drawInteractionHandler = new Draw({
 				type: 'Polygon',
 				geometryFunction: undefined,
 				condition: (event: ol.MapBrowserEvent) => (<MouseEvent>event.originalEvent).which === 1,
 				style: this.featureStyle.bind(this)
 			});
-			drawInteractionHandler.on('drawend', this.onDrawEndEvent.bind(this));
-			this.addInteraction(VisualizerInteractions.drawInteractionHandler, drawInteractionHandler);
+			if (isPolygonSearch === true ) {
+				drawInteractionHandler.on('drawend', this.onDrawEndEvent.bind(this));
+				this.addInteraction(VisualizerInteractions.drawInteractionHandler, drawInteractionHandler);
+			}
+			else {
+				this.removeDrawInteraction();
+			}
+		});
+
+	pinPointSearch$: Observable<any> = this.actions$
+		.ofType<UpdateStatusFlagsAction>(StatusBarActionsTypes.UPDATE_STATUS_FLAGS)
+		.filter(action => action.payload.key === statusBarFlagsItems.pinPointSearch && Boolean(this.flags.get('PIN_POINT_SEARCH')))
+		.withLatestFrom(this.store$.select(layersStateSelector).pluck<ILayerState, FeatureCollection<any>>('annotationsLayer'), (action: any, annotationsLayer: any): any =>
+		{
+			return annotationsLayer;
+		})
+		.do((annotationsLayer) => {
+			this.annotationsLayer = annotationsLayer;
+			this.removeLastSearchPolygon();
 		});
 
 
@@ -54,7 +71,8 @@ export class PolygonSearchVisualizer extends EntitiesVisualizer {
 	initEffects() {
 		this.subscriptions.push(
 			this.polygonSearch$.subscribe(),
-			this.annotationsLayer$.subscribe()
+			this.annotationsLayer$.subscribe(),
+			this.pinPointSearch$.subscribe()
 		);
 	}
 
@@ -63,6 +81,16 @@ export class PolygonSearchVisualizer extends EntitiesVisualizer {
 		this.initEffects();
 	}
 
+
+	removeLastSearchPolygon() {
+		let updatedAnnotationsLayer = <FeatureCollection<any>> { ...this.annotationsLayer };
+
+		remove(updatedAnnotationsLayer.features, function(aFeature)
+		{
+			return aFeature.properties.id === PolygonSearchVisualizer.lastPolygonSearchId;
+		});
+		this.store$.dispatch(new SetAnnotationsLayer(updatedAnnotationsLayer));
+	}
 
 	onDrawEndEvent({ feature }) {
 		this.removeDrawInteraction();
@@ -83,12 +111,8 @@ export class PolygonSearchVisualizer extends EntitiesVisualizer {
 			.take(1)
 			.subscribe((featureCollection: FeatureCollection<GeometryObject>) => {
 				const [geoJsonFeature] = featureCollection.features;
+				this.removeLastSearchPolygon();
 				let updatedAnnotationsLayer = <FeatureCollection<any>> { ...this.annotationsLayer };
-
-				remove(updatedAnnotationsLayer.features, function(aFeature)
-				{
-					return aFeature.properties.id === PolygonSearchVisualizer.lastPolygonSearchId;
-				});
 				PolygonSearchVisualizer.lastPolygonSearchId = geoJsonFeature.properties.id;
 				updatedAnnotationsLayer.features.push(geoJsonFeature);
 				this.store$.dispatch(new SetAnnotationsLayer(updatedAnnotationsLayer));
@@ -115,6 +139,7 @@ export class PolygonSearchVisualizer extends EntitiesVisualizer {
 
 	removeDrawInteraction() {
 		this.removeInteraction(VisualizerInteractions.drawInteractionHandler);
+		this.removeInteraction('drawInteractionHandler');
 	}
 
 	constructor(public store$: Store<any>,
@@ -136,6 +161,9 @@ export class PolygonSearchVisualizer extends EntitiesVisualizer {
 					width: 1
 				}
 			}
+		});
+		this.flags$.subscribe((flags: Map<StatusBarFlag, boolean>) => {
+			this.flags = new Map(flags);
 		});
 	}
 }

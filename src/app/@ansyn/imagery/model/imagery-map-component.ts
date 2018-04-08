c3dc479f576918f9e802c672898ac2849aa992c4
YairTawil <yairtawil@gmail.com>
2018-04-08 12:07:25 +0300

    fix(imagery-map): remove redundant mapCreated emitter

--- src/app/@ansyn/imagery/model/imagery-map-component.ts
+++ src/app/@ansyn/imagery/model/imagery-map-component.ts
@@ -1,21 +1,20 @@
-import { ElementRef, EventEmitter, OnDestroy, ViewChild } from '@angular/core';
+import { ElementRef, OnDestroy } from '@angular/core';
 import { IMap } from './imap';
 import { CaseMapPosition } from '@ansyn/core/models/case-map-position.model';
 import { BaseImageryPlugin } from '@ansyn/imagery';
+import { Observable } from 'rxjs/Observable';
 
 export class ImageryMapComponent implements OnDestroy{
 	protected map: IMap;
 	protected mapElement: ElementRef;
-	public mapCreated: EventEmitter<IMap> = new EventEmitter<IMap>();
 	public plugins: BaseImageryPlugin[];
 
-	createMap(layers: any, position?: CaseMapPosition): void {
-		this.map
+	createMap(layers: any, position?: CaseMapPosition): Observable<IMap> {
+		return this.map
 			.initMap(this.mapElement.nativeElement, layers, position)
 			.filter(success => success)
-			.do(() => this.mapCreated.emit(this.map))
+			.map(() => this.map)
 			.take(1)
-			.subscribe();
 	};
 
 	ngOnDestroy(): void {

7246d5cea0456233e52b99b3f13d2f7942efb670
ramtob <ramtob@gmail.com>
2018-04-10 16:56:49 +0300

    feat (copyright-notice): adding new component (cont.)

--- src/app/@ansyn/map-facade/components/overlay-copyright-notice/overlay-copyright-notice.component.ts
+++ src/app/@ansyn/map-facade/components/overlay-copyright-notice/overlay-copyright-notice.component.ts
@@ -1,7 +1,7 @@
-import { Component, Inject, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Overlay } from '@ansyn/core/models/overlay.model';
 import { IOverlaysConfig, KeyAndValue } from '@ansyn/overlays/models/overlays.config';
-import { OverlaysConfig } from '@ansyn/overlays';
+import { OverlaysService } from '@ansyn/overlays';
 
 @Component({
 	selector: 'ansyn-overlay-copyright-notice',
@@ -12,7 +12,7 @@ export class OverlayCopyrightNoticeComponent implements OnChanges {
 
 	@Input()  set overlay(newOverlay: Overlay) {
 		if (newOverlay) {
-			const keyAndValue: KeyAndValue = this.config.titlesForMapSourceTypes.find(keyAndValue => keyAndValue.key === newOverlay.sourceType);
+			const keyAndValue: KeyAndValue = this._config.titlesForMapSourceTypes.find(keyAndValue => keyAndValue.key === newOverlay.sourceType);
 			if (keyAndValue) {
 				this._title = keyAndValue.value;
 			// } else { // for testing
@@ -26,7 +26,10 @@ export class OverlayCopyrightNoticeComponent implements OnChanges {
 		return this._title;
 	}
 
-	constructor(@Inject(OverlaysConfig) public config: IOverlaysConfig) {
+	private _config: IOverlaysConfig;
+
+	constructor(overlaysService: OverlaysService) {
+		this._config = overlaysService.config;
 	}
 
 	ngOnChanges() {

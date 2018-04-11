2a22a6da6f6338de4cf3bc981f8639bd27aa1c78
ramtob <ramtob@gmail.com>
2018-04-11 11:13:19 +0300

    Unit testing tweaks

--- src/app/@ansyn/map-facade/components/overlay-copyright-notice/overlay-copyright-notice.component.spec.ts
+++ src/app/@ansyn/map-facade/components/overlay-copyright-notice/overlay-copyright-notice.component.spec.ts
@@ -1,12 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { OverlayCopyrightNoticeComponent } from './overlay-copyright-notice.component';
-import { SetWasOverlayCopyrightNoticeShownFlagAction } from '@ansyn/core';
 import { OverlaysModule } from '@ansyn/overlays';
 import { EffectsModule } from '@ngrx/effects';
-import { mapFeatureKey, MapReducer } from '@ansyn/map-facade/reducers/map.reducer';
 import { StoreModule } from '@ngrx/store';
 
-fdescribe('OverlayCopyrightNoticeComponent', () => {
+describe('OverlayCopyrightNoticeComponent', () => {
 	let component: OverlayCopyrightNoticeComponent;
 	let fixture: ComponentFixture<OverlayCopyrightNoticeComponent>;
 

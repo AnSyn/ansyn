b975a6eda40c3cafb6a7b22405aaa004388b10a5
Blaussh <shai.gm@gmail.com>
2018-04-12 19:04:06 +0300

    changed search method in overlays.service - removed unnecessary check

--- src/app/@ansyn/overlays/services/overlays.service.ts
+++ src/app/@ansyn/overlays/services/overlays.service.ts
@@ -98,7 +98,7 @@ export class OverlaysService {
 	}
 
 	search(params: OverlaysCriteria): Observable<OverlaysFetchData> {
-		let feature =  params.region.type !== 'Feature'  ? params.region : { type: 'Feature', geometry: params.region }
+		let feature = params.region
 		return this._overlaySourceProvider.fetch({
 			limit: this.config.limit,
 			region: feature,

f69c10d9d467740d6ca2b5518b3aebfd417bf65b
shaharido1 <shaharido1@gmail.com>
2018-04-16 13:19:59 +0300

    extended map fix

--- src/app/@ansyn/overlays/reducers/extendedMap.class.ts
+++ src/app/@ansyn/overlays/reducers/extendedMap.class.ts
@@ -1,8 +1,9 @@
 export class ExtendMap<T, V> {
 
 	private map = new Map<T, V>();
+
 	get keys() {
-		return this.keyList();
+		return Array.from(this.map.keys())
 	}
 
 	set(t: T, v: V) {
@@ -14,68 +15,22 @@ export class ExtendMap<T, V> {
 	}
 
 
-	keyList(): Array<T> {
-		let keyList = [];
-		const keysIterator = this.map.keys();
-		let keyInstance = keysIterator.next();
-		while (!keyInstance.done) {
-			const key = keyInstance.value;
-			keyList.push(key);
-			keyInstance = keysIterator.next();
-		}
-		return keyList;
+
+	findKeysByValue(searchValue, arrayField) {
+		return Array.from(this.map)
+			.filter(([key, val]) => Boolean(val && val[arrayField] && val[arrayField].some(item => item === searchValue)))
+			.map(([key, val]) => key)
+
 	}
 
 
-	findKeysByValue(searchValue, arrayField?) {
-		let foundInKeys = [];
-		const keys = this.map.keys();
-		let keyValue = keys.next();
-		while (!keyValue.done) {
-			const key = keyValue.value;
-			const val = this.map.get(key);
-			if (val) {
-				switch (typeof val) {
-					case 'string' : {
-						if (val === searchValue) {
-							foundInKeys.push(key);
-						}
-						break;
-					}
-					case 'object':
-						if (Array.isArray(val) && Boolean(val.find(item => item === searchValue))) {
-							foundInKeys.push(key);
-						}
-						else if (arrayField && val[arrayField]) {
-							foundInKeys.push(key);
-						}
-				}
-				keyValue = keys.next();
-			}
-		}
-		return foundInKeys;
-	}
-
-	removeValueFromMap(key: T, searchValue, arrayField?) {
+	removeValueFromMap(key: T, searchValue, arrayField) {
 		// currently out of use
-		let val = this.map.get(key);
-		switch (typeof val) {
-			case 'string' : {
-				if (val === searchValue) {
-					this.map.set(key, null);
-				}
-				break;
-			}
-			case 'object':
-				let newValue = val;
-				if (Array.isArray(val)) {
-					newValue = <any>val.filter(item => item !== searchValue);
-				}
-				else if (arrayField && val[arrayField]) {
-					const newList = <any>val[arrayField].filter(item => item !== searchValue);
-					newValue[arrayField] = newList;
-				}
-				this.map.set(key, newValue);
-		}
+		return Array.from(this.map)
+			.filter(([key, val]) => Boolean(val && val[arrayField] && val[arrayField].find(item => item === searchValue)))
+			.forEach(([key, val]) => {
+				val[arrayField] = val[arrayField].filter(item => item !== searchValue);
+				this.map.set(key, val);
+			})
 	}
 }

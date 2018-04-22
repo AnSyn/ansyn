c2f5dc9e2b8bf6a74688daa768e0073c4fa85a4d
YairTawil <yairtawil@gmail.com>
2018-04-22 12:57:18 +0300

    fix(status-bar): geo filter controllers

--- src/app/@ansyn/status-bar/reducers/status-bar.reducer.ts
+++ src/app/@ansyn/status-bar/reducers/status-bar.reducer.ts
@@ -1,7 +1,9 @@
-import { StatusActions, StatusBarActionsTypes } from '../actions/status-bar.actions';
+import { StatusBarActionsTypes } from '../actions/status-bar.actions';
 import { createFeatureSelector, createSelector, MemoizedSelector } from '@ngrx/store';
 import { ComboBoxesProperties } from '@ansyn/status-bar';
 import { statusBarFlagsItemsEnum } from '@ansyn/status-bar/models/status-bar-flag-items.model';
+import { isNil } from 'lodash';
+import { StatusBarActions } from '@ansyn/status-bar/actions/status-bar.actions';
 
 export const statusBarToastMessages = {
 	showLinkCopyToast: 'Link copied to clipboard',
@@ -21,11 +23,8 @@ export const StatusBarInitialState: IStatusBarState = {
 export const statusBarFeatureKey = 'statusBar';
 export const statusBarStateSelector: MemoizedSelector<any, IStatusBarState> = createFeatureSelector<IStatusBarState>(statusBarFeatureKey);
 
-export function StatusBarReducer(state = StatusBarInitialState, action: StatusActions): IStatusBarState {
+export function StatusBarReducer(state = StatusBarInitialState, action: StatusBarActions | any ): IStatusBarState {
 	switch (action.type) {
-		case StatusBarActionsTypes.COPY_SELECTED_CASE_LINK:
-			return Object.assign({}, state);
-
 		case StatusBarActionsTypes.UPDATE_STATUS_FLAGS:
 			const items = Object.keys(statusBarFlagsItemsEnum).map(k => statusBarFlagsItemsEnum[k]);
 			if (!items.includes(action.payload.key)) {
@@ -34,10 +33,12 @@ export function StatusBarReducer(state = StatusBarInitialState, action: StatusAc
 
 			const newMap = new Map(state.flags);
 
-			const value = typeof action.payload.value !== 'undefined' ? action.payload.value : !newMap.get(action.payload.key);
+			const value = !isNil(action.payload.value) ? action.payload.value : !newMap.get(action.payload.key);
 
 			newMap.set(action.payload.key, value);
-
+			if (action.payload.key === statusBarFlagsItemsEnum.geoFilterSearch && value) {
+				newMap.set(statusBarFlagsItemsEnum.geoFilterIndicator, true);
+			}
 			return { ...state, flags: newMap };
 
 		case StatusBarActionsTypes.SET_COMBOBOXES_PROPERTIES:

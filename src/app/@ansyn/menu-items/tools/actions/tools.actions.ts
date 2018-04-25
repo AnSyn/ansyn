48a75bbc7ed753e3a6d68f769a13e0ff3e3b7300
Blaussh <shai.gm@gmail.com>
2018-04-25 16:50:00 +0300

    corrected  UPDATE_IMAGE_PROCESSING_HASH typo

--- src/app/@ansyn/menu-items/tools/actions/tools.actions.ts
+++ src/app/@ansyn/menu-items/tools/actions/tools.actions.ts
@@ -26,7 +26,7 @@ export const ToolsActionsTypes = {
 	SET_MANUAL_IMAGE_PROCESSING_ARGUMENTS: type('SET_MANUAL_IMAGE_PROCESSING_ARGUMENTS'),
 	MAP_GEO_ENABLED_MODE_CHANGED: type('MAP_GEO_ENABLED_MODE_CHANGED'),
 	ANNOTATION_SET_PROPERTIES: type('ANNOTATION_SET_PROPERTIES'),
-	UPDATE_IMAGE_PROCESIING_HASH: type('UPDATE_IMAGE_PROCESIING_HASH'),
+	UPDATE_IMAGE_PROCESSING_HASH: type('UPDATE_IMAGE_PROCESSING_HASH'),
 	SET_SUB_MENU: type('SET_SUB_MENU'),
 	STORE: {
 		SET_ANNOTATION_MODE: type('SET_ANNOTATION_MODE')
@@ -35,7 +35,7 @@ export const ToolsActionsTypes = {
 };
 
 export class UpdateImageProcessingHash implements Action {
-	type = ToolsActionsTypes.UPDATE_IMAGE_PROCESIING_HASH;
+	type = ToolsActionsTypes.UPDATE_IMAGE_PROCESSING_HASH;
 
 	constructor(public payload: ImageProcessingHash) {
 

58c136b2749b314f66c815210c4c7b0b9098dbd2
ramtob <ramtob@gmail.com>
2018-04-26 16:56:53 +0300

    Show/hide and locate the overview component, according to the hover event data

--- src/app/@ansyn/overlays/components/overlay-overview/overlay-overview.component.ts
+++ src/app/@ansyn/overlays/components/overlay-overview/overlay-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { IOverlaysState, overlaysStateSelector } from '@ansyn/overlays/reducers/overlays.reducer';
 import { Store } from '@ngrx/store';
@@ -8,28 +8,40 @@ import { HoveredOverlayData } from '@ansyn/overlays/models/hovered-overlay-data.
 @Component({
 	selector: 'ansyn-overlay-overview',
 	templateUrl: './overlay-overview.component.html',
-	styleUrls: ['./overlay-overview.component.css']
+	styleUrls: ['./overlay-overview.component.less']
 })
 export class OverlayOverviewComponent implements OnInit, OnDestroy {
 	private _subscriptions: Subscription[] = [];
+	public x: number;
+	public y: number;
+	public showFlag = false;
 
 	hoveredOverlay$: Observable<HoveredOverlayData> = this.store$.select(overlaysStateSelector)
 		.pluck<IOverlaysState, HoveredOverlayData>('hoveredOverlay')
 		.distinctUntilChanged();
 
-	constructor(protected store$: Store<IOverlaysState>) {
-	}
+	constructor(
+		protected store$: Store<IOverlaysState>,
+		protected element: ElementRef
+	) {	}
 
 	ngOnInit() {
 		this._subscriptions.push(
-			this.hoveredOverlay$.subscribe((eventData: HoveredOverlayData) => {
-					console.log('hover data', eventData);
-				}
-			)
+			this.hoveredOverlay$.subscribe(this.showOrHide.bind(this))
 		);
 	}
 
 	ngOnDestroy(): void {
 		this._subscriptions.forEach(observable$ => observable$.unsubscribe());
+	}
+
+	showOrHide(eventData: HoveredOverlayData | null) {
+		if (eventData) {
+			this.element.nativeElement.style.left = (eventData.x - 50) + 'px';
+			this.element.nativeElement.style.top = (eventData.y - 50) + 'px';
+			this.showFlag = true;
+		} else {
+			this.showFlag = false;
+		}
 	}
 }

43a016055c6f3509e2669010ba6b6404947a23cc
ramtob <ramtob@gmail.com>
2018-04-29 10:52:29 +0300

    Refactoring: removing index.ts files (cont.)

--- src/app/@ansyn/overlays/reducers/overlays.reducer.ts
+++ src/app/@ansyn/overlays/reducers/overlays.reducer.ts
@@ -5,8 +5,7 @@ import { OverlaysActions, OverlaysActionTypes } from '../actions/overlays.action
 import { createFeatureSelector, MemoizedSelector } from '@ngrx/store';
 import * as _ from 'lodash';
 import { ExtendMap } from '@ansyn/overlays/reducers/extendedMap.class';
-import { HoveredOverlayData } from '@ansyn/overlays';
-
+import { HoveredOverlayData } from '@ansyn/overlays/models/hovered-overlay-data.model';
 
 export interface TimelineRange {
 	start: Date;

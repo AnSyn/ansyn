8b77c58365671e8e11f37a9188c592428467ed8c
ramtob <ramtob@gmail.com>
2018-04-29 14:28:28 +0300

    Adding store-related unit tests

--- src/app/@ansyn/overlays/components/overlay-overview/overlay-overview.component.spec.ts
+++ src/app/@ansyn/overlays/components/overlay-overview/overlay-overview.component.spec.ts
@@ -1,16 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 
 import { OverlayOverviewComponent } from './overlay-overview.component';
+import { By } from '@angular/platform-browser';
+import { Store, StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { IOverlaysState, OverlayReducer, overlaysFeatureKey } from '@ansyn/overlays/reducers/overlays.reducer';
+import { ClearHoveredOverlay, SetHoveredOverlay } from '@ansyn/overlays/actions/overlays.actions';
 
-describe('OverlayOverviewComponent', () => {
+fdescribe('OverlayOverviewComponent', () => {
 	let component: OverlayOverviewComponent;
 	let fixture: ComponentFixture<OverlayOverviewComponent>;
+	let store: Store<any>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+			imports: [
+				StoreModule.forRoot({[overlaysFeatureKey]: OverlayReducer}),
+				EffectsModule.forRoot([])
+			],
 			declarations: [OverlayOverviewComponent]
 		})
 			.compileComponents();
+	}));
+
+	beforeEach(inject([Store], (_store: Store<IOverlaysState>) => {
+		store = _store;
 	}));
 
 	beforeEach(() => {
@@ -21,5 +35,26 @@ describe('OverlayOverviewComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	describe('Show or hide me according to store', () => {
+		let innerWrapper: any;
+		beforeEach(() => {
+			innerWrapper = fixture.debugElement.query(By.css('.overlay-overview'));
+		});
+		it('inner wrapper should exist', () => {
+			expect(innerWrapper).toBeDefined();
+		});
+		it ('should hide me by default', () => {
+			expect(innerWrapper.nativeElement.hidden).toBeTruthy();
+		});
+		it ('should show or hide me according to store', () => {
+			store.dispatch(new SetHoveredOverlay({id: '234'}));
+			fixture.detectChanges();
+			expect(innerWrapper.nativeElement.hidden).toBeFalsy();
+			store.dispatch(new ClearHoveredOverlay());
+			fixture.detectChanges();
+			expect(innerWrapper.nativeElement.hidden).toBeTruthy();
+		});
 	});
 });

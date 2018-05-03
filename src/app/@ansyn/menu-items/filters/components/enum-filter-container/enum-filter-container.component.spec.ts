1121d7b5e598f6c2483f3776444bf77351527361
Blaussh <shai.gm@gmail.com>
2018-05-03 13:02:50 +0300

    corrected the tests of EnumFilterContainerComponent

--- src/app/@ansyn/menu-items/filters/components/enum-filter-container/enum-filter-container.component.spec.ts
+++ src/app/@ansyn/menu-items/filters/components/enum-filter-container/enum-filter-container.component.spec.ts
@@ -3,17 +3,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SortPipe } from '../../pipes/sort.pipe';
 import { MapIteratorPipe } from '../../pipes/map-iterator.pipe';
 import { EnumFilterContainerComponent } from './enum-filter-container.component';
+import { ShowMorePipe } from '@ansyn/menu-items/filters/pipes/show-more.pipe';
+import { filtersConfig } from '@ansyn/menu-items/filters/services/filters.service';
 
 describe('EnumFilterContainerComponent', () => {
 	let component: EnumFilterContainerComponent;
 	let fixture: ComponentFixture<EnumFilterContainerComponent>;
 
 	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [EnumFilterContainerComponent, SortPipe, MapIteratorPipe]
-		})
-			.compileComponents();
-	}));
+			TestBed.configureTestingModule({
+				declarations: [EnumFilterContainerComponent, SortPipe, MapIteratorPipe, ShowMorePipe],
+				providers: [{ provide: filtersConfig, useValue: {} }]
+			}).compileComponents();
+		}
+	));
 
 	beforeEach(() => {
 		fixture = TestBed.createComponent(EnumFilterContainerComponent);

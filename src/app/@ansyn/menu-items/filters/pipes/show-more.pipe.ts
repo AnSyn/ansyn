52ce5db24e96f41463579ca8ad2fd4146424b88b
Blaussh <shai.gm@gmail.com>
2018-05-03 11:02:47 +0300

    code now use show-more pipe in order to choose filter list length

--- src/app/@ansyn/menu-items/filters/pipes/show-more.pipe.ts
+++ src/app/@ansyn/menu-items/filters/pipes/show-more.pipe.ts
@@ -1,12 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Inject, Pipe, PipeTransform } from '@angular/core';
+import { IFiltersConfig } from '@ansyn/menu-items/filters/models/filters-config';
+import { filtersConfig } from '@ansyn/menu-items/filters/services/filters.service';
 
 @Pipe({
 	name: 'showMore'
 })
 export class ShowMorePipe implements PipeTransform {
 
+	constructor(@Inject(filtersConfig) protected config: IFiltersConfig) {
+
+	}
+
 	transform(value: any, args?: any): any {
-		return value.slice(0, 5);
+		return args ? value : value.slice(0, this.config.shortFilterListLength);
 	}
 
 }

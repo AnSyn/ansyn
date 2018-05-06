cfae1a9fb03392221b0823c0a49c45b3040b4649
YairTawil <yairtawil@gmail.com>
2018-05-06 13:09:39 +0300

    feat(base-ol-map-source-provider): CR request changes

--- src/app/@ansyn/ansyn/app-providers/map-source-providers/open-layers-MapBox-source-provider.ts
+++ src/app/@ansyn/ansyn/app-providers/map-source-providers/open-layers-MapBox-source-provider.ts
@@ -4,21 +4,19 @@ import { Injectable } from '@angular/core';
 import { CacheService } from '@ansyn/imagery/cache-service/cache.service';
 import { Store } from '@ngrx/store';
 import { ImageryCommunicatorService } from '@ansyn/imagery/communicator-service/communicator.service';
-import { OpenlayersMapName } from '@ansyn/plugins/openlayers/open-layers-map/openlayers-map/openlayers-map';
-import { BaseMapSourceProvider } from '@ansyn/imagery/model/base-map-source-provider';
-import { DisabledOpenLayersMapName } from '@ansyn/plugins/openlayers/open-layers-map/openlayers-disabled-map/openlayers-disabled-map';
+import { OpenLayersMapSourceProvider } from '@ansyn/ansyn/app-providers/map-source-providers/open-layers.map-source-provider';
 
 export const OpenLayerMapBoxSourceProviderSourceType = 'MapBox';
 
 @Injectable()
-export class OpenLayerMapBoxSourceProvider extends BaseMapSourceProvider {
-	public supported =  [OpenlayersMapName, DisabledOpenLayersMapName];
+export class OpenLayerMapBoxSourceProvider extends OpenLayersMapSourceProvider {
 	public sourceType = OpenLayerMapBoxSourceProviderSourceType;
 
 	constructor(protected store: Store<any>, protected cacheService: CacheService,
 				protected imageryCommunicatorService: ImageryCommunicatorService) {
-		super(store, cacheService, imageryCommunicatorService)
+		super(store, cacheService, imageryCommunicatorService);
 	}
+
 	create(metaData: any): any[] {
 		const source = new XYZ({
 			url: metaData.imageUrl,

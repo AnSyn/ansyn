51aaffaf725792edf58a1fea7cf40b4b46832d4e
shaharido1 <shaharido1@gmail.com>
2018-05-07 17:50:51 +0300

    change hack fix

--- src/app/builder/ansynCustomComponent.ts
+++ src/app/builder/ansynCustomComponent.ts
@@ -1,20 +1,8 @@
 import { AnsynComponent } from '@ansyn/ansyn/ansyn/ansyn.component';
-import { AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 
-export class AnsynCustomComponent extends AnsynComponent implements AfterViewInit {
-	@ViewChild('idForBuilder') element: ElementRef;
+export class AnsynCustomComponent extends AnsynComponent {
 
-	ngAfterViewInit() {
-		setTimeout(() => {
-			if (this.element) {
-				this.element.nativeElement.click();
-			}
-
-		}, 3000);
-
-
-	}
 }
 
 

0d9ca64cbcc156b8c9c56e9c0e63bc3c26e7e0af
Blaussh <shai.gm@gmail.com>
2018-05-19 14:49:29 +0300

    corrected tests in select-case and status-bar

--- src/app/@ansyn/status-bar/components/status-bar/status-bar.component.spec.ts
+++ src/app/@ansyn/status-bar/components/status-bar/status-bar.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { StatusBarComponent } from './status-bar.component';
 import { Store, StoreModule } from '@ngrx/store';
-import { IStatusBarState } from '../../reducers/status-bar.reducer';
+import { IStatusBarState, StatusBarInitialState, statusBarStateSelector } from '../../reducers/status-bar.reducer';
 import { ExpandAction, UpdateStatusFlagsAction } from '../../actions/status-bar.actions';
 import { StatusBarModule } from '../../status-bar.module';
 import { EffectsModule } from '@ngrx/effects';
@@ -11,16 +11,112 @@ import { statusBarFlagsItemsEnum } from '../../models/status-bar-flag-items.mode
 import { GoAdjacentOverlay } from '@ansyn/core/actions/core.actions';
 import { comboBoxesOptions, GEO_FILTERS, ORIENTATIONS, TIME_FILTERS } from '../../models/combo-boxes.model';
 import { ALERTS } from '@ansyn/core/alerts/alerts.model';
+import { TreeviewModule } from 'ngx-treeview';
+import { casesConfig, CasesService } from '@ansyn/menu-items/cases/services/cases.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { CoreConfig } from '@ansyn/core/models/core.config';
+import { UrlSerializer } from '@angular/router';
+import { casesStateSelector } from '@ansyn/menu-items/cases/reducers/cases.reducer';
+import { cloneDeep } from 'lodash';
+import { Observable } from 'rxjs/Observable';
+import { Case, CaseTimeState } from '@ansyn/core/models/case.model';
+import { coreInitialState, coreStateSelector } from '@ansyn/core/reducers/core.reducer';
+import { OverlaysCriteria } from '@ansyn/core/models/overlay.model';
+import { provideMockActions } from '@ngrx/effects/testing';
 
 describe('StatusBarComponent', () => {
 	let component: StatusBarComponent;
 	let fixture: ComponentFixture<StatusBarComponent>;
-	let store: Store<IStatusBarState>;
+	let store: Store<any>;
+	let actions: Observable<any>;
+	let statusBarState: IStatusBarState;
+	const time: CaseTimeState = { type: 'absolute', from: new Date(0), to: new Date(0) };
+
+	let searchParams: OverlaysCriteria = {
+		region: {
+			'type': 'Polygon',
+			'coordinates': [
+				[
+					[
+						-14.4140625,
+						59.99349233206085
+					],
+					[
+						37.96875,
+						59.99349233206085
+					],
+					[
+						37.96875,
+						35.915747419499695
+					],
+					[
+						-14.4140625,
+						35.915747419499695
+					],
+					[
+						-14.4140625,
+						59.99349233206085
+					]
+				]
+			]
+		},
+		time: {
+			type: 'absolute',
+			from: new Date(2020),
+			to: new Date()
+		}
+	};
+	const caseItem: Case[] = [{
+		id: '1',
+		name: 'name',
+		owner: 'owner',
+		creationTime: new Date(),
+		lastModified: new Date(),
+		state: {
+			time: { type: '', from: new Date(), to: new Date() },
+			region: {
+				type: 'Polygon',
+				coordinates: [
+					[
+						[-64.73, 32.31],
+						[-80.19, 25.76],
+						[-66.09, 18.43],
+						[-64.73, 32.31]
+					]
+				]
+			},
+			maps: {
+				data: [
+					{
+						id: 'imagery1',
+						data: {
+							position: { zoom: 1, center: 2 },
+							isAutoImageProcessingActive: true,
+							overlay: 'overlay'
+						}
+					}
+				],
+				activeMapId: 'imagery1',
+				layout: 'layout6'
+			},
+			overlaysManualProcessArgs: {
+				'overlay_123': { Contrast: 50, Brightness: 20 }
+			}
+		} as any
+	}];
+	const casesState = { selectedCase: caseItem, cases: [caseItem], time };
+	const coreState = { ...coreInitialState, searchParams };
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			imports: [StoreModule.forRoot({}), EffectsModule.forRoot([]), StatusBarModule],
-			providers: [{ provide: LoggerConfig, useValue: {} },
+			imports: [StoreModule.forRoot({}), EffectsModule.forRoot([]), StatusBarModule, TreeviewModule.forRoot()],
+			providers: [
+				CasesService,
+				HttpClient,
+				HttpHandler,
+				UrlSerializer,
+				{ provide: CoreConfig, useValue: {} },
+				{ provide: LoggerConfig, useValue: {} },
 				{
 					provide: StatusBarConfig,
 					useValue: { toolTips: {}, dataInputFiltersConfig: { filters: [] } }
@@ -37,18 +133,38 @@ describe('StatusBarComponent', () => {
 					provide: GEO_FILTERS,
 					useValue: comboBoxesOptions.geoFilters
 				},
-				{ provide: ALERTS, useValue: [] }
+				{ provide: ALERTS, useValue: [] },
+				{ provide: casesConfig, useValue: { schema: null } },
+				provideMockActions(() => actions),
+				{
+					provide: CasesService,
+					useValue: {
+						defaultCase: { id: 'defualtId' }
+					}
+				}
 			]
 		})
 			.compileComponents();
 	}));
 
-	beforeEach(inject([Store], (_store: Store<IStatusBarState>) => {
+	beforeEach(inject([Store], (_store) => {
+		store = _store;
+		statusBarState = cloneDeep(StatusBarInitialState);
+		const fakeStore = new Map<any, any>([
+			[casesStateSelector, casesState],
+			[coreStateSelector, coreState],
+			[statusBarStateSelector, statusBarState]
+		]);
+
+		spyOn(store, 'select').and.callFake(type => Observable.of(fakeStore.get(type)));
+	}));
+
+	beforeEach(() => {
 		fixture = TestBed.createComponent(StatusBarComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
-		store = _store;
-	}));
+	});
+
 
 	it('should be created', () => {
 		expect(component).toBeTruthy();

94149286c3e65db1ca561b737d0f1449d71bef6a
ramtob <ramtob@gmail.com>
2018-05-24 17:29:40 +0300

    Importing inner changes

--- src/app/@ansyn/plugins/openlayers/visualizers/overlays/frame-visualizer.ts
+++ src/app/@ansyn/plugins/openlayers/visualizers/overlays/frame-visualizer.ts
@@ -4,26 +4,26 @@ import { Inject } from '@angular/core';
 import { IVisualizersConfig, VisualizersConfig } from '@ansyn/core/tokens/visualizers-config.token';
 import { Actions } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { DisplayOverlaySuccessAction, OverlaysActionTypes } from '@ansyn/overlays/actions/overlays.actions';
 import { IMapState, mapStateSelector } from '@ansyn/map-facade/reducers/map.reducer';
-import { BackToWorldView, CoreActionTypes } from '@ansyn/core/actions/core.actions';
 import { Overlay } from '@ansyn/core/models/overlay.model';
 import { OpenLayersMap } from '@ansyn/plugins/openlayers/open-layers-map/openlayers-map/openlayers-map';
 import { ImageryVisualizer } from '@ansyn/imagery/model/base-imagery-visualizer';
+import { MapFacadeService } from '@ansyn/map-facade/services/map-facade.service';
 
 @ImageryVisualizer({
 	supported: [OpenLayersMap],
 	deps: [Store, Actions, VisualizersConfig]
 })
 export class FrameVisualizer extends EntitiesVisualizer {
-	public markups: any[] = [];
 	public isActive = false;
 
-	drawFrameToOverLay$: Observable<DisplayOverlaySuccessAction> = this.actions$
-		.ofType<DisplayOverlaySuccessAction>(OverlaysActionTypes.DISPLAY_OVERLAY_SUCCESS)
-		.filter((action: DisplayOverlaySuccessAction) => action.payload.mapId === this.mapId)
-		.map(({ payload }: DisplayOverlaySuccessAction) => payload.overlay)
-		.mergeMap(this.setOverlay.bind(this));
+	drawFrameToOverLay$ = this.store$.select(mapStateSelector)
+		.filter(() => Boolean(this.mapId))
+		.map(({ mapsList }: IMapState) => MapFacadeService.mapById(mapsList, this.mapId))
+		.filter(Boolean)
+		.map((map) => map.data.overlay)
+		.distinctUntilChanged()
+		.switchMap(this.setOverlay.bind(this));
 
 	isActive$: Observable<boolean> = this.store$
 		.select(mapStateSelector)
@@ -33,15 +33,11 @@ export class FrameVisualizer extends EntitiesVisualizer {
 		.do((isActive) => this.isActive = isActive)
 		.do(this.purgeCache.bind(this));
 
-	removeOverlayFram$: Observable<any> = this.actions$
-		.ofType(CoreActionTypes.BACK_TO_WORLD_VIEW)
-		.filter((action: BackToWorldView) => action.payload.mapId === this.mapId)
-		.do(this.clearEntities.bind(this));
-
 	constructor(public store$: Store<any>,
 				public actions$: Actions,
 				@Inject(VisualizersConfig) config: IVisualizersConfig) {
 		super(config.FrameVisualizer);
+		this.type = 'Frame vis';
 		this.updateStyle({
 			opacity: 0.5,
 			initial: {
@@ -55,10 +51,16 @@ export class FrameVisualizer extends EntitiesVisualizer {
 		});
 	}
 
-	setOverlay({ id, footprint }: Overlay) {
-		const featureJson: GeoJSON.Feature<any> = { type: 'Feature', geometry: footprint, properties: {} };
-		const entityToDraw = { id, featureJson };
-		return this.setEntities([entityToDraw]);
+	setOverlay(overlay: Overlay) {
+		if (overlay) {
+			const { id, footprint } = overlay;
+			const featureJson: GeoJSON.Feature<any> = { type: 'Feature', geometry: footprint, properties: {} };
+			const entityToDraw = { id, featureJson };
+			return this.setEntities([entityToDraw]);
+		} else {
+			this.clearEntities();
+			return Observable.of(true);
+		}
 	}
 
 	getStroke() {
@@ -76,18 +78,11 @@ export class FrameVisualizer extends EntitiesVisualizer {
 		return;
 	}
 
-	onResetView(): Observable<boolean> {
-		this.clearEntities();
-		this.initLayers();
-		return Observable.of(true);
-	}
-
 	onInit() {
 		super.onInit();
 		this.subscriptions.push(
 			this.drawFrameToOverLay$.subscribe(),
-			this.isActive$.subscribe(),
-			this.removeOverlayFram$.subscribe()
+			this.isActive$.subscribe()
 		);
 	}
 }

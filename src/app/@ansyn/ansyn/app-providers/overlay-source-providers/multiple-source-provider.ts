f4f0cd93bb00b5f6b8509b0fdae683f37be9cf33
ramtob <ramtob@gmail.com>
2018-06-05 16:05:05 +0300

    Add lodash.compact to eliminate providers (like open-aerial) that do not yet support getStartDateViaLimitFacets() and getStartAndEndDateViaRangeFacets()

--- src/app/@ansyn/ansyn/app-providers/overlay-source-providers/multiple-source-provider.ts
+++ src/app/@ansyn/ansyn/app-providers/overlay-source-providers/multiple-source-provider.ts
@@ -8,6 +8,7 @@ import { Overlay, OverlaysFetchData } from '@ansyn/core/models/overlay.model';
 import { Feature, Polygon } from 'geojson';
 import { LoggerService } from '@ansyn/core/services/logger.service';
 import { area, intersect, difference } from '@turf/turf';
+import { compact } from 'lodash';
 
 export interface FiltersList {
 	name: string,
@@ -152,7 +153,7 @@ export class MultipleOverlaysSourceProvider extends BaseOverlaySourceProvider {
 	public getStartDateViaLimitFacets(params: { facets, limit, region }): Observable<StartAndEndDate> {
 		const startEnd = Promise.all(this.sourceConfigs
 			.map(s => s.provider.getStartDateViaLimitFacets(params).toPromise()))
-			.then(dates => dates.map(d =>
+			.then(dates => compact(dates).map(d =>
 				({ startDate: new Date(d.startDate), endDate: new Date(d.endDate) })))
 			.then(dates => dates.reduce((d1, d2) => {
 				if (!d1) {
@@ -171,7 +172,7 @@ export class MultipleOverlaysSourceProvider extends BaseOverlaySourceProvider {
 	public getStartAndEndDateViaRangeFacets(params: { facets, limitBefore, limitAfter, date, region }): Observable<any> {
 		const startEnd = Promise.all(this.sourceConfigs
 			.map(s => s.provider.getStartAndEndDateViaRangeFacets(params).toPromise()))
-			.then(dates => dates.map(d =>
+			.then(dates => compact(dates).map(d =>
 				({ startDate: new Date(d.startDate), endDate: new Date(d.endDate) })))
 			.then(dates => dates.reduce((d1, d2) => {
 				if (!d1) {

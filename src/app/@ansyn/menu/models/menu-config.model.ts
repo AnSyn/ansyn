747be71a2615afcf2d221e3940a884f961b026a7
YairTawil <yairtawil@gmail.com>
2018-06-10 16:18:50 +0300

    feat: forkMe on menu component (#1504)
    * feat: forkMe on menu component
    
    * Landsat8L1G and Sentinel2L1C sensors
    
    * fix: CR request changes
    
    * fix: builder
    
    * fix: builder
    
    * polyfills

--- src/app/@ansyn/menu/models/menu-config.model.ts
+++ src/app/@ansyn/menu/models/menu-config.model.ts
@@ -1,6 +1,14 @@
+export interface ForkMe {
+	active: boolean;
+	href?: string;
+	title: string;
+	image: string;
+}
+
 export interface IMenuConfig {
 	path: string;
 	color: string;
 	mode: string;
 	background: string;
+	forkMe: ForkMe;
 }

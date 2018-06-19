e7c62fc0ce9473b81a26ed575ad17eeb345bfd45
YairTawil <yairtawil@gmail.com>
2018-06-19 08:22:29 +0300

    test script 'singleRun' to 'watch=false'

--- src/app/app/app-routing.module.ts
+++ src/app/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { loginRoutes } from '@ansyn/login/login.routes';
 import { ansynRoutes } from '@ansyn/ansyn/ansyn.routes';
 
-export const routes: Routes = ansynRoutes.concat(loginRoutes);
+export const routes: Routes = [];
+ansynRoutes.forEach((route) => routes.push(route));
+loginRoutes.forEach((route) => routes.push(route));
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes, { useHash: true })],

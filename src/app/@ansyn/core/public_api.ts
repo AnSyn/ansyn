0fd26afb7e5cdca66979faa5db7661f58890440e
YairTawil <yairtawil@gmail.com>
2018-06-19 14:28:12 +0300

    fix: e2e script

---
+++ src/app/@ansyn/core/public_api.ts
@@ -0,0 +1,5 @@
+export { CoreModule } from './core/core.module';
+export { CaseMapExtent } from '@ansyn/core/models/case-map-position.model';
+export { CaseMapState } from '@ansyn/core/models/case.model';
+export { VisualizersConfig } from '@ansyn/core/tokens/visualizers-config.token';
+export { CaseMapPosition } from './core/models/case-map-position.model';

393fbcc144714b7fee9965fe8247a360526b487c
YairTawil <yairtawil@gmail.com>
2018-06-19 14:36:22 +0300

    rmv: core public api

--- src/app/@ansyn/core/public_api.ts
+++
@@ -1,5 +0,0 @@
-export { CoreModule } from './core/core.module';
-export { CaseMapExtent } from '@ansyn/core/models/case-map-position.model';
-export { CaseMapState } from '@ansyn/core/models/case.model';
-export { VisualizersConfig } from '@ansyn/core/tokens/visualizers-config.token';
-export { CaseMapPosition } from './core/models/case-map-position.model';

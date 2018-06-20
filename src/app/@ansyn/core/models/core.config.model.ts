89ae5d2696ff8acfc84599da8659f7a0aae5ce3e
ramtob <ramtob@gmail.com>
2018-06-20 14:43:27 +0300

    Moving parameters to config file

--- src/app/@ansyn/core/models/core.config.model.ts
+++ src/app/@ansyn/core/models/core.config.model.ts
@@ -1,9 +1,16 @@
 import { IStorageConfig } from "../services/storage/config";
 
+export interface IMapSearchConfig {
+	active: boolean;
+	url: string;
+	apiKey: string;
+}
+
 export interface ICoreConfig {
 	storageService: IStorageConfig;
 	welcomeNotification: {
 		headerText: string;
 		mainText: string;
 	};
+	mapSearch: IMapSearchConfig;
 }
--- src/app/@ansyn/core/services/geocoder.service.ts
+++ src/app/@ansyn/core/services/geocoder.service.ts
@@ -1,18 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ErrorHandlerService } from '@ansyn/core/services/error-handler.service';
 import { Observable } from 'rxjs/Observable';
 import { Point } from 'geojson';
+import { ICoreConfig, IMapSearchConfig } from '@ansyn/core/models/core.config.model';
+import { CoreConfig } from '@ansyn/core/models/core.config';
 
 @Injectable()
 export class GeocoderService {
 
+	public config: IMapSearchConfig = null;
+
 	constructor(protected http: HttpClient,
-				protected errorHandlerService: ErrorHandlerService) {
+				protected errorHandlerService: ErrorHandlerService,
+				@Inject(CoreConfig) public packageConfig: ICoreConfig) {
+		this.config = this.packageConfig.mapSearch;
 	}
 
 	getLocation$(searchString): Observable<any> {
-		return this.http.get<any>(`http://dev.virtualearth.net/REST/v1/Locations?locality=${searchString}&maxResults=1&key=AofqJGk-uEej_Pv7hkbpXh6H3pb54luGZj6lfe90ksZEoBY-JmgTwGE2s9TF6peC`)
+		const url = this.config.url.replace('$searchString', searchString).replace('$apiKey', this.config.apiKey);
+		return this.http.get<any>(url)
 			.map(res => res.resourceSets[0].resources[0])
 			.filter(Boolean)
 			.map(res => res.point)
@@ -20,7 +27,6 @@ export class GeocoderService {
 			.catch((error: Response | any) => {
 				return this.errorHandlerService.httpErrorHandle(error);
 			});
-		// return  this.http.get<any>(`${this.path}${this.resFormat}?address=${address}&language=en&key=${this.googleApiKey}`);
 	}
 
 }

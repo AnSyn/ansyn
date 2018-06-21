ca5d26f4f6bda62b8239dd155274aae480150c21
ramtob <ramtob@gmail.com>
2018-06-21 10:52:45 +0300

    Adding unsubscribe() to subscriptions

--- src/app/@ansyn/map-facade/components/map-search-box/map-search-box.component.ts
+++ src/app/@ansyn/map-facade/components/map-search-box/map-search-box.component.ts
@@ -1,20 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { CommunicatorEntity } from '@ansyn/imagery/communicator-service/communicator.entity';
 import { ImageryCommunicatorService } from '@ansyn/imagery/communicator-service/communicator.service';
 import { GeocoderService } from '@ansyn/core/services/geocoder.service';
 import { Point } from 'geojson';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
 	selector: 'ansyn-map-search-box',
 	templateUrl: './map-search-box.component.html',
 	styleUrls: ['./map-search-box.component.less']
 })
-export class MapSearchBoxComponent {
+export class MapSearchBoxComponent implements OnDestroy {
 	@Input() mapId: string;
 
 	searchString: string;
 	communicator: CommunicatorEntity;
-
+	private _subscriptions: Subscription[] = [];
 	public error: boolean;
 
 	constructor(protected imageryCommunicatorService: ImageryCommunicatorService,
@@ -32,19 +33,26 @@ export class MapSearchBoxComponent {
 	}
 
 	onSubmit() {
-		if (!this.searchString) { return; }
+		if (!this.searchString) {
+			return;
+		}
 		this.communicator = this.imageryCommunicatorService.provide(this.mapId);
-		this.geocoderService.getLocation$(this.searchString)
-			.do((point: Point) => {
-				if (point) {
-					this.communicator.setCenter(point).take(1).subscribe()
-				} else {
-					this.error = true;
-				}
-			}
-			)
-			.take(1).subscribe()
+		this._subscriptions.push(
+			this.geocoderService.getLocation$(this.searchString)
+				.do((point: Point) => {
+						if (point) {
+							this._subscriptions.push(this.communicator.setCenter(point).take(1).subscribe())
+						} else {
+							this.error = true;
+						}
+					}
+				)
+				.take(1).subscribe())
 		;
+	}
+
+	ngOnDestroy(): void {
+		this._subscriptions.forEach(sub => sub.unsubscribe());
 	}
 
 }

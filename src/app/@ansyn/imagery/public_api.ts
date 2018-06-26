7fb28a8946e8c3633c366d3616c685ed75be79a6
YairTawil <yairtawil@gmail.com>
2018-06-26 18:44:35 +0300

    merging master

--- src/app/@ansyn/imagery/public_api.ts
+++ src/app/@ansyn/imagery/public_api.ts
@@ -5,6 +5,4 @@ export { CommunicatorEntity } from './communicator-service/communicator.entity';
 export { ProjectionService } from './projection-service/projection.service';
 export { BaseImageryPlugin } from './model/base-imagery-plugin';
 export { IMap } from './model/imap';
-export { ImageryMapComponent } from './model/imagery-map-component';
-export { BaseImageryPluginProvider, ProvideMap } from './imagery/providers/imagery.providers';
 export { ImageryModule } from './imagery.module';

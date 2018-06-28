60c389d9828d43cccb81d09dc729ae0b68174af3
ramtob <ramtob@gmail.com>
2018-06-28 16:01:49 +0300

    Updating unit test

--- src/app/@ansyn/menu-items/help/components/help.component.spec.ts
+++ src/app/@ansyn/menu-items/help/components/help.component.spec.ts
@@ -1,35 +1,34 @@
-import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HelpComponent } from './help.component';
-import { Store, StoreModule } from '@ngrx/store';
-import { ICoreState } from '@ansyn/core/reducers/core.reducer';
 import { By } from '@angular/platform-browser';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { EffectsModule } from '@ngrx/effects';
-import { menuFeatureKey, MenuReducer } from '@ansyn/menu/reducers/menu.reducer';
-import { CoreModule } from '@ansyn/core/core.module';
 import { AnsynCheckboxComponent } from '@ansyn/core/components/ansyn-checkbox/ansyn-checkbox.component';
+import { HelpLocalStorageService } from '@ansyn/menu-items/help/services/help.local-storage.service';
+import { MockComponent } from '@ansyn/core/test/mock-component';
 
 describe('HelpComponent', () => {
 	let component: HelpComponent;
 	let fixture: ComponentFixture<HelpComponent>;
-	let store: Store<any>;
+
+	const mockCarousel = MockComponent({ selector: 'carousel', inputs: ['interval'] });
+	const mockSlide = MockComponent({ selector: 'slide' });
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			imports: [
-				StoreModule.forRoot({[menuFeatureKey]: MenuReducer}),
-				EffectsModule.forRoot([]),
-				CarouselModule.forRoot(),
-				CoreModule
+			declarations: [HelpComponent,
+				AnsynCheckboxComponent,
+				mockCarousel,
+				mockSlide
 			],
-			declarations: [HelpComponent]
+			providers: [
+				{
+					provide: HelpLocalStorageService, useValue: {
+						setHelpLocalStorageData: () => {},
+						getHelpLocalStorageData: () => ({})
+					}
+				}
+			]
 		})
 			.compileComponents();
-	}));
-
-	beforeEach(inject([Store], (_store: Store<ICoreState>) => {
-		store = _store;
 	}));
 
 	beforeEach(() => {
@@ -45,9 +44,9 @@ describe('HelpComponent', () => {
 	describe('checkbox click event', () => {
 		it('should invoke a call to the store', () => {
 			let checkboxElement = fixture.debugElement.query(By.directive(AnsynCheckboxComponent));
-			spyOn(store, 'dispatch');
-			checkboxElement.triggerEventHandler('inputClicked', {data: { isChecked: true }});
-			expect(store.dispatch).toHaveBeenCalled();
+			spyOn(component.helpLocalStorageService, 'setHelpLocalStorageData');
+			checkboxElement.triggerEventHandler('inputClicked', { data: { isChecked: true } });
+			expect(component.helpLocalStorageService.setHelpLocalStorageData).toHaveBeenCalledWith({ dontShowHelpOnStartup: true });
 		});
 	});
 });

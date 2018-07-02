d757e16c535eb64d643f8d2aac7f60d945f442e1
YairTawil <yairtawil@gmail.com>
2018-07-02 11:34:23 +0300

    add description for map-source-providers functions

--- src/app/@ansyn/imagery/providers/map-source-providers.ts
+++ src/app/@ansyn/imagery/providers/map-source-providers.ts
@@ -5,14 +5,23 @@ import {
 } from '../model/base-map-source-provider';
 import { FactoryProvider, Provider } from '@angular/core';
 
+/**
+ * @description Factory for Merge an array of arrays to single array ( BaseMapSourceProvider[] )
+ */
 export function BaseMapSourceProviderFactory(mapSourceProviders) {
 	return mapSourceProviders.reduce((a, mapSourceProvider) => [...a, ...mapSourceProvider], []);
 }
 
+/**
+ * @description Enable to get single array of BaseMapSourceProvider[] for multi array
+ */
 export function MapSourceProvidersFactory(...mapSourceProviders) {
 	return mapSourceProviders;
 }
 
+/**
+ * @description Factory for Merge an array of arrays ( BaseMapSourceProvider )
+ */
 export function createMapSourceProviders(mapSourceProviders: BaseMapSourceProviderConstructor[]): Provider[] {
 	return [
 		mapSourceProviders,
@@ -24,10 +33,6 @@ export function createMapSourceProviders(mapSourceProviders: BaseMapSourceProvid
 		}
 	];
 }
-
-/**
- @description Factory provider for all BaseMapSourceProvider arrays(IMAGERY_MAP_SOURCE_PROVIDERS)
- */
 
 export const BaseMapSourceProviderProvider: FactoryProvider = {
 	provide: BaseMapSourceProvider,

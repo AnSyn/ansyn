5d42c2c5ca8914711bd6fa279caf980080403561
asafMasa <asaf.masa@gmail.com>
2018-07-04 10:33:10 +0300

    remove login from e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,19 +9,19 @@ describe('Ansyn App', () => {
 		page.navigateTo();
 	});
 
-	it('should initialize app with login', () => {
-		expect(page.loginComponent.isPresent()).toBe(true);
-	});
-
-	it('should instantiate the main app component, after a successful login', () => {
-		page.setCorrectUsername();
-		page.setCorrectPassword();
-		page.loginButton.click();
-		browser.wait(
-			() => page.mainComponent.isPresent(),
-			null,
-			'Timed out waiting for main component'
-		);
+	it('should initialize app with main app', () => {
 		expect(page.mainComponent.isPresent()).toBe(true);
 	});
+
+	// it('should instantiate the main app component, after a successful login', () => {
+	// 	page.setCorrectUsername();
+	// 	page.setCorrectPassword();
+	// 	page.loginButton.click();
+	// 	browser.wait(
+	// 		() => page.mainComponent.isPresent(),
+	// 		null,
+	// 		'Timed out waiting for main component'
+	// 	);
+	// 	expect(page.mainComponent.isPresent()).toBe(true);
+	// });
 });

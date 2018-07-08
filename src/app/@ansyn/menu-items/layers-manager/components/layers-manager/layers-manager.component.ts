c9ea62057602bea6bf4274b61f55816298e95fd7
YairTawil <yairtawil@gmail.com>
2018-07-08 17:45:50 +0300

    feat(annotations-layer): download annotation layers as json (#1577)
    * feat(annotations-layer): download annotation layers as json
    
    * remove console.log

--- src/app/@ansyn/menu-items/layers-manager/components/layers-manager/layers-manager.component.ts
+++ src/app/@ansyn/menu-items/layers-manager/components/layers-manager/layers-manager.component.ts
@@ -1,11 +1,18 @@
-import { ILayerState, selectLayers } from '@ansyn/menu-items/layers-manager/reducers/layers.reducer';
-import { Component, OnInit } from '@angular/core';
+import {
+	ILayerState,
+	selectDisplayAnnotationsLayer,
+	selectLayers
+} from '@ansyn/menu-items/layers-manager/reducers/layers.reducer';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { layersStateSelector } from '../../reducers/layers.reducer';
+import { select, Store } from '@ngrx/store';
 import { ILayer } from '@ansyn/menu-items/layers-manager/models/layers.model';
 import { ToggleDisplayAnnotationsLayer } from '@ansyn/menu-items/layers-manager/actions/layers.actions';
 import { groupBy } from 'lodash';
+import { saveAs } from 'file-saver';
+import { map } from 'rxjs/internal/operators';
+import { selectAnnotationLayer } from '@ansyn/menu-items/tools/reducers/tools.reducer';
+import { tap } from 'rxjs/operators';
 
 
 @Component({
@@ -14,29 +21,54 @@ import { groupBy } from 'lodash';
 	styleUrls: ['./layers-manager.component.less']
 })
 
-export class LayersManagerComponent implements OnInit {
+export class LayersManagerComponent implements OnInit, OnDestroy {
 	annotationLayerChecked;
+	annotationsLayer;
+	subscriptions = [];
 
-	public layers$: Observable<any> = this.store.select(selectLayers)
-		.map((layers: ILayer[]) => {
-			const typeGroupedLayers = groupBy(layers, l => l.type);
-			return Object.keys(typeGroupedLayers).map(layer => typeGroupedLayers[layer]);
-		});
+	public layers$: Observable<any> = this.store
+		.pipe(
+			select(selectLayers),
+			map((layers: ILayer[]) => {
+				const typeGroupedLayers = groupBy(layers, l => l.type);
+				return Object.keys(typeGroupedLayers).map(layer => typeGroupedLayers[layer]);
+			})
+		);
+
+	annotationsLayer$: Observable<any> = this.store
+		.pipe(
+			select(selectAnnotationLayer),
+			tap((annotationsLayer) => this.annotationsLayer = annotationsLayer)
+		);
+
+	displayAnnotationsLayer$ = this.store
+		.pipe(
+			select(selectDisplayAnnotationsLayer),
+			tap(result => this.annotationLayerChecked = result)
+		);
 
 	constructor(protected store: Store<ILayerState>) {
 	}
 
 	ngOnInit() {
-		this.store.select<ILayerState>(layersStateSelector)
-			.pluck<ILayerState, boolean>('displayAnnotationsLayer')
-			.subscribe(result => {
-				this.annotationLayerChecked = result;
-			});
-		this.layers$.subscribe();
+		this.subscriptions.push(
+			this.layers$.subscribe(),
+			this.annotationsLayer$.subscribe(),
+			this.displayAnnotationsLayer$.subscribe()
+		);
+	}
+
+	ngOnDestroy() {
+		this.subscriptions.forEach((sub) => sub.unsubscribe());
 	}
 
 	annotationLayerClick() {
 		this.store.dispatch(new ToggleDisplayAnnotationsLayer(!this.annotationLayerChecked));
+	}
+
+	downloadAnnotations() {
+		const blob = new Blob([JSON.stringify(this.annotationsLayer)], { type: 'application/json' });
+		saveAs(blob, 'annotations.json');
 	}
 
 }

37ad846232492ab810ebf3b44e91acfc840a3842
YairTawil <yairtawil@gmail.com>
2018-07-16 12:25:45 +0300

    fix: CR

--- src/app/@ansyn/core/components/ansyn-modal/ansyn-modal.component.ts
+++ src/app/@ansyn/core/components/ansyn-modal/ansyn-modal.component.ts
@@ -1,35 +1,11 @@
-import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
-import { animate, style, transition, trigger } from '@angular/animations';
-
-const animationsDuring = '0.5s';
-
-const animations: any[] = [
-	trigger('content', [
-		transition(':enter', [style({
-			'backgroundColor': '#27b2cf',
-			transform: 'translate(0, -100%)'
-		}), animate(animationsDuring, style({ 'backgroundColor': 'white', transform: 'translate(0, 0)' }))]),
-		transition(':leave', [style({
-			'backgroundColor': 'white',
-			transform: 'translate(0, 0)'
-		}), animate(animationsDuring, style({ 'backgroundColor': '#27b2cf', transform: 'translate(0, -100%)' }))])
-	])
-];
+import { Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 
 @Component({
 	selector: 'ansyn-modal',
 	templateUrl: './ansyn-modal.component.html',
-	styleUrls: ['./ansyn-modal.component.less'],
-	animations
+	styleUrls: ['./ansyn-modal.component.less']
 })
-export class AnsynModalComponent implements OnInit {
+export class AnsynModalComponent {
 	@Input() @HostBinding('class.show') show: boolean;
 	@Output() showChange = new EventEmitter<boolean>();
-
-	constructor() {
-	}
-
-	ngOnInit() {
-	}
-
 }

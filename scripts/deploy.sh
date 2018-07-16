b4ccfd42e46b529398387e96159655f530123ba6
asafMasa <asaf.masa@gmail.com>
2018-07-16 12:59:42 +0300

    fix travis to restart messi service

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,6 +24,14 @@ then
     exit 1
 fi
 
+service2=$4
+echo "service2 $service2"
+if [ -z "$service2" ]
+then
+    echo "Missing ECS service2 name"
+    exit 1
+fi
+
 echo "start deploying version $version on target $target"
 
 eval $(aws ecr get-login --no-include-email --region us-west-2)
@@ -41,3 +49,5 @@ docker push "223455578796.dkr.ecr.us-west-2.amazonaws.com/$target:$version"
 echo "deployment succeeded";
 
 node scripts/kill-tasks.js $service
+
+node scripts/kill-tasks.js $service2

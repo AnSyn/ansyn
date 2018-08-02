78eddb356bc60e30dfa820d20a5faa877bbd732f
YairTawil <yairtawil@gmail.com>
2018-08-02 16:34:13 +0300

    feat: color picker opacity

---
+++ src/app/@ansyn/menu-items/tools/components/color-picker/color-picker.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { UUID } from 'angular2-uuid';
+
+@Component({
+	selector: 'ansyn-color-picker',
+	templateUrl: './color-picker.component.html',
+	styleUrls: ['./color-picker.component.less']
+})
+export class ColorPickerComponent {
+	id = UUID.UUID();
+	@Input() color: string;
+	@Output() colorChange = new EventEmitter();
+	@Input() label: string;
+	@Input() shown: boolean;
+	@Output() shownChange = new EventEmitter();
+
+}

6a642787a0bce77f9d5f2bdaff5c168abd39dc9d
ramtob <ramtob@gmail.com>
2018-08-08 14:22:28 +0300

    Add catchError(empty)

--- src/app/@ansyn/context/services/context.service.ts
+++ src/app/@ansyn/context/services/context.service.ts
@@ -1,10 +1,11 @@
 import { Inject, Injectable } from '@angular/core';
 import { ContextConfig } from '../models/context.config';
 import { IContextConfig } from '../models/context.config.model';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 import { StorageService } from '@ansyn/core/services/storage/storage.service';
 import { ErrorHandlerService } from '@ansyn/core/services/error-handler.service';
 import { IContext } from '@ansyn/core/models/context.model';
+import { catchError } from 'rxjs/internal/operators';
 
 @Injectable()
 export class ContextService {
@@ -16,7 +17,11 @@ export class ContextService {
 
 	loadContexts(): Observable<any> {
 		return this.storageService.getPage<any>(this.config.schema, 0, 100)
-			.catch(err => this.errorHandlerService.httpErrorHandle(err));
+			.catch(err => this.errorHandlerService.httpErrorHandle(err))
+			.pipe(
+				catchError(() => EMPTY)
+			)
+			;
 	}
 
 	loadContext(selectedContextId: string): Observable<IContext> {

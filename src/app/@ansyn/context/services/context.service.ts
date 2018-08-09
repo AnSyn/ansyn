7d26dfe82e68f6ad6b0fb75489a9903c63428197
Ram Tobolski <ramtob@users.noreply.github.com>
2018-08-09 12:14:11 +0300

    Close icon tweaks (#1735)

--- src/app/@ansyn/context/services/context.service.ts
+++ src/app/@ansyn/context/services/context.service.ts
@@ -1,11 +1,12 @@
 import { Inject, Injectable } from '@angular/core';
 import { ContextConfig } from '../models/context.config';
 import { IContextConfig } from '../models/context.config.model';
-import { EMPTY, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { StorageService } from '@ansyn/core/services/storage/storage.service';
 import { ErrorHandlerService } from '@ansyn/core/services/error-handler.service';
 import { IContext } from '@ansyn/core/models/context.model';
 import { catchError } from 'rxjs/internal/operators';
+import { rxPreventCrash } from '@ansyn/core/utils/rxjs-operators/rxPreventCrash';
 
 @Injectable()
 export class ContextService {
@@ -17,17 +18,16 @@ export class ContextService {
 
 	loadContexts(): Observable<any> {
 		return this.storageService.getPage<any>(this.config.schema, 0, 100)
-			.catch(err => this.errorHandlerService.httpErrorHandle(err))
 			.pipe(
-				catchError(() => EMPTY)
-			)
-			;
+				catchError(err => this.errorHandlerService.httpErrorHandle(err)),
+				rxPreventCrash()
+			);
 	}
 
 	loadContext(selectedContextId: string): Observable<IContext> {
 		return this.storageService.get<IContext, IContext>(this.config.schema, selectedContextId)
 			.map(storedEntity =>
-				this.parseContext({...storedEntity.preview, ...storedEntity.data}))
+				this.parseContext({ ...storedEntity.preview, ...storedEntity.data }))
 			.catch(err => this.errorHandlerService.httpErrorHandle(err));
 	}
 

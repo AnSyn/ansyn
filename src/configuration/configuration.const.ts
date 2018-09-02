7550633236471ea8a6dd498260f8da8e26f1b516
YairTawil <yairtawil@gmail.com>
2018-09-02 10:06:13 +0300

    fix: aot compilation

--- src/configuration/configuration.const.ts
+++ src/configuration/configuration.const.ts
@@ -1,13 +1,17 @@
-import { FiltersCollectionComponent } from '@ansyn/menu-items/filters/components/filters-collection/filters-collection.component';
-import { ToolsComponent } from '@ansyn/menu-items/tools/tools/tools.component';
-import { LayersManagerComponent } from '@ansyn/menu-items/layers-manager/components/layers-manager/layers-manager.component';
-import { HelpComponent } from '@ansyn/menu-items/help/components/help.component';
 import { CasesComponent } from '@ansyn/menu-items/cases/components/cases/cases.component';
-import { configuration as constConfiguration } from './configuration.const';
+import { FiltersCollectionComponent } from '@ansyn/menu-items/filters/components/filters-collection/filters-collection.component';
+import { LayersManagerComponent } from '@ansyn/menu-items/layers-manager/components/layers-manager/layers-manager.component';
+import { ToolsComponent } from '@ansyn/menu-items/tools/tools/tools.component';
+import { AlgorithmsComponent } from '@ansyn/menu-items/algorithms/algorithms/algorithms.component';
+import { SettingsComponent } from '@ansyn/menu-items/settings/settings/settings.component';
+import { HelpComponent } from '@ansyn/menu-items/help/components/help.component';
 
 export const configuration = {
-	...constConfiguration,
-	production: true,
+	production: false,
+	configPath: 'assets/config/app.config.json',
+	overlays: {
+		overlayOverviewFailed: 'assets/icons/preview-failed-to-find-overlay-preview.svg'
+	},
 	ansynMenuItems: [
 		{
 			name: 'Cases',
@@ -29,10 +33,21 @@ export const configuration = {
 			component: ToolsComponent,
 			iconClass: 'icon-main-tools'
 		},
+
+		{
+			name: 'Algorithms',
+			component: AlgorithmsComponent,
+			iconClass: 'icon-main-algorithms'
+		},
+		{
+			name: 'Settings',
+			component: SettingsComponent,
+			iconClass: 'icon-main-settings'
+		},
 		{
 			name: 'Help',
 			component: HelpComponent,
 			iconClass: 'icon-help-settings'
 		}
-	],
+	]
 };
--- src/configuration/const/configuration.const.ts
+++
@@ -1,6 +0,0 @@
-export const constConfigurations = {
-	configPath: 'assets/config/app.config.json',
-	overlays: {
-		overlayOverviewFailed: 'assets/icons/preview-failed-to-find-overlay-preview.svg'
-	}
-};

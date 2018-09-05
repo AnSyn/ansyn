a03ce82240dd66a3fcdc228e4a0e36ca0413355b
YairTawil <yairtawil@gmail.com>
2018-09-05 10:46:27 +0300

    fix: types of tests

--- src/app/@ansyn/menu-items/filters/pipes/map-iterator.pipe.spec.ts
+++ src/app/@ansyn/menu-items/filters/pipes/map-iterator.pipe.spec.ts
@@ -12,8 +12,7 @@ describe('MapIteratorPipe', () => {
 		let map: Map<string, string> = new Map<string, string>();
 		map.set('1', 'one');
 		map.set('2', 'two');
-
-		expect(mapIteratorPipe.transform(map)).toEqual([{ key: '1', value: 'one' }, { key: '2', value: 'two' }]);
+		expect(mapIteratorPipe.transform(map)).toEqual(<any>[{ key: '1', value: 'one' }, { key: '2', value: 'two' }]);
 	});
 
 });

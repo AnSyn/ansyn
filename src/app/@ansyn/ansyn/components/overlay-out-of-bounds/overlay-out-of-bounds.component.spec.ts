415364f2af3a6b45449f0975427d6ef58681ae03
YairTawil <yairtawil@gmail.com>
2018-10-10 15:44:44 +0300

    add unit testing for out of bounds components

--- src/app/@ansyn/ansyn/components/overlay-out-of-bounds/overlay-out-of-bounds.component.spec.ts
+++ src/app/@ansyn/ansyn/components/overlay-out-of-bounds/overlay-out-of-bounds.component.spec.ts
@@ -1,13 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OverlayOutOfBoundsComponent } from './overlay-out-of-bounds.component';
+import { ImageryCommunicatorService } from '@ansyn/imagery';
+import { StoreModule } from '@ngrx/store';
+import { mapFeatureKey, MapReducer } from '@ansyn/map-facade';
+import { By } from '@angular/platform-browser';
 
-fdescribe('OverlayOutOfBoundsComponent', () => {
+describe('OverlayOutOfBoundsComponent', () => {
 	let component: OverlayOutOfBoundsComponent;
 	let fixture: ComponentFixture<OverlayOutOfBoundsComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
+			imports: [
+				StoreModule.forRoot({ [mapFeatureKey]: MapReducer })
+			],
+			providers: [ImageryCommunicatorService],
 			declarations: [OverlayOutOfBoundsComponent]
 		})
 			.compileComponents();
@@ -22,4 +30,12 @@ fdescribe('OverlayOutOfBoundsComponent', () => {
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('click on button should call "backToExtent"', () => {
+		spyOn(component, 'backToExtent');
+		const button = fixture.debugElement.query(By.css('button'));
+		button.triggerEventHandler('click', {});
+		expect(component.backToExtent).toHaveBeenCalled();
+	});
+
 });

980ad5a30332047955a89f70d1196c3d0faa2e1e
asafMasa <asaf.masa@gmail.com>
2018-10-21 14:23:23 +0300

    don't calc north on world view

--- src/app/@ansyn/plugins/openlayers/north-calculations/north-calculations.plugin.ts
+++ src/app/@ansyn/plugins/openlayers/north-calculations/north-calculations.plugin.ts
@@ -4,7 +4,7 @@ import {
 	BackToWorldView,
 	CaseOrientation,
 	CoreActionTypes,
-	ICaseMapPosition,
+	ICaseMapPosition, ICaseMapState,
 	IOverlay,
 	LoggerService,
 	toDegrees
@@ -23,16 +23,10 @@ import {
 import { select, Store } from '@ngrx/store';
 import { BaseImageryPlugin, CommunicatorEntity, ImageryPlugin, ProjectionService } from '@ansyn/imagery';
 import { comboBoxesOptions, IStatusBarState, statusBarStateSelector } from '@ansyn/status-bar';
-import { selectActiveMapId } from '@ansyn/map-facade';
+import { IMapState, MapFacadeService, mapStateSelector, selectActiveMapId } from '@ansyn/map-facade';
 import { AutoSubscription } from 'auto-subscriptions';
 import { OpenLayersMap } from '../open-layers-map/openlayers-map/openlayers-map';
-import { catchError, filter, map, mergeMap, switchMap, take, tap, withLatestFrom } from 'rxjs/operators';
-
-export interface INorthData {
-	northOffsetDeg: number;
-	northOffsetRad: number;
-	actualNorth: number;
-}
+import { catchError, distinctUntilChanged, filter, map, mergeMap, switchMap, take, tap, withLatestFrom } from 'rxjs/operators';
 
 @ImageryPlugin({
 	supported: [OpenLayersMap],
@@ -43,6 +37,24 @@ export class NorthCalculationsPlugin extends BaseImageryPlugin {
 	isEnabled = true;
 
 	protected thresholdDegrees = 0.1;
+
+	@AutoSubscription
+	baseMap$ = this.store$
+		.pipe(
+			select(mapStateSelector),
+			map(({ mapsList }: IMapState) => MapFacadeService.mapById(mapsList, this.mapId)),
+			filter(Boolean),
+			map((map: ICaseMapState) => map.data.overlay),
+			distinctUntilChanged(),
+			tap((overlay) => {
+				if (Boolean(overlay)) {
+					this.isEnabled = true;
+				} else {
+					this.isEnabled = false;
+					this.communicator.setVirtualNorth(0);
+				}
+			})
+		);
 
 	@AutoSubscription
 	hoveredOverlayPreview$: Observable<any> = this.store$.select(selectHoveredOverlay).pipe(
@@ -105,6 +117,7 @@ export class NorthCalculationsPlugin extends BaseImageryPlugin {
 		tap((position: ICaseMapPosition) => {
 			if (this.isEnabled && position) {
 				this.getVirtualNorth().pipe(take(1)).subscribe((virtualNorth: number) => {
+					// console.log(`after position chenged virtualNorth= '${toDegrees(virtualNorth)}'`);
 					this.communicator.setVirtualNorth(virtualNorth);
 				});
 			}
@@ -132,14 +145,29 @@ export class NorthCalculationsPlugin extends BaseImageryPlugin {
 	getVirtualNorth() {
 		return this.getProjectedCenters().pipe(
 			map((projectedCenters: Point[]): number => {
+				// console.log(`----------------------------------------`);
 				const projectedCenterView = projectedCenters[0].coordinates;
 				const projectedCenterViewWithOffset = projectedCenters[1].coordinates;
 				const northOffsetRad = Math.atan2((projectedCenterViewWithOffset[0] - projectedCenterView[0]), (projectedCenterViewWithOffset[1] - projectedCenterView[1]));
+				// console.log(`northOffset= '${toDegrees(northOffsetRad)}'`);
 				const northRad = northOffsetRad * -1;
-				if (this.thresholdDegrees > Math.abs(toDegrees(this.communicator.getRotation() - northRad))) {
+				const communicatorRad = this.communicator.getRotation();
+				let currentRotationDegrees = toDegrees(communicatorRad);
+				if (currentRotationDegrees < 0) {
+					currentRotationDegrees = 360 + currentRotationDegrees;
+				}
+				currentRotationDegrees = currentRotationDegrees % 360;
+				// console.log(`mapRotationDegrees= '${currentRotationDegrees}'`);
+				let northDeg = toDegrees(northRad);
+				if (northDeg < 0) {
+					northDeg = 360 + northDeg;
+				}
+				northDeg = northDeg % 360;
+				// console.log(`calcedNorthDegrees= '${toDegrees(northRad)}'`);
+				if (this.thresholdDegrees > Math.abs(currentRotationDegrees - northDeg)) {
 					return 0;
 				}
-				return this.communicator.getRotation() - northRad;
+				return (this.communicator.getRotation() - northRad) % (Math.PI * 2);
 			}),
 			catchError(() => of(0))
 		);

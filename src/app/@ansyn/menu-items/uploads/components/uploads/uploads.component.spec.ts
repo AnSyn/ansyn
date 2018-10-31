d82c6cc84b74b1af01fbe918fe3fa9e2365fd28d
YairTawil <yairtawil@gmail.com>
2018-10-31 10:11:05 +0200

    lint

--- src/app/@ansyn/menu-items/uploads/components/uploads/uploads.component.spec.ts
+++ src/app/@ansyn/menu-items/uploads/components/uploads/uploads.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UploadsComponent } from './uploads.component';
 
 describe('UploadsComponent', () => {
-  let component: UploadsComponent;
-  let fixture: ComponentFixture<UploadsComponent>;
+	let component: UploadsComponent;
+	let fixture: ComponentFixture<UploadsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UploadsComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [UploadsComponent]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UploadsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(UploadsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/@ansyn/menu-items/uploads/uploads.module.spec.ts
+++ src/app/@ansyn/menu-items/uploads/uploads.module.spec.ts
@@ -1,13 +1,13 @@
 import { UploadsModule } from './uploads.module';
 
 describe('UploadsModule', () => {
-  let uploadsModule: UploadsModule;
+	let uploadsModule: UploadsModule;
 
-  beforeEach(() => {
-    uploadsModule = new UploadsModule();
-  });
+	beforeEach(() => {
+		uploadsModule = new UploadsModule();
+	});
 
-  it('should create an instance', () => {
-    expect(uploadsModule).toBeTruthy();
-  });
+	it('should create an instance', () => {
+		expect(uploadsModule).toBeTruthy();
+	});
 });

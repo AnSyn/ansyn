a23955e0f5489964bfcdcfe6abc34effc1ce5772
ramtob <ramtob@gmail.com>
2018-11-05 14:32:42 +0200

    tasks form (cont.)

--- src/app/@ansyn/menu-items/algorithms/components/tasks-form/tasks-form.component.ts
+++ src/app/@ansyn/menu-items/algorithms/components/tasks-form/tasks-form.component.ts
@@ -9,6 +9,7 @@ export class TasksFormComponent implements OnInit {
 	taskName: string;
 	taskStatus: 'New' | 'Sent' = 'New';
 	algorithm: string;
+	whichOverlays: 'case_overlays' | 'favorite_overlays' | 'displayed_overlays' = 'favorite_overlays';
 
 	constructor() {
 	}

eb2381a9e08a18ce21c4adf947b6ad711abd74c7
ramtob <ramtob@gmail.com>
2018-11-05 17:11:10 +0200

    tasks form (cont.) + tests now pass + another attempt to use ansyn-input ... still not nice

--- src/app/@ansyn/menu-items/algorithms/components/tasks-form/tasks-form.component.spec.ts
+++ src/app/@ansyn/menu-items/algorithms/components/tasks-form/tasks-form.component.spec.ts
@@ -6,21 +6,53 @@ import { CoreModule } from '../../../../core/core.module';
 import { StoreModule } from '@ngrx/store';
 import { coreFeatureKey, CoreReducer } from '../../../../core/reducers/core.reducer';
 import { EffectsModule } from '@ngrx/effects';
+import { MockComponent } from '@ansyn/core';
 
 describe('TasksFormComponent', () => {
 	let component: TasksFormComponent;
 	let fixture: ComponentFixture<TasksFormComponent>;
 
+	const mockComboBoxOptionComponent = MockComponent({
+		selector: 'ansyn-combo-box-option',
+		inputs: ['value'],
+		outputs: []
+	});
+
+	const mockComboBoxComponent = MockComponent({
+		selector: 'ansyn-combo-box',
+		inputs: ['options', 'renderFunction', 'comboBoxToolTipDescription', 'ngModel', 'color', 'placeholder'],
+		outputs: ['ngModelChange']
+	});
+
+	const mockComboTrigger = MockComponent({
+		selector: 'button[ansynComboBoxTrigger]',
+		inputs: ['isActive', 'render', 'ngModel'],
+		outputs: ['ngModelChange']
+	});
+
+	const mockRadioBtn = MockComponent({
+		selector: 'ansyn-radio',
+		inputs: ['ngModel', 'value'],
+		outputs: ['ngModelChange']
+	});
+
+	const mockInput = MockComponent({
+		selector: 'ansyn-input',
+		inputs: ['ngModel', 'value', 'required'],
+		outputs: ['ngModelChange']
+	});
+
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [TasksFormComponent],
-			imports: [
-				FormsModule,
-
-				CoreModule,
-				StoreModule.forRoot({ [coreFeatureKey]: CoreReducer }),
-				EffectsModule.forRoot([])
-			]
+			declarations: [
+				TasksFormComponent,
+				mockComboBoxComponent,
+				mockComboBoxOptionComponent,
+				mockComboTrigger,
+				mockRadioBtn,
+				mockInput
+			],
+			imports: []
 		})
 			.compileComponents();
 	}));

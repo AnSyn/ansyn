40b71b3b826b51684934ea32eed2cc4e22811ca2
YairTawil <yairtawil@gmail.com>
2018-11-05 15:51:11 +0200

    feat: ansyn checkbox ng model (#1912)
    * feat: ansyn checkbox ng model
    
    * test: all ansyn-checkbox to ng model

--- src/app/@ansyn/core/forms/ansyn-checkbox/ansyn-checkbox.component.ts
+++ src/app/@ansyn/core/forms/ansyn-checkbox/ansyn-checkbox.component.ts
@@ -1,15 +1,55 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
 import { UUID } from 'angular2-uuid';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { noop } from 'rxjs';
 
 @Component({
 	selector: 'ansyn-checkbox',
 	templateUrl: './ansyn-checkbox.component.html',
-	styleUrls: ['./ansyn-checkbox.component.less']
+	styleUrls: ['./ansyn-checkbox.component.less'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => AnsynCheckboxComponent),
+			multi: true
+		}
+	]
 })
 
-export class AnsynCheckboxComponent {
+export class AnsynCheckboxComponent implements ControlValueAccessor {
 	@Input() id = UUID.UUID();
-	@Input() readonly checked: boolean;
-	@Input() readonly disabled: boolean;
-	@Output() readonly checkedChange = new EventEmitter<boolean>();
+	disabled: boolean;
+
+	protected _value;
+	private onTouchedCallback: () => void = noop;
+	private onChangeCallback: (_: any) => void = noop;
+
+	set value(value: boolean) {
+		if (this.value !== value) {
+			this._value = value;
+			this.onChangeCallback(value);
+		}
+	}
+
+	get value() {
+		return this._value;
+	}
+
+	writeValue(value: boolean): void {
+		if (this.value !== value) {
+			this.value = value;
+		}
+	}
+
+	registerOnChange(fn: any) {
+		this.onChangeCallback = fn;
+	}
+
+	registerOnTouched(fn: any) {
+		this.onTouchedCallback = fn;
+	}
+
+	setDisabledState(isDisabled: boolean): void {
+		this.disabled = isDisabled;
+	}
 }

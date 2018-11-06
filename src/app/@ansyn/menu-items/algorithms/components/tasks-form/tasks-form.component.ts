33f249ea94c11b686111c7c1f6c2b68a07faa0a3
ramtob <ramtob@gmail.com>
2018-11-06 14:56:22 +0200

    Tweaks

--- src/app/@ansyn/menu-items/algorithms/components/tasks-form/tasks-form.component.ts
+++ src/app/@ansyn/menu-items/algorithms/components/tasks-form/tasks-form.component.ts
@@ -11,14 +11,15 @@ export class TasksFormComponent implements OnInit {
 	taskStatus: 'New' | 'Sent' = 'New';
 	algName: string;
 	whichOverlays: 'case_overlays' | 'favorite_overlays' | 'displayed_overlays' = 'favorite_overlays';
+	algorithms = {};
 	algNames: string[] = [];
 
 	constructor(protected algorithmsService: AlgorithmsService) {
 	}
 
 	ngOnInit() {
-		console.log(this.algorithmsService);
-		this.algNames = Object.keys(this.algorithmsService.config);
+		this.algorithms = this.algorithmsService.config;
+		this.algNames = Object.keys(this.algorithms);
 	}
 
 	onSubmit() {

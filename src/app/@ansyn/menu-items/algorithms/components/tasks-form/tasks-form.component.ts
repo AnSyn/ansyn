b428785a0c7e5546c611deb35ed5275b18f4ca69
ramtob <ramtob@gmail.com>
2018-11-06 16:27:14 +0200

    Some code to show and/or clear error message

--- src/app/@ansyn/menu-items/algorithms/components/tasks-form/tasks-form.component.ts
+++ src/app/@ansyn/menu-items/algorithms/components/tasks-form/tasks-form.component.ts
@@ -13,6 +13,7 @@ export class TasksFormComponent implements OnInit {
 	whichOverlays: 'case_overlays' | 'favorite_overlays' | 'displayed_overlays' = 'favorite_overlays';
 	algNames: string[] = [];
 	overlays = ['a', 'b', 'c'];
+	errorMsg: string;
 
 	get algorithms() {
 		return this.algorithmsService.config;
@@ -30,8 +31,21 @@ export class TasksFormComponent implements OnInit {
 	}
 
 	ngOnInit() {
+		this.clearError();
 		console.log(this.algorithms);
 		this.algNames = Object.keys(this.algorithms);
+	}
+
+	clearError() {
+		this.errorMsg = '';
+	}
+
+	showError(msg: string) {
+		this.errorMsg = msg;
+	}
+
+	hasError(): boolean {
+		return Boolean(this.errorMsg);
 	}
 
 	onSubmit() {

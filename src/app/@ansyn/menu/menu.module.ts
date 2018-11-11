7ce817d80cbc598551f30a4c4f9643240f18b5ab
asafMasa <asaf.masa@gmail.com>
2018-11-11 15:37:16 +0200

    add menuItems from config

--- src/app/@ansyn/menu/menu.module.ts
+++ src/app/@ansyn/menu/menu.module.ts
@@ -2,7 +2,7 @@ import { ANALYZE_FOR_ENTRY_COMPONENTS, Inject, InjectionToken, ModuleWithProvide
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IMenuItem } from './models/menu-item.model';
+import { IMenuItem, MENU_ITEMS_CONFIG } from './models/menu-item.model';
 import { Store, StoreModule } from '@ngrx/store';
 import { InitializeMenuItemsAction } from './actions/menu.actions';
 import { menuFeatureKey, MenuReducer } from './reducers/menu.reducer';
@@ -38,7 +38,16 @@ export class MenuModule {
 		};
 	}
 
-	constructor(protected store: Store<any>, @Inject(MENU_ITEMS) menuItems: IMenuItem[]) {
+	constructor(protected store: Store<any>, @Inject(MENU_ITEMS) menuItems: IMenuItem[],
+				@Inject(MENU_ITEMS_CONFIG) public configMenuItems: string[]) {
+
+		// if empty put all
+		if (configMenuItems && configMenuItems.length > 0) {
+			menuItems = menuItems.filter((menuItem: IMenuItem) => {
+				return configMenuItems.includes(menuItem.name);
+			});
+		}
+
 		store.dispatch(new InitializeMenuItemsAction(menuItems));
 	}
 }

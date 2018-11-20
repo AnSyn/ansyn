1cc4fc9fecee770eb9a33b5a5a3f4c95858ecf58
YairTawil <yairtawil@gmail.com>
2018-11-20 15:12:31 +0200

    feat: settings store

--- src/app/@ansyn/menu-items/settings/settings/settings.component.ts
+++ src/app/@ansyn/menu-items/settings/settings/settings.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ISettingsState, selectIsAnaglyphActive } from '../reducers/settings.reducer';
+import { Store } from '@ngrx/store';
+import { SetAnaglyphStateAction } from '../actions/settings.actions';
 
 @Component({
 	selector: 'ansyn-settings',
 	templateUrl: './settings.component.html',
 	styleUrls: ['./settings.component.less']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent implements OnInit, OnDestroy {
+	public isAnaglyphEnabled$ = this.store.select(selectIsAnaglyphActive);
 
-	constructor() {
+	constructor(public store: Store<ISettingsState>) {
 	}
 
 	ngOnInit() {
+	}
+
+	ngOnDestroy(): void {
+	}
+
+	enableAnaglyph(isChecked) {
+		this.store.dispatch(new SetAnaglyphStateAction(isChecked));
 	}
 
 }

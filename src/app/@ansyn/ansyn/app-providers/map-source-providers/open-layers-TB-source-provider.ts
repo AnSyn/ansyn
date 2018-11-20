beab82f41e88d683b3878da233c42ce9736aeeda
YairTawil <yairtawil@gmail.com>
2018-11-20 12:37:19 +0200

    feat: get thumb nail

--- src/app/@ansyn/ansyn/app-providers/map-source-providers/open-layers-TB-source-provider.ts
+++ src/app/@ansyn/ansyn/app-providers/map-source-providers/open-layers-TB-source-provider.ts
@@ -1,7 +1,14 @@
 import { OpenLayersDisabledMap, OpenLayersMap } from '@ansyn/plugins';
-import { ImageryMapSource } from '@ansyn/imagery';
+import { CacheService, ImageryCommunicatorService, ImageryMapSource } from '@ansyn/imagery';
 import { OpenLayersMapSourceProvider } from './open-layers.map-source-provider';
 import { ICaseMapState } from '@ansyn/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Inject } from '@angular/core';
+import { TBSourceProvider } from '../overlay-source-providers/tb/tb-source-provider';
+import { ITBOverlaySourceConfig } from '../overlay-source-providers/tb/tb.model';
+import { IMapSourceProvidersConfig, MAP_SOURCE_PROVIDERS_CONFIG } from './map-source-providers-config';
+import { catchError } from 'rxjs/operators';
 
 export const OpenLayerTBSourceProviderSourceType = 'TB';
 
@@ -10,8 +17,23 @@ export const OpenLayerTBSourceProviderSourceType = 'TB';
 	supported: [OpenLayersMap, OpenLayersDisabledMap]
 })
 export class OpenLayerTBSourceProvider extends OpenLayersMapSourceProvider {
+	constructor(
+		protected cacheService: CacheService,
+		protected imageryCommunicatorService: ImageryCommunicatorService,
+		@Inject(MAP_SOURCE_PROVIDERS_CONFIG) protected config: IMapSourceProvidersConfig,
+		@Inject(TBSourceProvider) protected tbConfig: ITBOverlaySourceConfig,
+		protected http: HttpClient) {
+		super(cacheService, imageryCommunicatorService, config)
+	}
 
 	createAsync(metaData: ICaseMapState): Promise<any> {
 		return Promise.resolve(null);
+	}
+
+	getThumbnailUrl(overlay, position): Observable<string> {
+		console.log('getThumbnailUrl? TB')
+		return this.http.get<string>(`${this.tbConfig.baseUrl}/layers/${overlay.id}/thumbnail`).pipe(
+			catchError(() => of(''))
+		)
 	}
 }

6b54c297ab18e48ed9c6b51d93f8a2294356a9b6
YairTawil <yairtawil@gmail.com>
2018-11-21 22:38:40 +0200

    hot-fix for build libs

--- src/app/@ansyn/menu-items/settings/settings/settings.component.ts
+++ src/app/@ansyn/menu-items/settings/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ISettingsState, selectIsAnaglyphActive } from '../reducers/settings.reducer';
 import { Store } from '@ngrx/store';
 import { SetAnaglyphStateAction } from '../actions/settings.actions';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'ansyn-settings',
@@ -9,7 +10,7 @@ import { SetAnaglyphStateAction } from '../actions/settings.actions';
 	styleUrls: ['./settings.component.less']
 })
 export class SettingsComponent implements OnInit, OnDestroy {
-	public isAnaglyphEnabled$ = this.store.select(selectIsAnaglyphActive);
+	public isAnaglyphEnabled$: Observable<boolean> = this.store.select(selectIsAnaglyphActive);
 
 	constructor(public store: Store<ISettingsState>) {
 	}

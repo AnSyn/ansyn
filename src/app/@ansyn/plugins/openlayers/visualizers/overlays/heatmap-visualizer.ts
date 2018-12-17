288a9adac66edb4ec91f8ae411c1d7023ac8c00d
YairTawil <yairtawil@gmail.com>
2018-12-17 17:12:28 +0200

    Fork updates (#1986)
    * Fix others in tb fetch params (#25)
    
    * fix: fix the 'others' filter in TB provider
    
    * fix: remove the console-log
    
    * fix: initialize the sensor's list
    
    * fix: return only one list in the query
    
    * fix: fix PR's comments
    
    * fix: return the 'tb-server' domain instead of 'localhot:10010'
    
    * enh: multiple services for kill-tasks script
    
    * Update upload form (#26)
    
    * implement new upload form
    
    * Uppercase sensor type
    
    * feat: required validation for ansyn checkbox (#27)
    
    * feat: required validation for ansyn checkbox
    
    * update upload form
    
    * Simplify prop (#28)
    
    * add 'simplifyGeometries' prop on visualizers
    
    * add config for heatmap
    
    * public the config injection
    
    * fix illigal config
    
    * fix: geometryToEntity function
    
    * change simplifyGeometries to boolean (not string)
    
    * fix: libs error
    
    * num of points
    
    * fix: asaf CR request changes
    
    * fix reset form before request (#29)

--- src/app/@ansyn/plugins/openlayers/visualizers/overlays/heatmap-visualizer.ts
+++ src/app/@ansyn/plugins/openlayers/visualizers/overlays/heatmap-visualizer.ts
@@ -1,28 +1,25 @@
 import { OverlaysService } from '@ansyn/overlays';
 import { Store } from '@ngrx/store';
 import { Actions } from '@ngrx/effects';
-import { ImageryVisualizer } from '@ansyn/imagery';
+import { ImageryVisualizer, IVisualizersConfig, VisualizersConfig } from '@ansyn/imagery';
 import { IVisualizerEntity } from '@ansyn/core';
 import * as turf from '@turf/turf';
 import { OpenLayersMap } from '../../open-layers-map/openlayers-map/openlayers-map';
 import { BaseFootprintsVisualizer } from './base-footprints-visualizer';
+import { Inject } from '@angular/core';
 
 @ImageryVisualizer({
 	supported: [OpenLayersMap],
-	deps: [Store, Actions, OverlaysService]
+	deps: [Store, VisualizersConfig, Actions, OverlaysService]
 })
 export class FootprintHeatmapVisualizer extends BaseFootprintsVisualizer {
 
-	geometryToEntity(id, footprint): IVisualizerEntity {
-		const fp = turf.simplify(turf.multiPolygon(footprint.coordinates), { tolerance: 0.01, highQuality: true });
-		return super.geometryToEntity(id, fp.geometry);
-	}
-
 	constructor(public store: Store<any>,
+				@Inject(VisualizersConfig) public config: IVisualizersConfig,
 				public actions$: Actions,
 				public overlaysService: OverlaysService
 	) {
-		super(store, overlaysService, 'Heatmap', null, {
+		super(store, overlaysService, 'Heatmap', config.FootprintHeatmapVisualizer, {
 			opacity: 0.5,
 			initial: {
 				fill: 'rgb(255, 0, 0)',

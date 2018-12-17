8d724ecdf2129c012f5fda6e9fd68188c99a0cca
YairTawil <yairtawil@gmail.com>
2018-12-17 13:38:27 +0200

    Simplify prop (#28)
    * add 'simplifyGeometries' prop on visualizers
    
    * add config for heatmap
    
    * public the config injection
    
    * fix illigal config
    
    * fix: geometryToEntity function
    
    * change simplifyGeometries to boolean (not string)
    
    * fix: libs error
    
    * num of points
    
    * fix: asaf CR request changes

--- src/app/@ansyn/plugins/openlayers/visualizers/overlays/base-footprints-visualizer.ts
+++ src/app/@ansyn/plugins/openlayers/visualizers/overlays/base-footprints-visualizer.ts
@@ -4,10 +4,11 @@ import { IMapState, MapFacadeService, mapStateSelector } from '@ansyn/map-facade
 import { OverlaysService, selectDrops } from '@ansyn/overlays';
 import { select, Store } from '@ngrx/store';
 import { distinctUntilChanged, filter, map } from 'rxjs/operators';
-import { ICaseMapState, IOverlay } from '@ansyn/core';
+import { ICaseMapState, IOverlay, IVisualizerEntity, IVisualizerStateStyle } from '@ansyn/core';
 import { mergeMap, withLatestFrom } from 'rxjs/internal/operators';
 import { AutoSubscription } from 'auto-subscriptions';
 import { EMPTY } from 'rxjs/index';
+import * as turf from '@turf/turf';
 
 export class BaseFootprintsVisualizer extends EntitiesVisualizer {
 
@@ -39,9 +40,19 @@ export class BaseFootprintsVisualizer extends EntitiesVisualizer {
 	constructor(public store: Store<any>,
 				public overlaysService: OverlaysService,
 				public overlayDisplayMode: string,
+				public fpConfig: Partial<IVisualizerStateStyle>,
 				...superArgs
 	) {
-		super(...superArgs);
+		super(fpConfig, ...superArgs);
+	}
+
+	geometryToEntity(id, geometry): IVisualizerEntity {
+		const numOfPoints = geometry.coordinates[0][0].length;
+
+		if (this.fpConfig.minSimplifyVertexCountLimit < numOfPoints) {
+			geometry = turf.simplify(turf.multiPolygon(geometry.coordinates), { tolerance: 0.01, highQuality: true }).geometry;
+		}
+		return super.geometryToEntity(id, geometry);
 	}
 
 }

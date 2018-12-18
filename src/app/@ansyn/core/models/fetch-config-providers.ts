6929248165089f7cc265451a6e907351d7505b58
Dana <Dana@LAPTOP-SLR2VUV8>
2018-12-18 16:07:23 +0200

    (feat)added config merge for users use

--- src/app/@ansyn/core/models/fetch-config-providers.ts
+++ src/app/@ansyn/core/models/fetch-config-providers.ts
@@ -1,10 +1,18 @@
 import { ValueProvider } from '@angular/core';
+import { mergeWith, isArray } from 'lodash';
 
 export const getProviders = (conf): any[] => Object.entries(conf).map(([key, value]): ValueProvider => ({
 	provide: key,
 	useValue: value
 }));
 
-export const fetchConfigProviders = (configPath = 'assets/config/app.config.json') => fetch(configPath)
+export const fetchConfigProviders = (configPath = 'assets/config/app.config.json', mergeChanges = null) => fetch(configPath)
 	.then(response => response.json())
+	.then(jsonConfig => mergeWith(jsonConfig, mergeChanges, mergeConfig))
 	.then(getProviders);
+
+export const mergeConfig = (changeInConfig, mergeChanges) => {
+	if (isArray(mergeChanges)) {
+		return mergeChanges
+	}
+}

7fc25b55f1ad7c55579e2981ddd876fb002e301a
YairTawil <yairtawil@gmail.com>
2018-12-20 12:07:46 +0200

    multiple uploads requests

--- src/app/drone/uploads/components/uploads/uploads.component.ts
+++ src/app/drone/uploads/components/uploads/uploads.component.ts
@@ -3,7 +3,7 @@ import { IUploadsConfig, UploadsConfig } from '../../config/uploads-config';
 import { HttpClient } from '@angular/common/http';
 import { catchError, delay, tap } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
-import { ErrorHandlerService, SetToastMessageAction } from '@ansyn/core';
+import { ErrorHandlerService, SetToastMessageAction, forkJoinSafe } from '@ansyn/core';
 import { isEqual } from 'lodash';
 import { ResetFormData, UploadFormData } from '../../actions/uploads.actions';
 import { initialUploadsFromData, IUploadsFormData, selectFormData } from '../../reducers/uploads.reducer';
@@ -62,33 +62,28 @@ export class UploadsComponent {
 	submitCustomSensorName(text: string) {
 		if (text) {
 			console.log(text);
-			this.uploadFormData({ sensorName: text, otherSensorName: true })
+			this.uploadFormData({ sensorName: text, otherSensorName: true });
 		}
 		this.modal = false;
 	}
 
 	onSubmit() {
-
 		this.loading = true;
-		const formData = new FormData();
-		formData.append('description', this.formData.description);
-		formData.append('creditName', this.formData.creditName);
-		formData.append('sensorType', this.formData.sensorType);
-		formData.append('sensorName', this.formData.sensorName);
-		formData.append('sharing', this.formData.sharing);
-		Array.from(this.formData.files).forEach((file) => formData.append('uploads', file));
 
-		this.httpClient
-			.post(this.config.apiUrl, formData)
-			.pipe(
-				tap(() => this.store.dispatch(new SetToastMessageAction({ toastText: 'Success to upload file' }))),
-				catchError((err) => this.errorHandlerService.httpErrorHandle(err, 'Failed to upload file', null)),
-				tap(() => {
-					this.loading = false;
-					this.resetForm();
-				})
-			)
-			.subscribe();
+		const uploadRequests = Array.from(this.formData.files).map((file) => {
+			const formData = new FormData();
+			formData.append('description', this.formData.description);
+			formData.append('creditName', this.formData.creditName);
+			formData.append('sensorType', this.formData.sensorType);
+			formData.append('sensorName', this.formData.sensorName);
+			formData.append('sharing', this.formData.sharing);
+			formData.append('uploads', file);
+
+			return this.httpClient
+				.post(this.config.apiUrl, formData);
+		});
+
+		forkJoinSafe(uploadRequests).subscribe(() => this.loading = false);
 	}
 
 	resetForm() {

c0262a0539fdbecaa8853ff4b1496f74aef5ad79
danaorlov1005 <dandik.o12@gmail.com>
2018-12-24 15:40:28 +0200

    (fix) pr fixes

--- src/app/@ansyn/ansyn/config.model.ts
+++ src/app/@ansyn/ansyn/config.model.ts
@@ -8,12 +8,12 @@ import { IStatusBarConfig } from "@ansyn/status-bar";
 import { IMenuConfig } from "../menu/models/menu-config.model";
 import { ILoginConfig } from "../../app/login/models/login.config";
 import { IVisualizersConfig } from "@ansyn/imagery";
-import { IMultipleOverlaysSourceConfig } from "../../../../dist/ansyn/overlays/services/multiple-source-provider";
-import { IIdahoOverlaySourceConfig } from "../../../../dist/ansyn/ansyn/app-providers/overlay-source-providers/idaho-source-provider";
 import { IOpenAerialOverlaySourceConfig } from "./app-providers/overlay-source-providers/open-aerial-source-provider";
 import { IImisightOverlaySourceConfig } from "../../app/imisight/imisight.model";
 import { IPlanetOverlaySourceConfig } from "./app-providers/overlay-source-providers/planet/planet-source-provider";
 import { IAuth0Config } from "../../app/imisight/auth0.service";
+import { IIdahoOverlaySourceConfig } from "./app-providers/overlay-source-providers/idaho-source-provider";
+import { IMultipleOverlaysSourceConfig } from "../overlays/services/multiple-source-provider";
 
 export interface IConfigModel {
 	casesConfig?: ICasesConfig,

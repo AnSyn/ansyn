4532a3aaae677bb6d17b30b8f04efe868943444c
ramtob <ramtob@gmail.com>
2019-01-10 11:45:04 +0200

    Adding static image map source provider

---
+++ src/app/@ansyn/plugins/openlayers/mapSourceProviders/open-layers-static-image-source-provider.ts
@@ -0,0 +1,65 @@
+import { CacheService, ImageryCommunicatorService, ImageryMapSource } from '@ansyn/imagery';
+import {
+	ErrorHandlerService,
+	ICaseMapState,
+	IMapSourceProvidersConfig,
+	IOverlay,
+	MAP_SOURCE_PROVIDERS_CONFIG
+} from '@ansyn/core';
+import Projection from 'ol/proj/projection';
+import Static from 'ol/source/imagestatic';
+import ImageLayer from 'ol/layer/image';
+import { HttpClient } from '@angular/common/http';
+import { Inject } from '@angular/core';
+import { OpenLayersMap } from '../maps/open-layers-map/openlayers-map/openlayers-map';
+import { OpenLayersDisabledMap } from '../maps/openlayers-disabled-map/openlayers-disabled-map';
+import { OpenLayersMapSourceProvider } from './open-layers.map-source-provider';
+
+export const OpenLayerStaticImageSourceProviderSourceType = 'STATIC_IMAGE';
+
+@ImageryMapSource({
+	sourceType: OpenLayerStaticImageSourceProviderSourceType,
+	supported: [OpenLayersMap, OpenLayersDisabledMap],
+	forOverlay: true
+})
+export class OpenLayersStaticImageSourceProvider extends OpenLayersMapSourceProvider {
+
+	constructor(
+		protected cacheService: CacheService,
+		protected imageryCommunicatorService: ImageryCommunicatorService,
+		@Inject(MAP_SOURCE_PROVIDERS_CONFIG) protected mapSourceProvidersConfig: IMapSourceProvidersConfig,
+		protected errorHandlerService: ErrorHandlerService,
+		protected http: HttpClient) {
+		super(cacheService, imageryCommunicatorService, mapSourceProvidersConfig);
+	}
+
+	createAsync(metaData: ICaseMapState): Promise<any> {
+		let layer;
+		const extent: any = [0, 0, metaData.data.overlay.tag.imageData.ExifImageWidth, metaData.data.overlay.tag.imageData.ExifImageHeight];
+		const code = `static-image ${metaData.data.overlay.id}`;
+
+		const projection = new Projection({
+			code,
+			units: 'pixels',
+			extent
+		});
+
+		const source = new Static({
+			url: metaData.data.overlay.imageUrl,
+			crossOrigin: null,
+			imageExtent: extent,
+			projection
+		});
+
+		layer = new ImageLayer({
+			source,
+			extent
+		});
+		return this.addFootprintToLayerPromise(Promise.resolve(layer), metaData);
+	}
+
+	getThumbnailName(overlay: IOverlay): string {
+		return overlay.name;
+	}
+
+}

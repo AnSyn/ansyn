33d30138c790a5d17041525ab1a4dac88ce95997
Tzahi Levi <thetzahilevi@gmail.com>
2019-01-22 16:34:46 +0200

    Overlays bug on save case (#128)
    * update id on save
    
    * update id on save

--- src/app/@ansyn/menu-items/cases/services/cases.service.ts
+++ src/app/@ansyn/menu-items/cases/services/cases.service.ts
@@ -38,16 +38,36 @@ export class CasesService {
 	paginationLimit = 15;
 	queryParamsKeys;
 
-	get defaultCase() {
-		return this.config.defaultCase;
-	}
-
 	constructor(protected storageService: StorageService,
 				@Inject(casesConfig) public config: ICasesConfig,
 				public urlSerializer: UrlSerializer,
 				public errorHandlerService: ErrorHandlerService) {
 		this.paginationLimit = this.config.paginationLimit;
 		this.queryParamsKeys = this.config.casesQueryParamsKeys;
+	}
+
+	get defaultCase() {
+		return this.config.defaultCase;
+	}
+
+	get decodeCaseObjects() {
+		return this.queryParamsHelper.decodeCaseObjects.bind(this.queryParamsHelper);
+	}
+
+	get encodeCaseObjects() {
+		return this.queryParamsHelper.encodeCaseObjects.bind(this.queryParamsHelper);
+	}
+
+	get generateQueryParamsViaCase() {
+		return this.queryParamsHelper.generateQueryParamsViaCase.bind(this.queryParamsHelper);
+	}
+
+	get updateCaseViaQueryParmas() {
+		return this.queryParamsHelper.updateCaseViaQueryParmas.bind(this.queryParamsHelper);
+	}
+
+	get updateCaseViaContext() {
+		return this.queryParamsHelper.updateCaseViaContext.bind(this.queryParamsHelper);
 	}
 
 	loadCases(casesOffset: number = 0): Observable<any> {
@@ -59,7 +79,7 @@ export class CasesService {
 	}
 
 	parseCasePreview(casePreview: ICasePreview): ICasePreview {
-		return <any> {
+		return <any>{
 			...casePreview,
 			creationTime: new Date(casePreview.creationTime),
 			lastModified: new Date(casePreview.lastModified)
@@ -67,7 +87,7 @@ export class CasesService {
 	}
 
 	parseCase(caseValue: ICase): ICase {
-		return <any> {
+		return <any>{
 			...caseValue,
 			creationTime: new Date(caseValue.creationTime),
 			lastModified: new Date(caseValue.lastModified),
@@ -144,10 +164,18 @@ export class CasesService {
 	createCase(selectedCase: ICase): Observable<ICase> {
 		const currentTime = new Date();
 		const uuid = UUID.UUID();
+		let currentMapId = selectedCase.state.maps.activeMapId;
 		selectedCase.id = uuid;
 		selectedCase.creationTime = currentTime;
 		selectedCase.lastModified = currentTime;
 		selectedCase.autoSave = true;
+		selectedCase.state.maps.data.forEach(map => {
+			let thisMapId = map.id;
+			map.id = UUID.UUID();
+			if (thisMapId === currentMapId) {
+				selectedCase.state.maps.activeMapId = map.id;
+			}
+		});
 		return this.storageService.create(this.config.schema, this.convertToStoredEntity(selectedCase))
 			.pipe<any>(
 				map(_ => selectedCase),
@@ -179,26 +207,6 @@ export class CasesService {
 		const baseLocation = location.href.split('#')[0];
 		const href = this.config.useHash ? `${baseLocation}/#` : baseLocation;
 		return `${href}/case/${caseId}`;
-	}
-
-	get decodeCaseObjects() {
-		return this.queryParamsHelper.decodeCaseObjects.bind(this.queryParamsHelper);
-	}
-
-	get encodeCaseObjects() {
-		return this.queryParamsHelper.encodeCaseObjects.bind(this.queryParamsHelper);
-	}
-
-	get generateQueryParamsViaCase() {
-		return this.queryParamsHelper.generateQueryParamsViaCase.bind(this.queryParamsHelper);
-	}
-
-	get updateCaseViaQueryParmas() {
-		return this.queryParamsHelper.updateCaseViaQueryParmas.bind(this.queryParamsHelper);
-	}
-
-	get updateCaseViaContext() {
-		return this.queryParamsHelper.updateCaseViaContext.bind(this.queryParamsHelper);
 	}
 
 }

421ce99f5e54980be8c05b37a6d191d998051cc9
YairTawil <yairtawil@gmail.com>
2019-02-04 09:10:45 +0200

    feat: image to tiff (#151)

--- src/app/drone/map-source-provider/open-layers-TB-source-provider.ts
+++ src/app/drone/map-source-provider/open-layers-TB-source-provider.ts
@@ -1,11 +1,6 @@
 import { OpenLayersDisabledMap, OpenLayersMap, OpenLayersMapSourceProvider } from '@ansyn/plugins';
 import { CacheService, ImageryCommunicatorService, ImageryMapSource } from '@ansyn/imagery';
-import {
-	ICaseMapState,
-	IMapSourceProvidersConfig,
-	IOverlay,
-	MAP_SOURCE_PROVIDERS_CONFIG
-} from '@ansyn/core';
+import { ICaseMapState, IMapSourceProvidersConfig, IOverlay, MAP_SOURCE_PROVIDERS_CONFIG } from '@ansyn/core';
 import Projection from 'ol/proj/projection';
 import Static from 'ol/source/imagestatic';
 import ImageLayer from 'ol/layer/image';
@@ -14,7 +9,10 @@ import { Inject } from '@angular/core';
 import { ITBConfig } from '../overlay-source-provider/tb.model';
 import { createTransform, FROMCOORDINATES, FROMPIXEL } from './transforms';
 import TileWMS from 'ol/source/tilewms';
+
 import TileLayer from 'ol/layer/tile';
+
+
 import { noop } from 'rxjs';
 
 export const OpenLayerTBSourceProviderSourceType = 'TB';
@@ -28,8 +26,8 @@ export class OpenLayerTBSourceProvider extends OpenLayersMapSourceProvider<ITBCo
 	constructor(
 		protected cacheService: CacheService,
 		protected imageryCommunicatorService: ImageryCommunicatorService,
-		@Inject(MAP_SOURCE_PROVIDERS_CONFIG) protected mapSourceProvidersConfig: IMapSourceProvidersConfig,
-		) {
+		@Inject(MAP_SOURCE_PROVIDERS_CONFIG) protected mapSourceProvidersConfig: IMapSourceProvidersConfig
+	) {
 		super(cacheService, imageryCommunicatorService, mapSourceProvidersConfig);
 	}
 
@@ -37,15 +35,7 @@ export class OpenLayerTBSourceProvider extends OpenLayersMapSourceProvider<ITBCo
 		let layer;
 		if (metaData.data.overlay.tag.fileType === 'image') {
 			const extent: any = [0, 0, metaData.data.overlay.tag.imageData.ExifImageWidth, metaData.data.overlay.tag.imageData.ExifImageHeight];
-			const boundary = metaData.data.overlay.tag.geoData.footprint.geometry.coordinates[0];
-			const code = `tb-image ${metaData.data.overlay.id}`;
-			const transformer = createTransform(code, boundary, extent[2], extent[3]);
-
-			const projection = new Projection({
-				code,
-				units: 'pixels',
-				extent
-			});
+			const projection = this.createProjection(metaData.data.overlay);
 
 			const source = new Static({
 				url: metaData.data.overlay.imageUrl,
@@ -54,25 +44,6 @@ export class OpenLayerTBSourceProvider extends OpenLayersMapSourceProvider<ITBCo
 				projection,
 				imageLoadFunction: noop
 			});
-			proj.addProjection(source.getProjection());
-			proj.addCoordinateTransforms(projection, 'EPSG:3857',
-				coords => {
-					return transformer.EPSG3857(FROMPIXEL, ...coords);
-
-				},
-				coords => {
-					return transformer.EPSG3857(FROMCOORDINATES, ...coords);
-				}
-			);
-
-			proj.addCoordinateTransforms(projection, 'EPSG:4326',
-				coords => {
-					return transformer.EPSG4326(FROMPIXEL, ...coords);
-				},
-				coords => {
-					return transformer.EPSG4326(FROMCOORDINATES, ...coords);
-				}
-			);
 
 			layer = new ImageLayer({
 				source,
@@ -81,16 +52,21 @@ export class OpenLayerTBSourceProvider extends OpenLayersMapSourceProvider<ITBCo
 		}
 		else
 			{
+				const projection = this.createDroneTiffProjection(metaData.data.overlay);
 				const source = new TileWMS(<any>{
-					preload: Infinity,
 					url: metaData.data.overlay.imageUrl,
 					params: {
 						'VERSION': '1.1.0',
 						LAYERS: metaData.data.overlay.tag.geoserver.layer.resource.name
 					},
-					projection: metaData.data.overlay.tag.geoserver.data.srs
+					projection
 				});
-				layer = new TileLayer({ visible: true, source });
+				if (metaData.data.overlay.sensorType !== 'Awesome Drone Imagery (GeoTIFF)') {
+					layer = new TileLayer({ visible: true, source });
+				} else {
+					const extent: any = [0, -metaData.data.overlay.tag.imageData.ExifImageHeight, metaData.data.overlay.tag.imageData.ExifImageWidth, 0];
+					layer = new TileLayer({ visible: true, source, extent });
+				}
 			}
 		return [layer];
 		}
@@ -102,9 +78,84 @@ export class OpenLayerTBSourceProvider extends OpenLayersMapSourceProvider<ITBCo
 	}
 
 
-
 	getThumbnailName(overlay: IOverlay): string {
 		return overlay.name;
 	}
+
+	private createProjection(overlay) {
+		const extent: any = [0, 0, overlay.tag.imageData.ExifImageWidth, overlay.tag.imageData.ExifImageHeight];
+		const boundary = overlay.tag.geoData.footprint.geometry.coordinates[0];
+		const code = `tb-image ${overlay.id}`;
+		const transformer = createTransform(boundary, extent[2], extent[3]);
+
+		const projection = new Projection({
+			code,
+			units: 'pixels',
+			extent
+		});
+
+		proj.addProjection(projection);
+		proj.addCoordinateTransforms(projection, 'EPSG:3857',
+			coords => {
+				return transformer.EPSG3857(FROMPIXEL, ...coords);
+
+			},
+			coords => {
+				return transformer.EPSG3857(FROMCOORDINATES, ...coords);
+			}
+		);
+
+		proj.addCoordinateTransforms(projection, 'EPSG:4326',
+			coords => {
+				return transformer.EPSG4326(FROMPIXEL, ...coords);
+			},
+			coords => {
+				return transformer.EPSG4326(FROMCOORDINATES, ...coords);
+			}
+		);
+
+		return projection;
+
+	}
+
+	private createDroneTiffProjection(overlay) {
+		if (overlay.sensorType !== 'Awesome Drone Imagery (GeoTIFF)') {
+			return overlay.tag.geoserver.data.srs
+		}
+		const extent: any = [0, 0, overlay.tag.imageData.ExifImageWidth, overlay.tag.imageData.ExifImageHeight];
+		const boundary = overlay.tag.geoData.footprint.geometry.coordinates[0];
+		const transformer = createTransform(boundary, extent[2], extent[3]);
+
+		const projection = new Projection({
+			code: `EPSG:32662`,
+			units: 'm',
+			axisOrientation: 'neu',
+			global: false
+		});
+
+		proj.addProjection(projection);
+
+		proj.addCoordinateTransforms(projection, 'EPSG:3857',
+			coords => {
+				return transformer.EPSG3857(FROMPIXEL, ...coords);
+
+			},
+			coords => {
+				return transformer.EPSG3857(FROMCOORDINATES, ...coords);
+			}
+		);
+
+		proj.addCoordinateTransforms(projection, 'EPSG:4326', function ([x, y]) {
+			return transformer.EPSG4326(FROMPIXEL, x, y + overlay.tag.imageData.ExifImageHeight);
+		},
+		([lng, lat]) => {
+			const [x, y] = transformer.EPSG4326(FROMCOORDINATES, lng, lat);
+			return [x, y - overlay.tag.imageData.ExifImageHeight];
+		}
+		);
+
+		return projection;
+	}
+
 
 }

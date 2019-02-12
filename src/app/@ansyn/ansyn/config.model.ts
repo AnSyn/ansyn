8429fbfe43978d60b5b18fe7ba2fba43b289a35c
ramtob <ramtob@gmail.com>
2019-02-12 17:11:02 +0200

    Fixing build:libs errors

--- src/app/@ansyn/ansyn/config.model.ts
+++ src/app/@ansyn/ansyn/config.model.ts
@@ -1,16 +1,20 @@
-import { ICasesConfig, IFiltersConfig, ILayersManagerConfig, IToolsConfig } from "@ansyn/menu-items";
+import { IAlgorithmsConfig, ICasesConfig, IFiltersConfig, ILayersManagerConfig, IToolsConfig } from '@ansyn/menu-items';
 import { IOverlaysConfig } from '@ansyn/overlays';
-import { IMapFacadeConfig } from "@ansyn/map-facade";
-import { ICoreConfig, ILoggerConfig, IMapSourceProvidersConfig, IMultipleOverlaysSourceConfig	 } from "@ansyn/core";
-import { IStatusBarConfig } from "@ansyn/status-bar";
-import { IMenuConfig } from "@ansyn/menu";
-import { IContextConfig } from "@ansyn/context";
-import { IVisualizersConfig } from "@ansyn/imagery";
-import { IOpenAerialOverlaySourceConfig } from "./app-providers/overlay-source-providers/open-aerial-source-provider";
-import { IPlanetOverlaySourceConfig } from "./app-providers/overlay-source-providers/planet/planet-source-provider";
-import { IIdahoOverlaySourceConfig } from "./app-providers/overlay-source-providers/idaho-source-provider";
-import { IAlgorithmsConfig } from '../menu-items/algorithms/models/tasks.model';
-import { ITilesLoadingConfig } from '../core/models/tiles-loading.config';
+import { IMapFacadeConfig } from '@ansyn/map-facade';
+import {
+	ICoreConfig,
+	ILoggerConfig,
+	IMapSourceProvidersConfig,
+	IMultipleOverlaysSourceConfig,
+	ITilesLoadingConfig
+} from '@ansyn/core';
+import { IStatusBarConfig } from '@ansyn/status-bar';
+import { IMenuConfig } from '@ansyn/menu';
+import { IContextConfig } from '@ansyn/context';
+import { IVisualizersConfig } from '@ansyn/imagery';
+import { IOpenAerialOverlaySourceConfig } from './app-providers/overlay-source-providers/open-aerial-source-provider';
+import { IPlanetOverlaySourceConfig } from './app-providers/overlay-source-providers/planet/planet-source-provider';
+import { IIdahoOverlaySourceConfig } from './app-providers/overlay-source-providers/idaho-source-provider';
 
 export interface IConfigModel {
 	casesConfig: ICasesConfig,

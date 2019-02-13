783d9af1019c3e5c38755d09aa4afd39e29bb1ba
YairTawil <yairtawil@gmail.com>
2019-02-13 09:15:06 +0200

    refactor: drag drop maps

--- src/app/@ansyn/map-facade/components/imageries-manager/providers/drag-drop-map.service.ts
+++ src/app/@ansyn/map-facade/components/imageries-manager/providers/drag-drop-map.service.ts
@@ -3,104 +3,106 @@ import { SetMapsDataActionStore } from '../../../actions/map.actions';
 import { Store } from '@ngrx/store';
 import { DOCUMENT } from '@angular/common';
 
+export interface IDragDropData {
+	target: HTMLElement;
+	dragElement: HTMLElement;
+	dropElement?: HTMLElement;
+	dragElementInitialBoundingBox: ClientRect;
+	ids: string[];
+	entities: object;
+}
+
 @Injectable()
 export class DragDropMapService {
 	TRANSITION_DURATION = 500;
-
-	currentTarget: HTMLElement;
-	dragElement: HTMLElement;
-	dropElement: HTMLElement;
+	data: IDragDropData = null;
 
 	constructor(protected store: Store<any>, @Inject(DOCUMENT) protected document: any) {
 	}
 
-	onMouseDown($event, dragElement, ids, mapsEntities) {
-		this.currentTarget = $event.currentTarget;
-		this.dragElement = dragElement;
+	onMouseDown($event, dragElement: HTMLElement, ids: string[], entities: object) {
 		this.document.body.style.userSelect = 'none';
-		this.dragElement.classList.add('draggable');
-
-		const mouseMove = this.createMouseMove();
-		const mouseUp = this.createMouseUp(mouseMove, ids, mapsEntities);
-
-		this.document.addEventListener('mousemove', mouseMove);
-		this.document.addEventListener('mouseup', mouseUp);
+		dragElement.classList.add('draggable');
+		const { currentTarget: target } = $event;
+		const dragElementInitialBoundingBox = dragElement.getBoundingClientRect();
+		this.data = { target, dragElement, dragElementInitialBoundingBox, ids, entities };
+		this.document.addEventListener('mousemove', this.mouseMove);
+		this.document.addEventListener('mouseup', this.mouseUp);
 	}
 
-	createMouseMove = () => {
-		const { left: initialX, top: initialY } = this.dragElement.getBoundingClientRect();
-		const { width: targetWidth, height: targetHeight } = this.currentTarget.getBoundingClientRect();
-
-		return ($event) => {
-			this.dragElement.style.pointerEvents = 'none';
-			const { left, top, width, height } = this.dragElement.getBoundingClientRect();
-			const pointElem = this.document.elementFromPoint(left + (width / 2), top + (height / 2));
-			const newDropElement = <HTMLElement> pointElem && pointElem.closest('.map-container-wrapper');
-			if (this.dropElement) {
-				this.dropElement.style.filter = null;
-				this.dropElement.querySelector<HTMLElement>('.active-border').style.height = null;
-			}
-			this.dropElement = <HTMLElement> newDropElement;
-			if (this.dropElement) {
-				this.dropElement.style.filter = 'blur(2px)';
-				this.dropElement.querySelector<HTMLElement>('.active-border').style.height = '100%';
-			}
-			this.dragElement.style.transition = null;
-			this.dragElement.style.zIndex = '200';
-			this.dragElement.style.transform = `translate(${$event.clientX - initialX - (targetWidth / 2)}px, ${$event.clientY - initialY - (targetHeight / 2)}px)`;
-		};
+	mouseMove = ($event) => {
+		const { target, dragElement, dropElement, dragElementInitialBoundingBox } = this.data;
+		const { width: targetWidth, height: targetHeight } = target.getBoundingClientRect();
+		const { left: initialLeft, top: initialTop } = dragElementInitialBoundingBox;
+		const { left, top, width, height } = dragElement.getBoundingClientRect();
+		if (dropElement) {
+			dropElement.style.filter = null;
+			dropElement.querySelector<HTMLElement>('.active-border').style.height = null;
+		}
+		dragElement.style.pointerEvents = 'none';
+		const pointElem = this.document.elementFromPoint(left + (width / 2), top + (height / 2));
+		const newDropElement = <HTMLElement> pointElem && pointElem.closest('.map-container-wrapper');
+		if (newDropElement) {
+			newDropElement.style.filter = 'blur(2px)';
+			newDropElement.querySelector('.active-border').style.height = '100%';
+		}
+		this.data.dropElement = newDropElement;
+		dragElement.style.transition = null;
+		dragElement.style.zIndex = '200';
+		dragElement.style.transform = `translate(${$event.clientX - initialLeft - (targetWidth / 2)}px, ${$event.clientY - initialTop - (targetHeight / 2)}px)`;
 	};
 
-	createMouseUp = (mouseMove, mapIds, mapsEntities) => {
-		const { left: initialLeft, top: initialTop } = this.dragElement.getBoundingClientRect();
-		const mouseUp = () => {
-			this.document.body.style.userSelect = null;
-			this.dragElement.style.transition = `${this.TRANSITION_DURATION}ms`;
-			let mapsList = null;
+	mouseUp = () => {
+		const { dragElement, dropElement, dragElementInitialBoundingBox, ids: mapIds, entities } = this.data;
+		const { left: initialLeft, top: initialTop } = dragElementInitialBoundingBox;
+		this.document.body.style.userSelect = null;
+		dragElement.style.transition = `${this.TRANSITION_DURATION}ms`;
 
-			if (this.dropElement) {
-				const { left: dropLeft, top: dropTop } = this.dropElement.getBoundingClientRect();
-				this.dropElement.style.filter = null;
-				this.dropElement.style.transition = `${this.TRANSITION_DURATION}ms`;
-				this.dropElement.style.transform = `translate(${initialLeft - dropLeft}px, ${initialTop - dropTop}px)`;
-				this.dragElement.style.transform = `translate(${dropLeft - initialLeft}px, ${dropTop - initialTop}px)`;
-				this.dropElement.style.zIndex = '199';
-				const ids = [...mapIds];
-				const id1 = this.dragElement.id;
-				const id2 = this.dropElement.id;
-				const indexOf1 = ids.indexOf(id1);
-				const indexOf2 = ids.indexOf(id2);
-				ids[indexOf1] = id2;
-				ids[indexOf2] = id1;
-				mapsList = ids.map((id) => mapsEntities[id]);
-				this.dropElement.classList.remove('droppable');
-			} else {
-				this.dragElement.style.transform = `translate(0, 0)`;
-			}
-			setTimeout(() => {
-				this.dragElement.classList.remove('draggable');
-				this.dragElement.style.pointerEvents = null;
-				this.dragElement.style.transition = null;
-				this.dragElement.style.transform = null;
-				this.dragElement.style.zIndex = null;
-				if (this.dropElement) {
-					this.dropElement.style.transition = null;
-					this.dropElement.style.transform = null;
-					this.dropElement.style.zIndex = null;
-					this.dropElement.querySelector<HTMLElement>('.active-border').style.height = null;
-				}
-				if (mapsList) {
-					this.store.dispatch(new SetMapsDataActionStore({ mapsList }));
-				}
-				this.dropElement = null;
-				this.dragElement = null;
-				this.currentTarget = null;
-			}, this.TRANSITION_DURATION);
-
-			this.document.removeEventListener('mousemove', mouseMove);
-			this.document.removeEventListener('mouseup', mouseUp);
+		const onDragTransitionEnd = ($event) => {
+			const { target } = $event;
+			target.classList.remove('draggable');
+			target.style.pointerEvents = null;
+			target.style.transition = null;
+			target.style.transform = null;
+			target.style.zIndex = null;
+			target.removeEventListener('transitionend', onDragTransitionEnd)
 		};
 
-		return mouseUp;
+		const onDropTransitionEnd = ($event) => {
+			const { target } = $event;
+			target.style.transition = null;
+			target.style.transform = null;
+			target.style.zIndex = null;
+			target.querySelector('.active-border').style.height = null;
+			const ids = [...mapIds];
+			const id1 = dragElement.id;
+			const id2 = dropElement.id;
+			const indexOf1 = ids.indexOf(id1);
+			const indexOf2 = ids.indexOf(id2);
+			ids[indexOf1] = id2;
+			ids[indexOf2] = id1;
+			const mapsList = ids.map((id) => entities[id]);
+			this.store.dispatch(new SetMapsDataActionStore({ mapsList }));
+			target.removeEventListener('transitionend', onDropTransitionEnd)
+		};
+
+		dragElement.addEventListener('transitionend', onDragTransitionEnd);
+
+		if (dropElement) {
+			dropElement.addEventListener('transitionend', onDropTransitionEnd);
+			const { left: dropLeft, top: dropTop } = dropElement.getBoundingClientRect();
+			dropElement.style.filter = null;
+			dropElement.style.transition = `${this.TRANSITION_DURATION}ms`;
+			dropElement.style.transform = `translate(${initialLeft - dropLeft}px, ${initialTop - dropTop}px)`;
+			dragElement.style.transform = `translate(${dropLeft - initialLeft}px, ${dropTop - initialTop}px)`;
+			dropElement.style.zIndex = '199';
+			dropElement.classList.remove('droppable');
+		} else {
+			dragElement.style.transform = `translate(0, 0)`;
+		}
+
+		this.data = null;
+		this.document.removeEventListener('mousemove', this.mouseMove);
+		this.document.removeEventListener('mouseup', this.mouseUp);
 	};
 }

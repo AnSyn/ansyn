92cccb880f6747deb1532b6100f2d867507ae0a0
YairTawil <yairtawil@gmail.com>
2019-03-18 10:58:15 +0200

    fix: ansyn overlays loader

--- src/app/@ansyn/overlays/components/overlays-loader/overlays-loader.component.spec.ts
+++ src/app/@ansyn/overlays/components/overlays-loader/overlays-loader.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { OverlaysLoaderComponent } from './overlays-loader.component';
 import { OverlayReducer, overlaysFeatureKey } from '../../reducers/overlays.reducer';
 import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('OverlaysLoaderComponent', () => {
 	let component: OverlaysLoaderComponent;
@@ -11,7 +12,7 @@ describe('OverlaysLoaderComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [OverlaysLoaderComponent],
-			imports: [StoreModule.forRoot({ [overlaysFeatureKey]: OverlayReducer })]
+			imports: [StoreModule.forRoot({ [overlaysFeatureKey]: OverlayReducer }), BrowserAnimationsModule]
 		})
 			.compileComponents();
 	}));

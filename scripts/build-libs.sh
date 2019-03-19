d2234ab8192d0d25933390e83e02f1f88b0e0761
YairTawil <yairtawil@gmail.com>
2019-03-19 13:51:57 +0200

    fix: un build context

--- scripts/build-libs.sh
+++ scripts/build-libs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BUILD_ASSETS=./src/app/\@ansyn/assets/build.sh
 chmod +x $BUILD_ASSETS
-BUILDS=("core" "menu" "overlays" "status-bar" "imagery" "map-facade" "context" "menu-items" "plugins" "ansyn")
+BUILDS=("core" "menu" "overlays" "status-bar" "imagery" "map-facade" "menu-items" "plugins" "ansyn")
 len=${#BUILDS[*]}
 for (( i=0; i<len; i++ ))
 do

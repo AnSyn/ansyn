b38f71b6cea89949bcb2a331233cafdc0b44b1c9
ramtob <ramtob@gmail.com>
2019-03-20 10:49:20 +0200

    Revert previous fix with catch

--- src/app/@ansyn/menu-items/cases/services/cases.service.ts
+++ src/app/@ansyn/menu-items/cases/services/cases.service.ts
@@ -215,13 +215,8 @@ export class CasesService {
 	}
 
 	isStoreEntitiesEqual(caseA, caseB) {
-		let cloneA, cloneB;
-		try {
-			cloneA = JSON.parse(JSON.stringify(caseA));
-			cloneB = JSON.parse(JSON.stringify(caseB));
-		} catch (e) {
-			return false
-		}
+		const cloneA = JSON.parse(JSON.stringify(caseA));
+		const cloneB = JSON.parse(JSON.stringify(caseB));
 		cloneA.data.maps.data.forEach((map, index) => {
 			if (cloneA.data.maps.activeMapId === map.id) {
 				cloneA.data.maps.activeMapId = index;

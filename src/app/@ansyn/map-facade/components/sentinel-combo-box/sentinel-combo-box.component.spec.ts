37c16a38ed793a72de2cd8b77001411bda3928b5
Tzahi Levi <thetzahilevi@gmail.com>
2019-04-16 12:25:21 +0300

    :clown_face: fixing test

--- src/app/@ansyn/map-facade/components/sentinel-combo-box/sentinel-combo-box.component.spec.ts
+++ src/app/@ansyn/map-facade/components/sentinel-combo-box/sentinel-combo-box.component.spec.ts
@@ -1,27 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { Store, StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
+import { SetSentinelLayerOnMap } from "../../../../app/sentinel/actions/sentinel.actions";
+import {
+	selectSentinelLayers,
+	selectSentinelselectedLayers,
+	sentinelFeatureKey,
+	SentinelReducer
+} from "../../../../app/sentinel/reducers/sentinel.reducer";
 
 import { SentinelComboBoxComponent } from './sentinel-combo-box.component';
 
 describe('SentinelComboBoxComponent', () => {
 	let component: SentinelComboBoxComponent;
 	let fixture: ComponentFixture<SentinelComboBoxComponent>;
+	let store: Store<any>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [SentinelComboBoxComponent],
-			imports: [FormsModule]
-		})
-			.compileComponents();
+			imports: [FormsModule,
+				StoreModule.forRoot({[sentinelFeatureKey]: SentinelReducer})]
+		}).compileComponents();
 	}));
 
-	beforeEach(() => {
+	beforeEach(inject([Store], (_store: Store<any>) => {
 		fixture = TestBed.createComponent(SentinelComboBoxComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
-	});
+		store = _store;
+
+		const mockStore = new Map<any, any>([
+			[selectSentinelLayers, ['TRUE_COLOR', 'NDVI']],
+			[selectSentinelselectedLayers, 'TRUE_COLOR']
+		]);
+
+		spyOn(store, 'select').and.callFake(selector => of(mockStore.get(selector)))
+	}));
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('changeLayer must fire SetSentinelLayerOnMap action', () => {
+		spyOn(store, 'dispatch');
+		component.mapId = 'mapId';
+		const layer = 'TRUE_COLOR';
+		component.changeLayer(layer);
+		expect(store.dispatch).toHaveBeenCalledWith(new SetSentinelLayerOnMap({id: component.mapId, layer: 'TRUE_COLOR'}))
+	})
 });
--- src/app/app/sentinel/services/sentinel-layers.service.spec.ts
+++ src/app/app/sentinel/services/sentinel-layers.service.spec.ts
@@ -1,9 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { MultipleOverlaysSourceConfig } from "@ansyn/ansyn";
+import { MAP_SOURCE_PROVIDERS_CONFIG } from "@ansyn/imagery";
+import { StoreModule } from '@ngrx/store';
+import { sentinelFeatureKey, SentinelReducer } from "../reducers/sentinel.reducer";
+
 
 import { SentinelLayersService } from './sentinel-layers.service';
 
 describe('SentinelLayersService', () => {
-	beforeEach(() => TestBed.configureTestingModule({}));
+	beforeEach(() => TestBed.configureTestingModule({
+		imports: [HttpClientModule,
+			StoreModule.forRoot({[sentinelFeatureKey]: SentinelReducer})],
+		providers: [{
+			provide: MAP_SOURCE_PROVIDERS_CONFIG,
+			useValue: {SENTINEL: {}}
+		},
+			{
+				provide: MultipleOverlaysSourceConfig,
+				useValue: {
+					indexProviders: {SENTINEL: {}}
+				}
+			}]
+	}));
 
 	it('should be created', () => {
 		const service: SentinelLayersService = TestBed.get(SentinelLayersService);

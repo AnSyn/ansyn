94d8c35da48ca11e1822715e0a96bb7e931f7f76
Tzahi Levi <thetzahilevi@gmail.com>
2019-04-30 08:57:14 +0300

    fix cp command

--- scripts/build-libs.sh
+++ scripts/build-libs.sh
@@ -5,4 +5,4 @@ for (( i=0; i<len; i++ ))
 do
     ng build @ansyn/${BUILDS[$i]} || exit 1
 done
-cp ../src/app/@ansyn/ansyn/assets/* ../dist/ansyn/ansyn/assets/
+cp -r src/app/@ansyn/ansyn/assets/ dist/ansyn/ansyn/

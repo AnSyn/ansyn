226507d7be9cec27453d7befe58418fa3b34f0f4
YairTawil <yairtawil@gmail.com>
2019-05-02 15:00:49 +0300

    fix: test

--- src/app/app/sentinel/services/sentinel-layers.service.spec.ts
+++ src/app/app/sentinel/services/sentinel-layers.service.spec.ts
@@ -1,14 +1,16 @@
-import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
-import { MultipleOverlaysSourceConfig } from "@ansyn/ansyn";
-import { MAP_SOURCE_PROVIDERS_CONFIG } from "@ansyn/imagery";
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TestBed, inject } from '@angular/core/testing';
+import { MultipleOverlaysSourceConfig } from '@ansyn/ansyn';
+import { MAP_SOURCE_PROVIDERS_CONFIG } from '@ansyn/imagery';
 import { StoreModule } from '@ngrx/store';
-import { sentinelFeatureKey, SentinelReducer } from "../reducers/sentinel.reducer";
-
-
+import { sentinelFeatureKey, SentinelReducer } from '../reducers/sentinel.reducer';
 import { SentinelLayersService } from './sentinel-layers.service';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('SentinelLayersService', () => {
+	let httpClient: HttpClient;
+	let sentinelLayersService: SentinelLayersService;
+
 	beforeEach(() => TestBed.configureTestingModule({
 		imports: [HttpClientModule,
 			StoreModule.forRoot({[sentinelFeatureKey]: SentinelReducer})],
@@ -24,8 +26,17 @@ describe('SentinelLayersService', () => {
 			}]
 	}));
 
+	beforeEach(inject([HttpClient], (_httpClient: HttpClient) => {
+		httpClient = _httpClient;
+		spyOn(httpClient, 'get').and.callFake(() => of(''));
+	}));
+
+	beforeEach(inject([SentinelLayersService], (_sentinelLayersService: SentinelLayersService) => {
+		sentinelLayersService = _sentinelLayersService;
+	}));
+
 	it('should be created', () => {
-		const service: SentinelLayersService = TestBed.get(SentinelLayersService);
-		expect(service).toBeTruthy();
+		expect(sentinelLayersService).toBeTruthy();
 	});
+
 });

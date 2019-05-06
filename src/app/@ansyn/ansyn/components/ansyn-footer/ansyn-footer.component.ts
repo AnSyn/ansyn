fa4941abe75634fffea15f3ee363b5e1fa5a5345
ramtob <ramtob@gmail.com>
2019-05-06 14:36:33 +0300

    Initial code for the new vertical toggle button

--- src/app/@ansyn/ansyn/components/ansyn-footer/ansyn-footer.component.ts
+++ src/app/@ansyn/ansyn/components/ansyn-footer/ansyn-footer.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Inject, Input, OnInit, ViewChild } from '@angular/core';
 import { ICaseMapState } from '../../modules/menu-items/cases/models/case.model';
+import { ContainerChangedTriggerAction, IMenuState } from '@ansyn/menu';
+import { Store } from '@ngrx/store';
+import { CoreConfig } from '../../modules/core/models/core.config';
+import { ICoreConfig } from '../../modules/core/models/core.config.model';
 
 @Component({
 	selector: 'ansyn-footer',
@@ -9,11 +13,41 @@ import { ICaseMapState } from '../../modules/menu-items/cases/models/case.model'
 export class AnsynFooterComponent implements OnInit {
 	@Input() selectedCaseName: string;
 	@Input() activeMap: ICaseMapState;
+	@Input() animatedElement: HTMLElement;
 
-	constructor() {
+	@ViewChild('footerWrapper') wrapperElement: ElementRef;
+
+	collapse = false;
+
+	constructor(
+		protected elementRef: ElementRef,
+		protected store: Store<IMenuState>,
+		@Inject(CoreConfig) public config: ICoreConfig
+	) {
 	}
 
 	ngOnInit() {
+	}
+
+	startToggleCollapse() {
+		this.collapse = !this.collapse;
+
+		this.wrapperElement.nativeElement.classList.toggle('collapsed');
+
+		this.forceRedraw()
+			.then(() => this.store.dispatch(new ContainerChangedTriggerAction()));
+
+		this.animatedElement.style.animation = this.collapse ? 'collapsed .3s' : 'unCollapsed .6s';
+	}
+
+	forceRedraw() {
+		return new Promise(resolve => {
+			this.elementRef.nativeElement.style.display = 'none';
+			requestAnimationFrame(() => {
+				this.elementRef.nativeElement.style.display = '';
+				resolve();
+			});
+		});
 	}
 
 }

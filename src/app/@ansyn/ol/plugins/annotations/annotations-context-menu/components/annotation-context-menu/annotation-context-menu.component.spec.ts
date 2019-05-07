e290f8f7958deae17e809471f50c5739b5d5ce09
YairTawil <yairtawil@gmail.com>
2019-05-07 08:01:16 +0300

    fix: tests

--- src/app/@ansyn/ol/plugins/annotations/annotations-context-menu/components/annotation-context-menu/annotation-context-menu.component.spec.ts
+++ src/app/@ansyn/ol/plugins/annotations/annotations-context-menu/components/annotation-context-menu/annotation-context-menu.component.spec.ts
@@ -40,6 +40,8 @@ describe('AnnotationContextMenuComponent', () => {
 		fixture = TestBed.createComponent(AnnotationContextMenuComponent);
 		component = fixture.componentInstance;
 		component.mapState = <any>{ id: 'mapId' };
+		spyOn(component, 'calcBoundingRect').and.returnValue({ top: `0px`, left: `0px`, width: `0px`, height: `0px`});
+		spyOn(component, 'getFeatureProps').and.returnValue({});
 		fixture.detectChanges();
 	});
 

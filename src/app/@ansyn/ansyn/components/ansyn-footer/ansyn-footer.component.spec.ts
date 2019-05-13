d31437064e070ee44a9ce8670ddd523cba4766c7
ramtob <ramtob@gmail.com>
2019-05-13 11:30:27 +0300

    Unit tests

--- src/app/@ansyn/ansyn/components/ansyn-footer/ansyn-footer.component.spec.ts
+++ src/app/@ansyn/ansyn/components/ansyn-footer/ansyn-footer.component.spec.ts
@@ -1,14 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnsynFooterComponent } from './ansyn-footer.component';
+import { MockComponent } from '../../modules/core/test/mock-component';
+import { StoreModule } from '@ngrx/store';
+import { CoreConfig } from '../../modules/core/models/core.config';
 
 describe('AnsynFooterComponent', () => {
 	let component: AnsynFooterComponent;
 	let fixture: ComponentFixture<AnsynFooterComponent>;
 
+	const mockStatusBar = MockComponent({
+		selector: 'ansyn-status-bar',
+		inputs: ['selectedCaseName', 'activeMap']
+	});
+	const mockOverlays = MockComponent({
+		selector: 'ansyn-overlays-container'
+	});
+
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [AnsynFooterComponent]
+			declarations: [
+				AnsynFooterComponent,
+				mockStatusBar,
+				mockOverlays
+			],
+			imports: [
+				StoreModule.forRoot({})
+			],
+			providers: [
+				{
+					provide: CoreConfig,
+					useValue: {}
+				}
+			]
 		})
 			.compileComponents();
 	}));

ce6b4543b5fb76738572aae554237f4d7478afa2
Tzahi Levi <thetzahilevi@gmail.com>
2019-05-23 14:08:22 +0300

    lint

--- src/app/@ansyn/ansyn/modules/alerts/components/alerts-container/alerts-container.component.spec.ts
+++ src/app/@ansyn/ansyn/modules/alerts/components/alerts-container/alerts-container.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AlertsContainerComponent } from './alerts-container.component';
 
 describe('AlertsContainerComponent', () => {
-  let component: AlertsContainerComponent;
-  let fixture: ComponentFixture<AlertsContainerComponent>;
+	let component: AlertsContainerComponent;
+	let fixture: ComponentFixture<AlertsContainerComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AlertsContainerComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [AlertsContainerComponent]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AlertsContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AlertsContainerComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });

617a1a79f5b59b2cdd3d7094112c8797645f0818
Tzahi Levi <thetzahilevi@gmail.com>
2019-05-29 13:54:56 +0300

    add test to measure control

--- src/app/@ansyn/ansyn/modules/menu-items/tools/components/measure-control/measure-control.component.spec.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/tools/components/measure-control/measure-control.component.spec.ts
@@ -1,14 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { mapFeatureKey, MapReducer, selectActiveMapId } from '@ansyn/map-facade';
+import { selectIsMeasureToolActive, toolsFeatureKey, ToolsReducer } from '../../reducers/tools.reducer';
 
 import { MeasureControlComponent } from './measure-control.component';
+import { StoreModule, Store } from '@ngrx/store';
 
+const mockStore = new Map<any, any>([
+	[selectActiveMapId, 'mapId']
+]);
 describe('MeasureControlComponent', () => {
 	let component: MeasureControlComponent;
 	let fixture: ComponentFixture<MeasureControlComponent>;
-
+	let store: Store<any>;
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [MeasureControlComponent]
+			declarations: [MeasureControlComponent],
+			imports: [
+				StoreModule.forRoot({ [toolsFeatureKey]: ToolsReducer,
+				[mapFeatureKey]: MapReducer})
+			]
 		})
 			.compileComponents();
 	}));
@@ -16,10 +26,22 @@ describe('MeasureControlComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(MeasureControlComponent);
 		component = fixture.componentInstance;
+		component.mapId = 'mapId';
 		fixture.detectChanges();
 	});
+
+	beforeEach(inject([Store], (_store: Store<any>) => {
+		store = _store;
+	}));
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('show should be false', () => {
+		mockStore.set(selectIsMeasureToolActive, false);
+		spyOn(store, 'select');
+		fixture.detectChanges();
+		expect(component.show).toBeFalsy();
+	})
 });

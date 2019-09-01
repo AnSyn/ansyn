fa8bfc768960d9d784c737b6fe961b9a469ca78f
Tzahi Levi <thetzahilevi@gmail.com>
2019-09-01 18:14:05 +0300

    fix zommer goes to infinity (#2501)

--- src/app/@ansyn/ansyn/modules/plugins/components/imagery-zoomer/imagery-zoomer.component.ts
+++ src/app/@ansyn/ansyn/modules/plugins/components/imagery-zoomer/imagery-zoomer.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IEntryComponent } from "@ansyn/map-facade";
-import { ImageryZoomerService } from "@ansyn/map-facade";
+import { IEntryComponent } from '@ansyn/map-facade';
+import { ImageryZoomerService } from '@ansyn/map-facade';
 
 @Component({
 	selector: 'ansyn-imagery-zoomer',

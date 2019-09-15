371b026235c45ef36712e4cee326c01c4ba3bbbf
Tzahi Levi <thetzahilevi@gmail.com>
2019-09-15 11:10:38 +0300

    imports

--- src/app/@ansyn/ansyn/modules/plugins/app/alerts/alerts.plugin.ts
+++ src/app/@ansyn/ansyn/modules/plugins/app/alerts/alerts.plugin.ts
@@ -8,7 +8,7 @@ import { OpenLayersDisabledMap, OpenLayersMap } from '@ansyn/ol';
 import { select, Store } from '@ngrx/store';
 import { combineLatest, Observable } from 'rxjs';
 import { selectMapPositionByMapId, selectOverlayByMapId } from '@ansyn/map-facade';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
 import { AutoSubscription } from 'auto-subscriptions';
 import { AlertMsgTypesEnum } from '../../../alerts/model';
 import { AddAlertMsg, RemoveAlertMsg } from '../../../overlays/overlay-status/actions/overlay-status.actions';

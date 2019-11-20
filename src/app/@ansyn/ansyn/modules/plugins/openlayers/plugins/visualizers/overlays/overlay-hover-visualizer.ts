8c83ae28efd4bada8be8985b91cd7eae90526781
ramtob <ramtob@gmail.com>
2019-11-20 16:14:10 +0200

    Now the footprint is shown on the map on hover (given appropriate position and zoom level)

--- src/app/@ansyn/ansyn/modules/plugins/openlayers/plugins/visualizers/overlays/overlay-hover-visualizer.ts
+++ src/app/@ansyn/ansyn/modules/plugins/openlayers/plugins/visualizers/overlays/overlay-hover-visualizer.ts
@@ -1,43 +1,33 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Inject } from '@angular/core';
 import { ImageryVisualizer, IVisualizersConfig, VisualizersConfig } from '@ansyn/imagery';
 import { Actions } from '@ngrx/effects';
 import { select, Store } from '@ngrx/store';
-import { selectActiveMapId } from '@ansyn/map-facade';
 import { AutoSubscription } from 'auto-subscriptions';
-import { map, tap, withLatestFrom } from 'rxjs/operators';
+import { map, switchMap, withLatestFrom } from 'rxjs/operators';
 import { EntitiesVisualizer, OpenLayersMap } from '@ansyn/ol';
 import { ExtendMap } from "../../../../../overlays/reducers/extendedMap.class";
 import { IMarkUpData, MarkUpClass, selectDropMarkup } from "../../../../../overlays/reducers/overlays.reducer";
 import { OverlaysService } from "../../../../../overlays/services/overlays.service";
 import { IOverlay } from "../../../../../overlays/models/overlay.model";
+import { UUID } from "angular2-uuid";
+import { feature } from '@turf/turf';
 
 @ImageryVisualizer({
 	supported: [OpenLayersMap],
 	deps: [Store, Actions, VisualizersConfig, OverlaysService]
 })
 export class OverlayHoverVisualizer extends EntitiesVisualizer {
-	public isActive = false;
 
 	@AutoSubscription
-	isActive$: Observable<boolean> = this.store$.pipe(
-		select(selectActiveMapId),
-		map((activeMapId: string) => activeMapId === this.mapId),
-		tap((isActive) => this.isActive = isActive),
-		tap(this.purgeCache.bind(this))
-	);
-
-	markups: ExtendMap<MarkUpClass, IMarkUpData>;
-
-	@AutoSubscription
-	hoveredOverlay$: Observable<IOverlay> = this.store$.pipe(
+	hoveredOverlay$: Observable<boolean> = this.store$.pipe(
 		select(selectDropMarkup),
 		map((markups: ExtendMap<MarkUpClass, IMarkUpData>) => markups && markups.get(MarkUpClass.hover)),
 		map((markUpData: IMarkUpData) => markUpData && markUpData.overlaysIds[0]),
 		withLatestFrom(this.overlaysService.getAllOverlays$),
 		map(([overlayId, overlays]: [string, Map<string, IOverlay>]) => overlayId && overlays.get(overlayId)),
-		tap((overlay: IOverlay) => {
-				this.onMarkupsChange(overlay);
+		switchMap((overlay: IOverlay) => {
+				return this.showOrHide$(overlay);
 			}
 		)
 	);
@@ -48,50 +38,26 @@ export class OverlayHoverVisualizer extends EntitiesVisualizer {
 		@Inject(VisualizersConfig) config: IVisualizersConfig,
 		public overlaysService: OverlaysService
 	) {
-		super(config.FrameVisualizer);
+		super(config.OverlayHoverVisualizer);
 		this.updateStyle({
 			opacity: 0.5,
 			initial: {
 				zIndex: 10,
 				fill: null,
-				stroke: this.getStroke.bind(this),
+				stroke: '#ff0eb9',
 				'stroke-width': 3
 			}
 		});
 	}
 
-	getStroke() {
-		return this.isActive ? this.visualizerStyle.colors.active : this.visualizerStyle.colors.inactive;
-	}
-
-	public purgeCache() {
-		if (this.source) {
-			const features = this.source.getFeatures();
-			if (features && features[0]) {
-				delete (<any>features[0]).styleCache;
-				this.source.refresh();
-			}
-		}
-		return;
-	}
-
-	private onMarkupsChange(hoveredOverlay: IOverlay) {
+	private showOrHide$(hoveredOverlay: IOverlay): Observable<boolean> {
 		if (hoveredOverlay) {
-			console.log('detected hover on overlay', hoveredOverlay);
+			const featureJson = feature(hoveredOverlay.footprint);
+			const entityToDraw = {id: UUID.UUID(), featureJson};
+			return this.setEntities([entityToDraw]);
 		} else {
-			console.log('detected no hover on overlay');
+			this.clearEntities();
+			return of(true);
 		}
 	}
-
-	// onResetView(): Observable<any> {
-	// 	this.clearEntities();
-	// 	this.initLayers();
-	// 	const footprint = this.iMap.getMainLayer().get('footprint');
-	// 	if (footprint) {
-	// 		const featureJson = feature(footprint);
-	// 		const entityToDraw = { id: UUID.UUID(), featureJson };
-	// 		return this.setEntities([entityToDraw]);
-	// 	}
-	// 	return of(true);
-	// }
 }

2169d83e5094e955db0291ccbee6b7cb364f497f
ramtob <ramtob@gmail.com>
2019-11-21 15:01:31 +0200

    Moving overlay hover display from polyline-visualizer to overlay-hover-visualizer

--- src/app/@ansyn/ansyn/modules/plugins/openlayers/plugins/visualizers/overlays/overlay-hover-visualizer.ts
+++ src/app/@ansyn/ansyn/modules/plugins/openlayers/plugins/visualizers/overlays/overlay-hover-visualizer.ts
@@ -1,10 +1,10 @@
 import { Observable, of } from 'rxjs';
 import { Inject } from '@angular/core';
-import { ImageryVisualizer, IVisualizersConfig, VisualizersConfig } from '@ansyn/imagery';
+import { IIcon, ImageryVisualizer, IVisualizersConfig, VisualizersConfig } from '@ansyn/imagery';
 import { Actions } from '@ngrx/effects';
 import { select, Store } from '@ngrx/store';
 import { AutoSubscription } from 'auto-subscriptions';
-import { map, switchMap, withLatestFrom } from 'rxjs/operators';
+import { map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
 import { EntitiesVisualizer, OpenLayersMap } from '@ansyn/ol';
 import { ExtendMap } from "../../../../../overlays/reducers/extendedMap.class";
 import { IMarkUpData, MarkUpClass, selectDropMarkup } from "../../../../../overlays/reducers/overlays.reducer";
@@ -12,16 +12,20 @@ import { OverlaysService } from "../../../../../overlays/services/overlays.servi
 import { IOverlay } from "../../../../../overlays/models/overlay.model";
 import { UUID } from "angular2-uuid";
 import { feature } from '@turf/turf';
+import { getIconSvg } from "./arrow-svg";
+import Feature from 'ol/Feature';
 
 @ImageryVisualizer({
 	supported: [OpenLayersMap],
 	deps: [Store, Actions, VisualizersConfig, OverlaysService]
 })
 export class OverlayHoverVisualizer extends EntitiesVisualizer {
+	markups: ExtendMap<MarkUpClass, IMarkUpData>;
 
 	@AutoSubscription
 	hoveredOverlay$: Observable<boolean> = this.store$.pipe(
 		select(selectDropMarkup),
+		tap((markups: ExtendMap<MarkUpClass, IMarkUpData>) => { this.markups = markups; }),
 		map((markups: ExtendMap<MarkUpClass, IMarkUpData>) => markups && markups.get(MarkUpClass.hover)),
 		map((markUpData: IMarkUpData) => markUpData && markUpData.overlaysIds[0]),
 		withLatestFrom(this.overlaysService.getAllOverlays$),
@@ -42,10 +46,14 @@ export class OverlayHoverVisualizer extends EntitiesVisualizer {
 		this.updateStyle({
 			opacity: 0.5,
 			initial: {
-				zIndex: 10,
-				fill: null,
-				stroke: '#ff0eb9',
-				'stroke-width': 3
+				zIndex: 4,
+				circle: (feature) => this.getRadius(feature, true),
+				fill: (feature) => this.getFillColor(feature, true),
+				'fill-opacity': (feature) => this.getFillOpacity(feature, true),
+				'stroke-width': (feature) => this.getStrokeWidth(feature, true),
+				'stroke': (feature) => this.getStrokeColor(feature, true),
+				'stroke-opacity': (feature) => this.getStrokeOpacity(feature, true),
+				icon: (feature) => this.getImage(feature, true),
 			}
 		});
 	}
@@ -60,4 +68,118 @@ export class OverlayHoverVisualizer extends EntitiesVisualizer {
 			return of(true);
 		}
 	}
+
+	// Start: code partly shared with polyline-visualizer
+
+	private propsByFeature(feature: Feature) {
+		const classes = this.markups.findKeysByValue(<string>feature.getId(), 'overlaysIds');
+
+		const isFavorites = classes.includes(MarkUpClass.favorites);
+		const isActive = classes.includes(MarkUpClass.active);
+		const isDisplayed = classes.includes(MarkUpClass.displayed);
+
+		return { isFavorites, isActive, isDisplayed };
+	}
+
+	private getFeatureType(feature: Feature): 'MultiLineString' | 'Point' | 'LineString' | 'MultiPolygon' {
+		const type = feature && feature.getGeometry().getType();
+		if (type && !['MultiLineString', 'Point', 'LineString', 'MultiPolygon'].includes(type)) {
+			console.warn(`polyline-visualizer.ts getFeatureType - unsupported type ${type}`);
+		}
+		return type;
+	}
+
+	private getRadius(feature: Feature, hover = true) {
+		if (this.getFeatureType(feature) === 'Point') {
+			return hover ? 8 : 7;
+		}
+		return undefined;
+	}
+
+	private getFillOpacity(feature: Feature, hover = true) {
+		switch (this.getFeatureType(feature)) {
+			case 'MultiLineString':
+			case 'MultiPolygon':
+				return hover ? 0.4 : 1;
+			case 'Point' :
+				return hover ? 1 : 0.8;
+			case 'LineString':
+				return undefined;
+			default:
+				return hover;
+		}
+	}
+
+	private getFillColor(feature: Feature, hover = true) {
+		const { active, display, favorite, inactive } = this.visualizerStyle.colors;
+		const { isActive, isFavorites, isDisplayed } = this.propsByFeature(feature);
+		switch (this.getFeatureType(feature)) {
+			case 'MultiLineString':
+			case 'MultiPolygon':
+				return hover ? 'white' : 'transparent';
+			case 'Point' :
+				return isFavorites ? favorite : isActive ? active : !isDisplayed ? inactive : display;
+			case 'LineString':
+				return undefined;
+		}
+	}
+
+	private getStrokeColor(feature: Feature, hover = true) {
+		const { active, display, favorite, inactive } = this.visualizerStyle.colors;
+		const { isActive, isDisplayed, isFavorites } = this.propsByFeature(feature);
+
+		switch (this.getFeatureType(feature)) {
+			case 'MultiLineString':
+			case 'MultiPolygon':
+				return isActive ? active : isDisplayed ? display : inactive;
+			case 'Point' :
+				return display;
+			case 'LineString':
+				return isFavorites ? favorite : isActive ? active : display;
+		}
+	}
+
+	private getStrokeOpacity(feature: Feature, hover = true) {
+		if (this.getFeatureType(feature) === 'LineString') {
+			return hover ? 1 : 0.5
+		}
+		return 1;
+	}
+
+	private getStrokeWidth(feature: Feature, hover = true) {
+		const { isActive, isDisplayed, isFavorites } = this.propsByFeature(feature);
+		switch (feature.getGeometry().getType()) {
+			case 'MultiLineString':
+			case 'MultiPolygon':
+				return isFavorites ? 4 : (isActive || isDisplayed || hover) ? 5 : 3;
+			case 'Point' :
+				return undefined;
+			case 'LineString':
+				return hover ? 7 : 5;
+		}
+	}
+
+	private getImage(feature, hover = true): IIcon | undefined {
+		if (this.getFeatureType(feature) === 'LineString') {
+			const coordinates = feature.getGeometry().getCoordinates();
+			const rotation = this.calcArrowAngle(coordinates[0], coordinates[1]);
+			const color = this.getStrokeColor(feature);
+			return {
+				src: getIconSvg(hover ? this.colorWithAlpha(color, 0.5) : color),
+				anchor: [0.5, 0.5],
+				scale: 0.15,
+				rotation,
+				rotateWithView: true
+			}
+		}
+		return undefined;
+	}
+
+	private calcArrowAngle(previousPoint: [number, number], currentPoint: [number, number]): number {
+		const dx = currentPoint[0] - previousPoint[0];
+		const dy = currentPoint[1] - previousPoint[1];
+		return Math.atan2(dx, dy);
+	}
+
+	// End: code partly shared with polyline-visualizer
 }

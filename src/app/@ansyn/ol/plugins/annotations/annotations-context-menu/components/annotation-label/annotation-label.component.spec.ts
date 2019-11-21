0c82f3226c7506df79d6ed00ec1ca7328e68e849
Tzahi Levi <thetzahilevi@gmail.com>
2019-11-21 15:31:06 +0200

    fix tests

--- src/app/@ansyn/ol/plugins/annotations/annotations-context-menu/components/annotation-label/annotation-label.component.spec.ts
+++ src/app/@ansyn/ol/plugins/annotations/annotations-context-menu/components/annotation-label/annotation-label.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatInputModule, MatSelectModule } from '@angular/material';
 
 import { AnnotationLabelComponent } from './annotation-label.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AnnotationLabelComponent', () => {
 	let component: AnnotationLabelComponent;
@@ -8,7 +12,8 @@ describe('AnnotationLabelComponent', () => {
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [AnnotationLabelComponent]
+			declarations: [AnnotationLabelComponent],
+			imports: [BrowserAnimationsModule, FormsModule, MatInputModule, MatSelectModule, TranslateModule.forRoot()]
 		})
 			.compileComponents();
 	}));

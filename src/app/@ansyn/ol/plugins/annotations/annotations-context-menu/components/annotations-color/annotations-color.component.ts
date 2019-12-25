427adee192feb89fe7b702579437aa4b8b8ef1a8
ramtob <ramtob@gmail.com>
2019-12-25 16:43:28 +0200

    Trying to implement a similar method with annotations-color component

--- src/app/@ansyn/ol/plugins/annotations/annotations-context-menu/components/annotations-color/annotations-color.component.ts
+++ src/app/@ansyn/ol/plugins/annotations/annotations-context-menu/components/annotations-color/annotations-color.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
 export interface IAnnotationColorProps {
 	fill: string;
@@ -12,7 +12,7 @@ export interface IAnnotationColorProps {
 	templateUrl: './annotations-color.component.html',
 	styleUrls: ['./annotations-color.component.less']
 })
-export class AnnotationsColorComponent {
+export class AnnotationsColorComponent implements OnInit, AfterViewInit, OnDestroy {
 	@Input() show: boolean;
 	@Input() strokeModeActive = true;
 	@Input() fillModeActive = true;
@@ -21,6 +21,41 @@ export class AnnotationsColorComponent {
 	@Output() colorChange = new EventEmitter();
 
 
-	constructor() {
+	timerId: number;
+	imageryElement: Element;
+	moveLeft = 0;
+
+	constructor(protected myElement: ElementRef) {
+	}
+
+	ngOnInit(): void {
+	}
+
+	ngAfterViewInit(): void {
+		this.imageryElement = (this.myElement.nativeElement as HTMLElement).closest('.imagery');
+		if (this.imageryElement) {
+			this.timerId = window.setInterval(this.calcPosition.bind(this), 300);
+		}
+	}
+
+	ngOnDestroy(): void {
+		if (this.timerId) {
+			window.clearInterval(this.timerId);
+		}
+	}
+
+	calcPosition() {
+		const myRect = this.myElement.nativeElement.getBoundingClientRect();
+		const imageryRect = this.imageryElement.getBoundingClientRect() as DOMRect;
+		const delta = myRect.left - imageryRect.left + myRect.width - imageryRect.width + 3;
+		if (delta > 0) {
+			this.moveLeft += delta;
+		} else {
+			this.moveLeft = Math.max(0, this.moveLeft + delta);
+		}
+	}
+
+	getStyle() {
+		return { 'transform': `translate(-${ this.moveLeft }px)` };
 	}
 }

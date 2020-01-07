9999fe4c1ad698632558b448f8203122f55b0a63
ramtob <ramtob@gmail.com>
2020-01-07 10:27:39 +0200

    Unit test

--- src/app/@ansyn/ol/helpers/stay-in-imagery.service.spec.ts
+++ src/app/@ansyn/ol/helpers/stay-in-imagery.service.spec.ts
@@ -3,7 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { StayInImageryService } from './stay-in-imagery.service';
 
 describe('StayInImageryService', () => {
-	beforeEach(() => TestBed.configureTestingModule({}));
+	beforeEach(() => TestBed.configureTestingModule({
+		providers: [StayInImageryService]
+	}));
 
 	it('should be created', () => {
 		const service: StayInImageryService = TestBed.get(StayInImageryService);

68ad886a2311093a7ea1edc72a42fd8161cf166a
sulimani55 <55398513+sulimani55@users.noreply.github.com>
2020-03-03 16:07:23 +0200

    Fixing popup to answer standarts (#2944)
    * Fixing popup to answer standarts
    
    * fix(export-maps-popup.component.spec.ts): updating the tests so it matches the credential config

--- src/app/@ansyn/ansyn/modules/menu-items/tools/export-maps-popup/export-maps-popup.component.spec.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/tools/export-maps-popup/export-maps-popup.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExportMapsPopupComponent } from './export-maps-popup.component';
 import { ImageryCommunicatorService } from '@ansyn/imagery';
@@ -6,6 +6,10 @@ import { MatDialog, MatDialogRef } from '@angular/material';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { StoreModule } from '@ngrx/store';
 import { mapFeatureKey, MapReducer } from '@ansyn/map-facade';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
+import { credentialsConfig } from '../../../core/services/credentials/config';
+
 
 describe('ExportMapsPopupComponent', () => {
 	let component: ExportMapsPopupComponent;
@@ -14,10 +18,19 @@ describe('ExportMapsPopupComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [ExportMapsPopupComponent],
-			imports: [MatProgressBarModule, StoreModule.forRoot({[mapFeatureKey]: MapReducer})],
+			imports: [MatProgressBarModule,
+				HttpClientModule,
+				StoreModule.forRoot({ [mapFeatureKey]: MapReducer }),
+				TranslateModule.forRoot()],
 			providers: [ImageryCommunicatorService,
-						{ provide: MatDialogRef, useValue: {} },
-						{ provide: MatDialog, useValue: {} }]
+				{ provide: MatDialogRef, useValue: {} },
+				{ provide: MatDialog, useValue: {} },
+				{
+					provide: credentialsConfig,
+					useValue: {
+						noCredentialsMessage: 'TEST'
+					}
+				}]
 		})
 			.compileComponents();
 	}));
@@ -30,6 +43,5 @@ describe('ExportMapsPopupComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
-
 	});
 });

707643887df3ac7e74647fc1c419f117699e6831
Tzahi Levi <thetzahilevi@gmail.com>
2020-03-08 18:01:16 +0200

    integration_on_8_3_20 (#2963)
    * integration_on_8_3_20
    
    * test
    
    * fix build-libs

--- src/app/@ansyn/ol/projection/free-layer-projection.ts
+++ src/app/@ansyn/ol/projection/free-layer-projection.ts
@@ -1,46 +1,44 @@
 import TileLayer from 'ol/layer/Tile';
-import { add, get } from 'ol/proj/projections';
-import { remove as removeTransformFunc } from 'ol/proj/transforms';
+import { add, get as getProjection } from 'ol/proj/projections';
+import { remove as removeTransformFunc, get as getTransform } from 'ol/proj/transforms';
 import { EPSG_3857, EPSG_4326 } from '@ansyn/imagery';
 
 TileLayer.prototype.disposeLayer = (layer) => {
+	function removeTransform(fromProjectionCode, toProjection) {
+		const toProjectionCode = toProjection.getCode();
+		const fromProjection = getProjection(fromProjectionCode);
+		let isExistTransform = getTransform(fromProjectionCode, toProjectionCode);
+		if (isExistTransform) {
+			removeTransformFunc(fromProjection, toProjection);
+		}
+		isExistTransform = getTransform(toProjectionCode, fromProjectionCode);
+		if (isExistTransform) {
+			removeTransformFunc(toProjection, fromProjection);
+		}
+	}
+
 	const source = layer.getSource();
 	if (Boolean(source)) {
 		let projection = source.projection || source.getProjection();
 		const code = projection && projection.getCode();
-		if (!projection || !Boolean(code) || code === 'EPSG:4236' || code === EPSG_3857) {
+		if (!projection || !Boolean(code) || code === EPSG_4326 || code === EPSG_3857) {
 			return;
 		}
 
-		const like4326 = get(EPSG_4326);
-		removeTransformFunc(like4326, projection);
-		let isExistTransform = get(projection, like4326);
-		if (isExistTransform) {
-			removeTransformFunc(projection, like4326);
-		}
+		removeTransform(EPSG_4326, projection);
+		removeTransform(EPSG_3857, projection);
+		removeTransform(code, projection);
 
-		const like3857 = get(EPSG_3857);
-		removeTransformFunc(like3857, projection);
-		isExistTransform = get(projection, like3857);
-		if (isExistTransform) {
-			removeTransformFunc(projection, like3857);
-		}
-		isExistTransform = get(projection, projection);
-		if (isExistTransform) {
-			removeTransformFunc(projection, projection);
-		}
 		if (projection.tag) {
 			projection.tag.dispose();
-			delete(projection.tag);
+			delete (projection.tag);
 			projection.tag = undefined;
-			projection = undefined;
 		}
 
-		if (projection.dispose) {
+		if (projection && projection.dispose) {
 			projection.dispose();
-			projection = undefined;
-		}
+			}
+		projection = undefined;
 		add(code, undefined);
-
 	}
 };

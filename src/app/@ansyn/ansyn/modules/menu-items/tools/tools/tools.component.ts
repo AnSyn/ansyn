8998049684ea97a358087dc9832e007ce13c375a
ramtob <ramtob@gmail.com>
2020-03-31 12:38:00 +0300

    Adding ? guards (crash in console)

--- src/app/@ansyn/ansyn/modules/menu-items/tools/tools/tools.component.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/tools/tools/tools.component.ts
@@ -74,23 +74,23 @@ export class ToolsComponent implements OnInit, OnDestroy {
 	}
 
 	get isGeoOptionsDisabled() {
-		return !this.flags.get(toolsFlags.geoRegisteredOptionsEnabled);
+		return !this.flags?.get(toolsFlags.geoRegisteredOptionsEnabled);
 	}
 
 	get imageProcessingDisabled() {
-		return this.flags.get(toolsFlags.imageProcessingDisabled);
+		return this.flags?.get(toolsFlags.imageProcessingDisabled);
 	}
 
 	get shadowMouseDisabled() {
-		return this.flags.get(toolsFlags.shadowMouseDisabled);
+		return this.flags?.get(toolsFlags.shadowMouseDisabled);
 	}
 
 	get onShadowMouse() {
-		return this.flags.get(toolsFlags.shadowMouse);
+		return this.flags?.get(toolsFlags.shadowMouse);
 	}
 
 	get onAutoImageProcessing() {
-		return this.flags.get(toolsFlags.autoImageProcessing);
+		return this.flags?.get(toolsFlags.autoImageProcessing);
 	}
 
 	get imageManualProcessingDisabled() {
@@ -98,7 +98,7 @@ export class ToolsComponent implements OnInit, OnDestroy {
 	}
 
 	get onMeasureTool() {
-		return this.flags.get(toolsFlags.isMeasureToolActive);
+		return this.flags?.get(toolsFlags.isMeasureToolActive);
 	}
 
 	// @TODO display the shadow mouse only if there more then one map .

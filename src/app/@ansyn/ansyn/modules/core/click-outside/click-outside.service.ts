bf6c435a4b95b2f7cd70fa848b9faded654b5c0a
Tzahi Levi <thetzahilevi@gmail.com>
2020-04-19 14:23:29 +0300

    make click outside as global service

--- src/app/@ansyn/ansyn/modules/core/click-outside/click-outside.service.ts
+++ src/app/@ansyn/ansyn/modules/core/click-outside/click-outside.service.ts
@@ -1,26 +1,19 @@
 import { Injectable, OnDestroy, OnInit, ElementRef } from '@angular/core';
-import { Subscription, fromEvent } from 'rxjs';
+import { Subscription, fromEvent, Observable } from 'rxjs';
 import { tap, map } from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ClickOutsideService{
-	clickOutsideSubscription: Subscription;
-	open: boolean;
-
 	constructor(protected element: ElementRef) {
-		console.log('constructor', element);
 	}
 
 
-	onClickOutside(classQuery?: string) { // classQuery is the class of the element that toggle this.element
-		return fromEvent(window, 'click').pipe(
-			tap(() => console.log(this.element.nativeElement.tagName)),
-			map( event => {
-				const target = event.target;
-				let queryElement = classQuery && window.document.getElementsByClassName(classQuery);
-				let shouldNotCallback = this.check(this.element.nativeElement.children, event.target) || (queryElement && this.check(queryElement, event.target));
+	onClickOutside(targetElement?: any): Observable<boolean> { // classQuery is the class of the element that toggle this.element
+		return fromEvent(targetElement || window, 'click').pipe(
+			map( (event: MouseEvent) => {
+				let shouldNotCallback = this.check(this.element.nativeElement.children, event.target);
 				return !shouldNotCallback;
 			})
 		)
@@ -40,26 +33,6 @@ export class ClickOutsideService{
 		}
 
 		return shouldNotCallback;
-	}
-
-	bind(callback) {
-		this.clickOutsideSubscription = fromEvent(window, 'click').pipe(
-			tap( event => {
-				let shouldNotCallback = false;
-				const children = this.element.nativeElement.children;
-				// we use this loop instead of path.include for chrome 44
-				for (let i = 0; i < children.length && !shouldNotCallback; i++) {
-					shouldNotCallback = event.srcElement === children.item(i);
-				}
-				if (!shouldNotCallback) {
-					callback();
-				}
-			})
-		).subscribe()
-	}
-
-	destroy(): void {
-		this.clickOutsideSubscription.unsubscribe()
 	}
 
 }

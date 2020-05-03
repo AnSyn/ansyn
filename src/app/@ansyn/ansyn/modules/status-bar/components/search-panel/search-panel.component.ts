2b6882da196a8b0e0bb65a370bec49d16177fa23
Tzahi Levi <thetzahilevi@gmail.com>
2020-05-03 15:52:49 +0300

    separed title and coordinates and style inner circle (#3121)

--- src/app/@ansyn/ansyn/modules/status-bar/components/search-panel/search-panel.component.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/search-panel/search-panel.component.ts
@@ -45,6 +45,7 @@ export class SearchPanelComponent implements OnInit, OnDestroy {
 	dataInputFilterTitle = 'All';
 	timeSelectionTitle: string;
 	geoFilterTitle: string;
+	geoFilterCoordinates: string;
 	dataInputFilters: ICaseDataInputFiltersState;
 
 	@AutoSubscription
@@ -73,8 +74,8 @@ export class SearchPanelComponent implements OnInit, OnDestroy {
 	).pipe(
 		withLatestFrom(this.store$.select(selectRegion)),
 		tap(([[geoFilterType, active], region]) => {
-			const sCoordinates = region ? ` (${region.coordinates.toString()})` : '';
-			this.geoFilterTitle = `${geoFilterType}${sCoordinates}`;
+			this.geoFilterTitle = `${geoFilterType}`;
+			this.geoFilterCoordinates = region ? `${region.coordinates.toString()}` : '';
 			this.locationPickerExpand = active;
 		})
 	);

43af4f69b55f13adb99ee1225eaecabad932fd9a
Sulimani55 <tktur9@gmail.com>
2020-05-06 13:23:17 +0300

    fixing effect

--- src/app/@ansyn/ansyn/app-effects/effects/overlays.app.effects.ts
+++ src/app/@ansyn/ansyn/app-effects/effects/overlays.app.effects.ts
@@ -52,7 +52,7 @@ import {
 	DisplayOverlaySuccessAction,
 	LoadOverlaysAction,
 	LoadOverlaysSuccessAction,
-	OverlaysActionTypes,
+	OverlaysActionTypes, SetFilteredOverlaysAction,
 	SetHoveredOverlayAction,
 	SetMarkUp,
 	SetTotalOverlaysAction
@@ -274,7 +274,7 @@ export class OverlaysAppEffects {
 
 	@Effect()
 	updateResultTableBadge$: Observable<SetBadgeAction> = this.actions$.pipe(
-		ofType<SetTotalOverlaysAction>(OverlaysActionTypes.SET_TOTAL_OVERLAYS),
+		ofType<SetFilteredOverlaysAction | SetTotalOverlaysAction>(OverlaysActionTypes.SET_FILTERED_OVERLAYS, OverlaysActionTypes.SET_TOTAL_OVERLAYS),
 		map((action) => new SetBadgeAction({ key: 'Results table', badge: `${ action.payload.length }` })));
 
 	onDropMarkupFilter([prevAction, currentAction]): boolean {

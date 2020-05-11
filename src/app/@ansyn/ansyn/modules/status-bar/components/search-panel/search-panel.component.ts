848547b04026ff7235b9387bb708a8bc0a2b3de6
Tzahi Levi <thetzahilevi@gmail.com>
2020-05-11 16:30:52 +0300

    selection logic

--- src/app/@ansyn/ansyn/modules/status-bar/components/search-panel/search-panel.component.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/search-panel/search-panel.component.ts
@@ -4,7 +4,7 @@ import { IStatusBarConfig } from '../../models/statusBar-config.model';
 import { IStatusBarState, selectGeoFilterActive, selectGeoFilterType } from '../../reducers/status-bar.reducer';
 import { StatusBarConfig } from '../../models/statusBar.config';
 import { Store } from '@ngrx/store';
-import { combineLatest, fromEvent, Observable } from 'rxjs';
+import { combineLatest, fromEvent, merge, Observable } from 'rxjs';
 import { animate, style, transition, trigger } from '@angular/animations';
 import { AnimationTriggerMetadata } from '@angular/animations/src/animation_metadata';
 import { filter, tap } from 'rxjs/operators';
@@ -55,6 +55,7 @@ const DATE_FORMAT = 'DD/MM/YYYY HH:mm';
 export class SearchPanelComponent implements OnInit, OnDestroy {
 	popupExpanded = new Map<SearchPanelTitle, boolean>([['DataInputs', false], ['TimePicker', false], ['LocationPicker', false], ['TimePickerPreset', false]]);
 	timeRange: Date[];
+	timeError: { from: boolean, to: boolean };
 	dataInputFilterTitle: string;
 	timeSelectionTitle: { from: string, to: string };
 	geoFilterTitle: string;
@@ -71,6 +72,10 @@ export class SearchPanelComponent implements OnInit, OnDestroy {
 				this.timeSelectionTitle = {
 					from: moment(this.timeRange[0]).format(DATE_FORMAT),
 					to: moment(this.timeRange[1]).format(DATE_FORMAT)
+				};
+				this.timeError = {
+					from: !this.validateDate(this.timeSelectionTitle.from),
+					to: !this.validateDate(this.timeSelectionTitle.to)
 				}
 			}
 		})
@@ -115,45 +120,48 @@ export class SearchPanelComponent implements OnInit, OnDestroy {
 	}
 
 	@AutoSubscription
-	timeInputChangeFrom$ = () => fromEvent(this.timePickerInputFrom.nativeElement, 'keydown').pipe(
+	timeInputChangeFrom$ = () => merge(
+		fromEvent(this.timePickerInputFrom.nativeElement, 'keydown'),
+		fromEvent(this.timePickerInputTo.nativeElement, 'keydown')
+	).pipe(
 		tap(this.checkForTimeContentOk.bind(this))
 	);
 
 	@AutoSubscription
-	timeInputChangeTo$ = () => fromEvent(this.timePickerInputTo.nativeElement, 'keydown').pipe(
-		tap(this.checkForTimeContentOk.bind(this))
+	loggerManualChangeTime$ = () => merge(
+		fromEvent(this.timePickerInputFrom.nativeElement, 'keyup'),
+		fromEvent(this.timePickerInputTo.nativeElement, 'keyup')
+	).pipe(
+		filter(isDigitKey),
+		tap((event: any) => this.loggerService.info('change From date manually ' + event.target.textContent))
 	);
 
 	@AutoSubscription
-	loggerManualChangeTimeFrom$ = () => fromEvent(this.timePickerInputFrom.nativeElement, 'keyup').pipe(
-			filter( isDigitKey),
-			tap( (event: any) =>  this.loggerService.info('change From date manually ' + event.target.textContent))
+	onSelectTitle$ = () => merge(fromEvent(this.timePickerInputFrom.nativeElement, 'mouseup'),
+		fromEvent(this.timePickerInputTo.nativeElement, 'mouseup')).pipe(
+		tap(this.selectOnlyNumber.bind(this))
 	);
 
-
 	@AutoSubscription
-	loggerManualChangeTimeTo$ = () => fromEvent(this.timePickerInputTo.nativeElement, 'keyup').pipe(
-		filter( isDigitKey),
-		tap( (event: any) =>  this.loggerService.info('change To date manually ' + event.target.textContent))
+	disableDragText$ = () => merge(fromEvent(this.timePickerInputFrom.nativeElement, 'dragstart'),
+		fromEvent(this.timePickerInputTo.nativeElement, 'dragstart')).pipe(
+		tap( (event: DragEvent) =>  event.preventDefault())
 	);
 
 	ngOnInit() {
 	}
 
-	closeTimePicker() {
-		this.popupExpanded.set('TimePicker', false);
-		this.popupExpanded.set('TimePickerPreset', false);
-	}
-
-	toggleExpander(popup: SearchPanelTitle) {
+	toggleExpander(popup: SearchPanelTitle, forceState?: boolean) {
 		if (this.isDataInputsOk()) {
-			const newState = !this.popupExpanded.get(popup);
+			const newState = forceState || !this.popupExpanded.get(popup);
 			this.popupExpanded.forEach((_, key, map) => {
-				map.set(key , key === popup ? newState : false)
+				map.set(key, key === popup ? newState : false)
 			});
-		}
-		else {
-			this.store$.dispatch(new SetToastMessageAction({toastText: 'Please select at least one type', showWarningIcon: true}));
+		} else {
+			this.store$.dispatch(new SetToastMessageAction({
+				toastText: 'Please select at least one type',
+				showWarningIcon: true
+			}));
 
 		}
 	}
@@ -169,56 +177,109 @@ export class SearchPanelComponent implements OnInit, OnDestroy {
 		return this.dataInputFilters.fullyChecked || this.dataInputFilters.filters.length > 0;
 	}
 
+	// Time Picker functions
+	// TODO: refactor eject timepicker logic outside search panel
 	checkForTimeContentOk(event: any) {
 		if (isArrowRightKey(event) || isArrowLeftKey(event)) {
+			if (event.shiftKey) {
+				this.selectOnlyNumber();
+			}
 			return;
 		}
 		if (isBackspaceKey(event)) {
 			const selection = window.getSelection();
-			const charToRemove = selection.focusNode.textContent.charAt(selection.focusOffset - 1);
-			if (charToRemove === '/' || charToRemove === ':') {
-				event.preventDefault();
+			if (selection.type === 'Caret' && !/[ \/:]/g.test(selection.baseNode.textContent.charAt(selection.baseOffset - 1))) {
+				return;
 			}
-			return;
+			selection.deleteFromDocument();
 		}
 		if (isDigitKey(event)) {
 			this.loggerService.info('change date manual ' + this.timePickerInputFrom.nativeElement.textContent + ' - ' + this.timePickerInputTo.nativeElement.textContent)
 			return;
 		}
 		if (isEnterKey(event)) {
-			if (this.validateDates()) {
-				const from = this.getDateFromString(this.timePickerInputFrom.nativeElement.textContent);
-				const to = this.getDateFromString(this.timePickerInputTo.nativeElement.textContent);
-				this.store$.dispatch(new SetOverlaysCriteriaAction({
-					time: {
-						type: 'absolute',
-						from,
-						to
-					}
-				}))
+			if (!this.setTimeCriteria()) {
+				this.store$.dispatch(new SetToastMessageAction({ toastText: 'Invalid date' }));
 			}
-			else {
-				this.store$.dispatch(new SetToastMessageAction({toastText: "Invalid date"}));
-			}
-			this.loggerService.info(`press enter on time picker ${this.timePickerInputFrom.nativeElement.textContent} - ${this.timePickerInputTo.nativeElement.textContent}`);
+			this.loggerService.info(`press enter on time picker ${ this.timePickerInputFrom.nativeElement.textContent } - ${ this.timePickerInputTo.nativeElement.textContent }`);
 		}
 		if (isEscapeKey(event)) {
-			this.timePickerInputTo.nativeElement.textContent = this.timeSelectionTitle.to;
-			this.timePickerInputFrom.nativeElement.textContent = this.timeSelectionTitle.from;
+			this.revertTime();
 		}
-		this.closeTimePicker();
 		event.preventDefault();
 	}
 
-	validateDates() {
-		return this.validateDate(this.timePickerInputFrom.nativeElement.textContent) && this.validateDate(this.timePickerInputTo.nativeElement.textContent)
+	openTimePickerPreset() {
+		this.toggleExpander('TimePickerPreset', true);
 	}
-	validateDate(date) {
-		return moment(date, DATE_FORMAT, true).isValid();
+
+	closeTimePicker() {
+		window.getSelection().empty();
+		this.popupExpanded.set('TimePicker', false);
+		this.popupExpanded.set('TimePickerPreset', false);
+		if (!this.setTimeCriteria()) {
+			this.revertTime();
+		}
+	}
+
+	setTimeCriteria() {
+		if (this.validateDates()) {
+			const from = this.getDateFromString(this.timePickerInputFrom.nativeElement.textContent);
+			const to = this.getDateFromString(this.timePickerInputTo.nativeElement.textContent);
+			this.store$.dispatch(new SetOverlaysCriteriaAction({
+				time: {
+					type: 'absolute',
+					from,
+					to
+				}
+			}));
+			return true;
+		}
+		return false;
+	}
+
+	selectOnlyNumber() {
+		const selection = window.getSelection();
+		if (selection.type === 'Range') {
+			const { baseOffset, extentOffset, baseNode, extentNode} = selection;
+			const ltr = baseOffset < extentOffset;
+			const minIndex = Math.min(baseOffset, extentOffset);
+			const maxIndex = Math.max(baseOffset, extentOffset);
+			const textContent = baseNode.textContent;
+			const contentToRemove = textContent.substring(minIndex, maxIndex);
+			if (contentToRemove.split('').some(letterToRemove => ['/', ':', ' '].includes(letterToRemove))) {
+				const offset = this.findExtentOffset(contentToRemove, !ltr);
+
+				selection.setBaseAndExtent(baseNode, baseOffset, extentNode, ltr ? baseOffset + offset: baseOffset - offset);
+			}
+		}
+	}
+
+	validateDates() {
+		this.timeError.from = !this.validateDate(this.timePickerInputFrom.nativeElement.textContent);
+		this.timeError.to = !this.validateDate(this.timePickerInputTo.nativeElement.textContent);
+		return !this.timeError.from && !this.timeError.to;
 	}
 
 	getDateFromString(date) {
 		return moment(date, DATE_FORMAT, true).toDate();
+	}
+
+	revertTime() {
+		this.timePickerInputTo.nativeElement.textContent = this.timeSelectionTitle.to;
+		this.timePickerInputFrom.nativeElement.textContent = this.timeSelectionTitle.from;
+		this.timeError.from = false;
+		this.timeError.to = false;
+	}
+
+	private validateDate(date) {
+		return moment(date, DATE_FORMAT, true).isValid();
+	}
+
+	private findExtentOffset(content: string, fromLast: boolean) {
+		const reg = /[ \/:]/g;
+		const reverse = content.split('').reverse().join('');
+		return fromLast ? reverse.search(reg) : content.search(reg)
 	}
 
 }

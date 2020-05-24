ef8feccd7173fbe44d0795bad082e9b2021a507f
Tzahi Levi <thetzahilevi@gmail.com>
2020-05-24 11:53:36 +0300

    change name

--- src/app/app/sentinel/sentinel-source-provider.ts
+++ src/app/app/sentinel/sentinel-source-provider.ts
@@ -157,7 +157,7 @@ export class SentinelSourceProvider extends BaseOverlaySourceProvider {
 			sourceType: this.sourceType,
 			isGeoRegistered: GeoRegisteration.geoRegistered,
 			tag: sentinelElement,
-			icon: 'lavian',
+			icon: 'satellite',
 			sensorLocation: this.multipleOverlaysSourceConfig.useAngleDebugMode ? getPointByGeometry(geometry) : undefined
 		});
 	}

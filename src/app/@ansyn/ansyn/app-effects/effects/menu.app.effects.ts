93bedb1ebeeffffe9a8635d1b267becb5edcbcd1
sulimani55 <55398513+sulimani55@users.noreply.github.com>
2020-05-27 14:10:15 +0300

    Fix reset button (#3262)
    * fixing badge after opening menu
    
    * fixing reset button in component mode
    
    * fixing tests
    
    * fix libs
    
    * refactor after cr

--- src/app/@ansyn/ansyn/app-effects/effects/menu.app.effects.ts
+++ src/app/@ansyn/ansyn/app-effects/effects/menu.app.effects.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 import { IAppState } from '../app.effects.module';
 import { select, Store } from '@ngrx/store';
 import { UpdateMapSizeAction } from '@ansyn/map-facade';
-import { MenuActionTypes, SetAutoClose } from '@ansyn/menu';
+import { IMenuConfig, MenuActionTypes, MenuConfig, SetAutoClose } from '@ansyn/menu';
 import { selectSubMenu } from '../../modules/menu-items/tools/reducers/tools.reducer';
 import { map, mergeMap } from 'rxjs/operators';
 import { RedrawTimelineAction, SetTotalOverlaysAction } from '../../modules/overlays/actions/overlays.actions';
 import { LoadDefaultCaseAction } from '../../modules/menu-items/cases/actions/cases.actions';
 import { selectDropsWithoutSpecialObjects } from '../../modules/overlays/reducers/overlays.reducer';
 import { IOverlayDrop } from '../../modules/overlays/models/overlay.model';
+import { COMPONENT_MODE } from '../../app-providers/component-mode';
 
 @Injectable()
 export class MenuAppEffects {
@@ -40,12 +41,30 @@ export class MenuAppEffects {
 		);
 
 	@Effect()
-	onResetApp$: Observable<LoadDefaultCaseAction> = this.actions$
+	onResetApp$ = this.actions$
 		.pipe(
 			ofType(MenuActionTypes.RESET_APP),
-			map(() => new LoadDefaultCaseAction())
+			mergeMap(() => {
+				if (this.componentMode) {
+					window.open(this.menuConfig.baseUrl, '_blank');
+					return EMPTY;
+				}
+
+				return [new LoadDefaultCaseAction()];
+			})
 		);
 
-	constructor(protected actions$: Actions, protected store$: Store<IAppState>) {
+	resetApp() {
+		if (!this.componentMode) {
+			window.open(this.menuConfig.baseUrl, '_blank');
+			return new LoadDefaultCaseAction();
+		}
+
+		return EMPTY;
+	}
+
+	constructor(protected actions$: Actions, protected store$: Store<IAppState>,
+				@Inject(COMPONENT_MODE) public componentMode: boolean,
+				@Inject(MenuConfig) public menuConfig: IMenuConfig) {
 	}
 }

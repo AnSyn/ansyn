b367fe504a09f3984fbd03e2d061ee919f445b06
suzanz <suzanz@codevalue.net>
2020-06-21 10:16:09 +0300

    resolve comment - add cesium and webpack issue reference

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,4 +1,4 @@
-/* This file was added in order to overcome a bug with Cesium and webpack- it enables custom webpack config. */
+/* This file was added in order to overcome a bug with Cesium and webpack (CesiumGS/cesium#8673) - it enables custom webpack config. */
 // TODO - file should be removed in Cesium future versions.
 module.exports = {
 	node: {

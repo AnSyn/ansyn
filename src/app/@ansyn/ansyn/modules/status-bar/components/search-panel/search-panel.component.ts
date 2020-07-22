aa7f71083c0fff541d3b24108a2584eaea9af2df
Sulimani55 <tktur9@gmail.com>
2020-07-22 17:46:42 +0300

    preventing the timepicker from picking a date in the future

--- src/app/@ansyn/ansyn/modules/status-bar/components/search-panel/search-panel.component.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/search-panel/search-panel.component.ts
@@ -317,7 +317,8 @@ export class SearchPanelComponent implements OnInit, OnDestroy {
 	}
 
 	private validateDate(date) {
-		return moment(date, DATE_FORMAT, true).isValid();
+		const dateFromFormat = moment(date, DATE_FORMAT, true);
+		return dateFromFormat.isValid() && dateFromFormat.toDate().getTime() <= Date.now();
 	}
 
 	private findExtentOffset(content: string, fromLast: boolean) {

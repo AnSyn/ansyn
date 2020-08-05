087df58aa90b205d37e55c1a8818f517f70f20ad
Tzahi Levi <thetzahilevi@gmail.com>
2020-08-05 13:50:14 +0300

    open help menu on app initializer

--- src/app/@ansyn/ansyn/modules/menu-items/help/help.module.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/help/help.module.ts
@@ -1,23 +1,41 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { HelpComponent } from './components/help.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CommonModule } from '@angular/common';
-import { EffectsModule } from '@ngrx/effects';
 import { HelpLocalStorageService } from './services/help.local-storage.service';
-import { HelpEffects } from './effects/help.effects';
 import { FormsModule } from '@angular/forms';
 import { CoreModule } from '../../core/core.module';
+import { IMenuConfig, IMenuState, MenuConfig, SelectMenuItemAction } from '@ansyn/menu';
+import { Store } from '@ngrx/store';
 
 @NgModule({
 	imports: [
 		CoreModule,
 		CommonModule,
 		FormsModule,
-		CarouselModule.forRoot(),
-		EffectsModule.forFeature([HelpEffects])
+		CarouselModule.forRoot()
 	],
-	providers: [HelpLocalStorageService],
+	providers: [
+		HelpLocalStorageService,
+		{
+			provide: APP_INITIALIZER,
+			useFactory: helpAppInitializeFactory,
+			deps: [Store, MenuConfig, HelpLocalStorageService],
+			multi: true
+
+		}
+		],
 	declarations: [HelpComponent],
 })
 export class HelpModule {
+}
+
+export function helpAppInitializeFactory(store: Store<IMenuState>, menuConfig: IMenuConfig, helpStorage: HelpLocalStorageService) {
+	return function () {
+		const { menuItems } = menuConfig;
+		const { dontShowHelpOnStartup } = helpStorage.getHelpLocalStorageData();
+		if ( (!Boolean(menuItems) || menuItems.includes('Help')) && !dontShowHelpOnStartup) {
+			store.dispatch(new SelectMenuItemAction({ menuKey: 'Help' }));
+		}
+	};
 }

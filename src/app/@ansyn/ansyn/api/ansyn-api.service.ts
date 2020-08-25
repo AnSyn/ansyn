5123cb644c9233b5a3aa2e83890639494233295a
Tzahi Levi <thetzahilevi@gmail.com>
2020-08-25 13:14:16 +0300

    fix build:libs

--- src/app/@ansyn/ansyn/api/ansyn-api.service.ts
+++ src/app/@ansyn/ansyn/api/ansyn-api.service.ts
@@ -1,7 +1,7 @@
 import { EventEmitter, Inject, Injectable, NgModuleRef } from '@angular/core';
 import {
-	getPolygonByPointAndRadius,
 	getPolygonByBufferRadius,
+	getPolygonByPointAndRadius,
 	ImageryCommunicatorService,
 	ImageryMapPosition,
 	IMapSettings
@@ -17,6 +17,7 @@ import {
 	SetLayoutAction,
 	SetMapPositionByRadiusAction,
 	SetMapPositionByRectAction,
+	SetMinimalistViewModeAction,
 	ShadowMouseProducer,
 	ToggleFooter
 } from '@ansyn/map-facade';
@@ -40,11 +41,7 @@ import {
 	selectActiveAnnotationLayer,
 	selectLayersEntities
 } from '../modules/menu-items/layers-manager/reducers/layers.reducer';
-import {
-	GoToAction,
-	SetActiveCenter,
-	ToolsActionsTypes
-} from '../modules/menu-items/tools/actions/tools.actions';
+import { GoToAction, SetActiveCenter, ToolsActionsTypes } from '../modules/menu-items/tools/actions/tools.actions';
 import {
 	DisplayOverlayAction,
 	DisplayOverlaySuccessAction,
@@ -62,10 +59,10 @@ import {
 import { ToggleMenuCollapse } from '@ansyn/menu';
 import { UUID } from 'angular2-uuid';
 import { DataLayersService } from '../modules/menu-items/layers-manager/services/data-layers.service';
-import { SetMinimalistViewModeAction } from '@ansyn/map-facade';
-import { AnnotationsVisualizer } from "@ansyn/ol";
+import { AnnotationsVisualizer } from '@ansyn/ol';
 
 type mapIdOrNumber = string | number | undefined;
+
 @Injectable({
 	providedIn: 'root'
 })
@@ -77,11 +74,9 @@ type mapIdOrNumber = string | number | undefined;
  * a api for manipulating on the ansyn app.
  */
 export class AnsynApi {
-	private mapList;
-	private features = [];
 	/**
 	 * the id of the active map.
-	 * @var {string}
+	 * @var
 	 */
 	activeMapId;
 	/**
@@ -96,7 +91,7 @@ export class AnsynApi {
 	activeAnnotationLayer;
 	/**
 	 * the id of the default layer.
-	 * @var {string}.
+	 * @var {string}
 	 */
 	defaultLayerId: string;
 	events = {
@@ -106,44 +101,66 @@ export class AnsynApi {
 		onReady: new EventEmitter<boolean>(),
 		/**
 		 * fire when the timeline was load with overlays.
-		 * @property {IOverlay[] | false} overlays - array of the loaded overlays or false if there was error.
+		 * @property overlays - array of the loaded overlays or false if there was error.
 		 */
 		overlaysLoadedSuccess: new EventEmitter<IOverlay[] | false>(),
 		/**
 		 * fire when an overlay was loaded to the map
-		 * @property {IOverlay | false} the loaded overlay or false if there was error on open the overlay.
-		 * @property {string} mapId - the id of the map.
+		 * @property the loaded overlay or false if there was error on open the overlay.
+		 * @property mapId - the id of the map.
 		 */
 		displayOverlaySuccess: new EventEmitter<{ overlay: IOverlay | false, mapId: string }>()
 	};
 	/** @deprecated onReady as own events was deprecated use events.onReady instead */
 	onReady = new EventEmitter<boolean>(true);
-
 	/**
-	 * @return {Observable} An observable that emits the maps in array.
+	 * @return An observable that emits the maps in array.
 	 */
 	getMaps$: Observable<IMapSettings[]> = this.store.pipe(
 		select(selectMapsList),
 		take(1)
 	);
-
+	/**
+	 * @return an observable that emit the current mouse position on the map.
+	 */
+	onShadowMouseProduce$: Observable<any> = this.actions$.pipe(
+		ofType(MapActionTypes.SHADOW_MOUSE_PRODUCER),
+		map(({ payload }: ShadowMouseProducer) => {
+			return payload.point.coordinates;
+		})
+	);
+	/**
+	 * @return An observable that emit when the map layout is change
+	 */
+	onSetLayoutSuccess$: Observable<any> = this.actions$.pipe(
+		ofType(MapActionTypes.SET_LAYOUT_SUCCESS)
+	);
+	/**
+	 * @return An observable that emit when the center of the map is change
+	 * from the search box or the go-to tool
+	 */
+	getActiveCenter$: Observable<any> = this.actions$.pipe(
+		ofType(ToolsActionsTypes.SET_ACTIVE_CENTER),
+		map(({ payload }: SetActiveCenter) => {
+			return payload;
+		})
+	);
+	private mapList;
+	private features = [];
 	@AutoSubscription
 	private activateMap$: Observable<string> = this.store.select(selectActiveMapId).pipe(
 		tap((activeMapId) => this.activeMapId = activeMapId)
 	);
-
 	@AutoSubscription
 	private mapsEntities$: Observable<Dictionary<IMapSettings>> = this.store.pipe(
 		select(selectMaps),
 		tap((mapsEntities) => this.mapsEntities = mapsEntities)
 	);
-
 	@AutoSubscription
 	private mapList$ = this.store.pipe(
 		select(selectMapsList),
-		tap( (mapList) => this.mapList = mapList)
+		tap((mapList) => this.mapList = mapList)
 	);
-
 	@AutoSubscription
 	private activeAnnotationLayer$: Observable<ILayer> = this.store
 		.pipe(
@@ -155,8 +172,6 @@ export class AnsynApi {
 				this.defaultLayerId = this.activeAnnotationLayer ? this.activeAnnotationLayer.id : undefined;
 			})
 		);
-
-
 	/** Events **/
 	@AutoSubscription
 	private ready$ = this.imageryCommunicatorService.instanceCreated.pipe(
@@ -167,6 +182,7 @@ export class AnsynApi {
 		})
 	);
 
+	/** Events end **/
 	@AutoSubscription
 	private overlaysSearchEnd$ = this.actions$.pipe(
 		ofType<LoadOverlaysSuccessAction>(OverlaysActionTypes.LOAD_OVERLAYS_SUCCESS),
@@ -174,7 +190,6 @@ export class AnsynApi {
 			this.events.overlaysLoadedSuccess.emit(payload.length > 0 ? payload : false);
 		})
 	);
-
 	@AutoSubscription
 	private displayOverlaySuccess$ = this.actions$.pipe(
 		ofType<DisplayOverlaySuccessAction>(OverlaysActionTypes.DISPLAY_OVERLAY_SUCCESS),
@@ -183,7 +198,6 @@ export class AnsynApi {
 			mapId: payload.mapId
 		}))
 	);
-
 	@AutoSubscription
 	private displayOverlayFailed$ = this.actions$.pipe(
 		ofType<DisplayOverlaySuccessAction>(OverlaysActionTypes.DISPLAY_OVERLAY_FAILED),
@@ -191,35 +205,6 @@ export class AnsynApi {
 			overlay: false,
 			mapId: payload.mapId
 		}))
-	);
-
-	/** Events end **/
-
-	/**
-	 * @return {Observable} an observable that emit the current mouse position on the map.
-	 */
-	onShadowMouseProduce$: Observable<any> = this.actions$.pipe(
-		ofType(MapActionTypes.SHADOW_MOUSE_PRODUCER),
-		map(({ payload }: ShadowMouseProducer) => {
-			return payload.point.coordinates;
-		})
-	);
-	/**
-	 * @return {Observable} An observable that emit when the map layout is change
-	 */
-	onSetLayoutSuccess$: Observable<any> = this.actions$.pipe(
-		ofType(MapActionTypes.SET_LAYOUT_SUCCESS)
-	);
-
-	/**
-	 * @return {Observable} An observable that emit when the center of the map is change
-	 * from the search box or the go-to tool
-	 */
-	getActiveCenter$: Observable<any> = this.actions$.pipe(
-		ofType(ToolsActionsTypes.SET_ACTIVE_CENTER),
-		map(({ payload }: SetActiveCenter) => {
-			return payload;
-		})
 	);
 
 	constructor(protected store: Store<any>,
@@ -232,26 +217,9 @@ export class AnsynApi {
 		this.init();
 	}
 
-	private getMapIdFromMapNumber(mapId: mapIdOrNumber): string {
-		if (!Boolean(mapId)) {
-			return this.activeMapId;
-		}
-		if (typeof mapId === 'string') {
-			return mapId;
-		}
-		return (mapId < 0 || mapId >= 4) && this.mapList[mapId - 1] ? this.mapList[mapId - 1].id : this.activeMapId;
-	}
-
-	private removeElement(id): void {
-		const elem: HTMLElement = <any>document.getElementById(id);
-		if (elem) {
-			elem.innerHTML = '';
-		}
-	}
-
 	/**
 	 * draw the shadow mouse visualizer in the specific point
-	 * @param {Point} geoPoint The point where we will draw the visualizer.
+	 * @param geoPoint The point where we will draw the visualizer.
 	 */
 	setOutSourceMouseShadow(geoPoint: Point): void {
 		if (!Boolean(geoPoint)) {
@@ -266,22 +234,24 @@ export class AnsynApi {
 
 	/**
 	 * open an overlay on the map
-	 * @param {IOverlay} overlay the overlay will be display
-	 * @param {mapIdOrNumber} mapId the map number or id. default the active map id.
+	 * @param overlay the overlay will be display
+	 * @param mapId the map number or id. default the active map id.
 	 */
 	displayOverLay(overlay: IOverlay, mapId?: mapIdOrNumber): void {
 		if (!Boolean(overlay)) {
 			console.error('can\'t display undefined overlay');
 			return null;
 		}
-		this.store.dispatch(new DisplayOverlayAction({ overlay,
+		this.store.dispatch(new DisplayOverlayAction({
+			overlay,
 			mapId: this.getMapIdFromMapNumber(mapId),
-			forceFirstDisplay: true }));
+			forceFirstDisplay: true
+		}));
 	}
 
 	/**
 	 * draw the feature collection on the map.
-	 * @param {FeatureCollection} featureCollection the features to draw on the map.
+	 * @param featureCollection the features to draw on the map.
 	 */
 	setAnnotations(featureCollection: FeatureCollection<any>): void {
 		if (!Boolean(featureCollection)) {
@@ -311,7 +281,7 @@ export class AnsynApi {
 
 	/**
 	 * feed the timeline with the given overlays
-	 * @param {Array<IOverlay>>} overlays array of IOverlay to be load into the timeline
+	 * @param overlays array of IOverlay to be load into the timeline
 	 */
 	setOverlays(overlays: IOverlay[]): void {
 		if (!Boolean(overlays)) {
@@ -323,7 +293,7 @@ export class AnsynApi {
 
 	/**
 	 * change the application layout
-	 * @param {LayoutKey} layout the new layout key.
+	 * @param layout the new layout key.
 	 * @return Observable that emit once the layout successfully change.
 	 */
 	changeMapLayout(layout: LayoutKey): Observable<any> {
@@ -337,10 +307,10 @@ export class AnsynApi {
 
 	/**
 	 * convert coordinate from one coordinate system to another
-	 * @param {Array<number>} position the position in the old coordinate system
-	 * @param {ICoordinatesSystem} convertFrom the current coordinate system of the position
-	 * @param {ICoordinatesSystem} convertTo the new coordinate system to the position
-	 * @return {Array<number>} the position in the new coordinate system.
+	 * @param position the position in the old coordinate system
+	 * @param convertFrom the current coordinate system of the position
+	 * @param convertTo the new coordinate system to the position
+	 * @return the position in the new coordinate system.
 	 */
 	transfromHelper(position: number[], convertFrom: ICoordinatesSystem, convertTo: ICoordinatesSystem): void {
 		const conversionValid = this.projectionConverterService.isValidConversion(position, convertFrom);
@@ -350,8 +320,8 @@ export class AnsynApi {
 	}
 
 	/**
-	 * @param {mapIdOrNumber} mapId the map id or number, default the active map id.
-	 * @return {ImageryMapPosition} the position of the map.
+	 * @param mapId the map id or number, default the active map id.
+	 * @return the position of the map.
 	 */
 	getMapPosition(mapId: mapIdOrNumber): ImageryMapPosition {
 		return this.mapsEntities[this.getMapIdFromMapNumber(mapId)].data.position;
@@ -359,8 +329,8 @@ export class AnsynApi {
 
 	/**
 	 * set the center of the map to the given point
-	 * @param {Point} geoPoint the new center for the map.
-	 * @param {mapIdOrNumber} mapId the map id or number, default the active map id.
+	 * @param geoPoint the new center for the map.
+	 * @param mapId the map id or number, default the active map id.
 	 */
 	goToPosition(geoPoint: Point, mapId?: mapIdOrNumber): void {
 		if (!Boolean(geoPoint)) {
@@ -372,8 +342,8 @@ export class AnsynApi {
 
 	/**
 	 * set the map extent to the given polygon
-	 * @param {Polygon} rect a rectangle polygon
-	 * @param {mapIdOrNumber} mapId the id of the map to be set. default the active map id.
+	 * @param rect a rectangle polygon
+	 * @param mapId the id of the map to be set. default the active map id.
 	 */
 	setMapPositionByRect(rect: Polygon, mapId?: mapIdOrNumber) {
 		if (!Boolean(rect)) {
@@ -384,7 +354,7 @@ export class AnsynApi {
 	}
 
 	/**
-	 * @return {Array<IOverlay>}all the overlays in the timeline.
+	 * @return all the overlays in the timeline.
 	 */
 	getOverlays(): Observable<IOverlay[]> {
 		return combineLatest(this.store.select(selectOverlaysArray), this.store.select(selectFilteredOveralys)).pipe(
@@ -399,8 +369,8 @@ export class AnsynApi {
 
 	/**
 	 * rotate the map by degree. if it is not geo registered image rotate the image.
-	 * @param {number} radian The rotation of the map in radians.
-	 * @param {mapIdOrNumber} mapId the id of the map. default to the active map.
+	 * @param radian The rotation of the map in radians.
+	 * @param mapId the id of the map. default to the active map.
 	 */
 	setRotation(radian: number, mapId?: mapIdOrNumber) {
 		if (radian === undefined) {
@@ -412,25 +382,30 @@ export class AnsynApi {
 
 	/**
 	 * get the current rotation of the map.
-	 * @param {mapIdOrNumber} mapId the id of the map. default to the active map.
+	 * @param mapId the id of the map. default to the active map.
+	 * @return the map rotation in radians.
 	 */
-	getRotation(mapId?: mapIdOrNumber) {
+	getRotation(mapId?: mapIdOrNumber): number {
 		return this.imageryCommunicatorService.provide(this.getMapIdFromMapNumber(mapId)).getRotation();
 	}
 
 	/**
 	 * set the map center to the specific point and scale the map to the radius
-	 * @param {Point} center GeoJSON point to be set the map to.
-	 * @param {number} radiusInMeters set the map scale to radiusInMeters / 10
-	 * @param {mapIdOrNumber} mapId the id of the map to be set. default the active map id.
-	 * @param {boolean} search does make new search in the new position or not.
+	 * @param center GeoJSON point to be set the map to.
+	 * @param radiusInMeters set the map scale to radiusInMeters / 10
+	 * @param mapId the id of the map to be set. default the active map id.
+	 * @param search does make new search in the new position or not.
 	 */
-	setMapPositionByRadius(center: Point, radiusInMeters: number, mapId: mapIdOrNumber, search: boolean = false) {
+	setMapPositionByRadius(center: Point, radiusInMeters: number, mapId: mapIdOrNumber, search: boolean = false): void {
 		if (!Boolean(center)) {
 			console.error('can\'t set position to undefined point');
 			return null;
 		}
-		this.store.dispatch(new SetMapPositionByRadiusAction({ id: this.getMapIdFromMapNumber(mapId), center, radiusInMeters }));
+		this.store.dispatch(new SetMapPositionByRadiusAction({
+			id: this.getMapIdFromMapNumber(mapId),
+			center,
+			radiusInMeters
+		}));
 		if (search) {
 			const criteria: IOverlaysCriteria = {
 				region: center
@@ -441,8 +416,8 @@ export class AnsynApi {
 
 	/**
 	 * make a new search with the specific criteria
-	 * @param {IOverlaysCriteria} criteria of the search. you can pass only one criterion.
-	 * @param {number} [radiusInMetersBuffer] if the region criterion is provide, bump the region by the radius.
+	 * @param criteria of the search. you can pass only one criterion.
+	 * @param [radiusInMetersBuffer] if the region criterion is provide, bump the region by the radius.
 	 */
 	setOverlaysCriteria(criteria: IOverlaysCriteria, radiusInMetersBuffer?: number) {
 		if (!Boolean(criteria)) {
@@ -467,14 +442,14 @@ export class AnsynApi {
 	}
 
 	/**
-	 * @return {Observable} An Observable that emits when the search criteria was change.
+	 * @return An Observable that emits when the search criteria was change.
 	 */
 	getOverlaysCriteria(): Observable<IOverlaysCriteria> {
 		return this.store.select(selectOverlaysCriteria);
 	}
 
 	/**
-	 * @return {IOverlay} the overlay that display on the map, or undefined if no overlay was display
+	 * @return the overlay that display on the map, or undefined if no overlay was display
 	 * @param mapId the id of the map.
 	 */
 	getOverlayData(mapId?: mapIdOrNumber): IOverlay {
@@ -483,7 +458,7 @@ export class AnsynApi {
 
 	/**
 	 * set if the footer(the timeline container) is minimize or maximize.
-	 * @param {boolean} collapse true for minimize the footer, or false to maximize.
+	 * @param collapse true for minimize the footer, or false to maximize.
 	 */
 	collapseFooter(collapse: boolean) {
 		this.store.dispatch(new ToggleFooter(collapse));
@@ -491,7 +466,7 @@ export class AnsynApi {
 
 	/**
 	 * set if the menu is minimize or maximize.
-	 * @param {boolean} collapse true for minimize the menu, or false to maximize.
+	 * @param collapse true for minimize the menu, or false to maximize.
 	 */
 	collapseMenu(collapse: boolean) {
 		this.store.dispatch(new ToggleMenuCollapse(collapse));
@@ -500,7 +475,7 @@ export class AnsynApi {
 	/**
 	 * set the app to minimalist view.
 	 * minimalist view show only the map the compass and the name of the overlay(or Base map, if there is no overlay display)/
-	 * @param {boolean} collapse true to set the app to minimalist view, false to set the app to normal view.
+	 * @param collapse true to set the app to minimalist view, false to set the app to normal view.
 	 */
 	setMinimalistViewMode(collapse: boolean) {
 		this.collapseFooter(collapse);
@@ -510,10 +485,10 @@ export class AnsynApi {
 
 	/**
 	 * add new layer to the Data Layer
-	 * @param {string}layerName the layer name.
-	 * @param {FeatureCollection} layerData the features to be add into the layer.
-	 * @param {boolean} isEditable set if this layer was editable or not. default true
-	 * @return {string} the layer id. for future deletion of the layer.
+	 * @param layerName the layer name.
+	 * @param layerData the features to be add into the layer.
+	 * @param isEditable set if this layer was editable or not. default true
+	 * @return the layer id. for future deletion of the layer.
 	 */
 	insertLayer(layerName: string, layerData: FeatureCollection<any>, isEditable: boolean = true): string {
 		if (!(layerName && layerName.length)) {
@@ -539,7 +514,7 @@ export class AnsynApi {
 
 	/**
 	 * remove the specific layer from the Data layer.
-	 * @param {string} layerId the id of the layer who return from {inserLayer}
+	 * @param layerId the id of the layer who return from
 	 */
 	removeLayer(layerId: string): void {
 		if (!(layerId && layerId.length)) {
@@ -551,8 +526,8 @@ export class AnsynApi {
 
 	/**
 	 * set if the specific layer is show or hide.
-	 * @param {string} layerId the id of the layer who return from {inserLayer}
-	 * @param {boolean} show true to show the layer, false to hide.
+	 * @param layerId the id of the layer who return from
+	 * @param show true to show the layer, false to hide.
 	 */
 	showLayer(layerId: string, show: boolean): void {
 		if (!(layerId && layerId.length)) {
@@ -568,6 +543,23 @@ export class AnsynApi {
 	destroy(): void {
 		this.moduleRef.destroy();
 		this.removeElement(this.id);
+	}
+
+	private getMapIdFromMapNumber(mapId: mapIdOrNumber): string {
+		if (!Boolean(mapId)) {
+			return this.activeMapId;
+		}
+		if (typeof mapId === 'string') {
+			return mapId;
+		}
+		return (mapId < 0 || mapId >= 4) && this.mapList[mapId - 1] ? this.mapList[mapId - 1].id : this.activeMapId;
+	}
+
+	private removeElement(id): void {
+		const elem: HTMLElement = <any>document.getElementById(id);
+		if (elem) {
+			elem.innerHTML = '';
+		}
 	}
 
 	private generateFeaturesIds(annotationsLayer): void {

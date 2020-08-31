8a7bb17a15b0a1f375f48b521f8875ec22303bb7
Sulimani55 <tktur9@gmail.com>
2020-08-31 10:08:11 +0300

    fixing code

--- src/app/@ansyn/ansyn/modules/menu-items/cases/services/cases.service.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/cases/services/cases.service.ts
@@ -175,11 +175,7 @@ export class CasesService {
 		return this.storageService.create(this.linksConfig.schema, link)
 			.pipe(
 				take(1),
-				catchError(err => {
-					console.log('err', err);
-					return this.errorHandlerService.httpErrorHandle(err, 'Failed to create case');
-				})
-			);
+				catchError(err => this.errorHandlerService.httpErrorHandle(err, 'Failed to create case')));
 	}
 
 	updateCase(selectedCase: ICase): Observable<IStoredEntity<ICasePreview, IDilutedCaseState>> {

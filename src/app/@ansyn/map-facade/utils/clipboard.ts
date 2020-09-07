50eeb5ae4ddeedd4a1f3b4357279609534c24c6d
Sulimani55 <tktur9@gmail.com>
2020-09-07 09:51:47 +0300

    fixing lint error

--- src/app/@ansyn/map-facade/utils/clipboard.ts
+++ src/app/@ansyn/map-facade/utils/clipboard.ts
@@ -1,4 +1,12 @@
+interface IClipboard {
+	writeText(newClipText: string): Promise<void>;
+}
+
+interface INavigatorClipboard extends Navigator {
+	readonly clipboard?: IClipboard;
+}
+
 export function copyFromContent(content: string): Promise<any> {
-	return navigator.clipboard.writeText(content);
+	return (navigator as INavigatorClipboard).clipboard.writeText(content);
 }
 

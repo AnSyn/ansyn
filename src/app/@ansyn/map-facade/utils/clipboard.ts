01c398a476cff3d29d1470a0d6cc42c38e99492a
Tzahi Levi <thetzahilevi@gmail.com>
2020-09-07 12:25:45 +0300

    remove navigator interface

--- src/app/@ansyn/map-facade/utils/clipboard.ts
+++ src/app/@ansyn/map-facade/utils/clipboard.ts
@@ -1,12 +1,4 @@
-interface IClipboard {
-	writeText(newClipText: string): Promise<void>;
-}
-
-interface INavigatorClipboard extends Navigator {
-	readonly clipboard?: IClipboard;
-}
-
 export function copyFromContent(content: string): Promise<any> {
-	return (navigator as INavigatorClipboard).clipboard.writeText(content);
+	return navigator.clipboard.writeText(content);
 }
 

08627b60a40ebae6eab1d9298886169b74e4890d
Tzahi Levi <thetzahilevi@gmail.com>
2020-09-08 14:32:43 +0300

    on import layer go to layer bbox

--- src/app/@ansyn/ansyn/modules/menu-items/layers-manager/components/import-layer/import-layer.component.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/layers-manager/components/import-layer/import-layer.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, EventEmitter } from '@angular/core';
 import { AutoSubscription, AutoSubscriptions } from 'auto-subscriptions';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { DataLayersService } from '../../services/data-layers.service';
 import { AddLayer } from '../../actions/layers.actions';
-import { fromEvent, Observable } from 'rxjs';
+import { fromEvent, Observable, EMPTY } from 'rxjs';
 import { UUID } from 'angular2-uuid';
-import { SetToastMessageAction } from '@ansyn/map-facade';
-import { tap } from 'rxjs/operators';
+import { selectActiveMapId, SetMapPositionByRectAction, SetToastMessageAction } from '@ansyn/map-facade';
+import { tap, map, withLatestFrom, catchError } from 'rxjs/operators';
 import { FeatureCollection } from 'geojson';
 import KmlFormat from 'ol/format/KML';
 import GeoJSONFormat from 'ol/format/GeoJSON';
 import * as shapeFile from 'shapefile';
 import { getErrorMessageFromException } from '../../../../core/utils/logs/timer-logs';
-import { getInitialAnnotationsFeatureProperties } from '@ansyn/imagery';
+import {
+	bboxFromGeoJson,
+	polygonFromBBOX,
+	validateFeatureProperties
+} from '@ansyn/imagery';
 
 @Component({
 	selector: 'ansyn-import-layer',
@@ -29,38 +33,51 @@ export class ImportLayerComponent implements OnInit, OnDestroy {
 	geoJsonFormat = new GeoJSONFormat();
 	file: File;
 	fileType: string;
+	onReadLayer$ = new EventEmitter<{data: any, name: string}>();
 
 	@AutoSubscription
 	onFileLoad$: Observable<any> = fromEvent(this.reader, 'load').pipe(
 		tap(() => {
 			const layerName = this.file.name.slice(0, this.file.name.lastIndexOf('.'));
-			let layerData;
+			let layerData = {name: layerName, data: null};
 			try {
 				const readerResult: string = <string>this.reader.result;
 				switch (this.fileType.toLowerCase()) {
 					case 'kml':
 						const features = this.kmlFormat.readFeatures(readerResult);
-						layerData = JSON.parse(this.geoJsonFormat.writeFeatures(features));
-						this.simpleStyleToVisualizer(layerData);
+						layerData.data = this.geoJsonFormat.writeFeaturesObject(features);
+						this.onReadLayer$.emit(layerData);
 						break;
 					case 'json':
 					case 'geojson':
-						layerData = JSON.parse(readerResult);
+						layerData.data = JSON.parse(readerResult);
+						this.onReadLayer$.emit(layerData);
 						break;
 					case 'shp':
-						this.addShapeFileLayer(layerName);
-						return;
+						this.addShapeFileLayer(readerResult).then( (data) => {
+							this.onReadLayer$.emit({...layerData. data});
+						});
+						break;
 					default:
 						throw new Error('Can\'t read file type');
 				}
-
-				this.generateFeatureCollection(layerData, layerName);
 			} catch (error) {
-				this.store.dispatch(new SetToastMessageAction({
-					showWarningIcon: true,
-					toastText: getErrorMessageFromException(error, 'Failed to import file')
-				}));
+				this.importError(error);
 			}
+		})
+	);
+
+	@AutoSubscription
+	onReadLayerSuccess$ = this.onReadLayer$.pipe(
+		map( (layer) => {
+			this.generateFeatureCollection(layer.data, layer.name);
+			return this.calculateLayerBbox(layer.data);
+		}),
+		withLatestFrom(this.store.pipe(select(selectActiveMapId))),
+		tap( ([layerBbox, activeMapId]) => this.store.dispatch(new SetMapPositionByRectAction({id: activeMapId, rect: layerBbox}))),
+		catchError((error) => {
+			this.importError(error);
+			return EMPTY;
 		})
 	);
 
@@ -77,22 +94,14 @@ export class ImportLayerComponent implements OnInit, OnDestroy {
 		}
 	}
 
-	addShapeFileLayer(layerName) {
-		let layerData;
-		shapeFile.read(this.reader.result).then(geoJson => {
-			layerData = geoJson;
-			layerData.features.forEach( feature => {
-				feature.properties = getInitialAnnotationsFeatureProperties();
-				feature.properties.id = feature.id;
-			});
-			this.generateFeatureCollection(layerData, layerName);
-		}).catch(e => console.error(e));
+	addShapeFileLayer(data) {
+		return shapeFile.read(data).catch(e => {throw new Error(e)});
 
 	}
 
 	generateFeatureCollection(layerData, layerName) {
 		if (this.isFeatureCollection(layerData)) {
-			this.generateFeaturesIds(layerData);
+			this.ValidateFeatures(layerData);
 			const isNonEditable = this.isNonEditable(layerData);
 			const layer = this.dataLayersService.generateAnnotationLayer(layerName, layerData, isNonEditable);
 			this.store.dispatch(new AddLayer(layer));
@@ -111,47 +120,34 @@ export class ImportLayerComponent implements OnInit, OnDestroy {
 		return json && json.type === 'FeatureCollection' && Array.isArray(json.features);
 	}
 
-	generateFeaturesIds(featureCollection: FeatureCollection): void {
-		/* reference */
-		featureCollection.features.forEach((feature) => {
-			const newId = UUID.UUID();
-			feature.id = newId;
-			feature.properties = { ...feature.properties, id: newId };
-		});
-	}
-
 	isNonEditable(featureCollection: FeatureCollection): boolean {
 		return featureCollection.features.some((feature) => feature.properties.isNonEditable);
 	}
 
-	simpleStyleToVisualizer(annotationsLayer): void {
+	ValidateFeatures(annotationsLayer): void {
 		/* reference */
-		annotationsLayer.features.forEach((feature) => {
-			const { id, showMeasures, mode, icon, labelSize, undeletable, label, style, isNonEditable, showArea } = feature.properties;
-			feature.properties = {
-				id,
-				label: this.extractLabel(label),
-				showMeasures: JSON.parse(showMeasures ? showMeasures : null),
-				showArea: JSON.parse(showArea ? showArea : null),
-				mode,
-				icon,
-				labelSize: isNaN(labelSize) ? 28 : parseInt(labelSize, 10),
-				undeletable: JSON.parse(undeletable),
-				style: JSON.parse(style),
-				isNonEditable: JSON.parse(isNonEditable || false)
-			};
+		annotationsLayer.features.forEach( (feature, index, features) => {
+			features[index] = validateFeatureProperties(feature);
+			features[index].id = UUID.UUID();
+			features[index].properties.id = features[index].id;
 		});
 	}
 
-	extractLabel(label: string): { text: string, geometry: any } {
-		let newLabel = { text: '', geometry: null };
-		try {
-			newLabel = JSON.parse(label);
-		} catch (e) {
-			newLabel.text = label;
-		} finally {
-			return newLabel;
+	private importError(error) {
+		this.store.dispatch(new SetToastMessageAction({
+			showWarningIcon: true,
+			toastText: getErrorMessageFromException(error, 'Failed to import file')
+		}));
+	}
+
+	private calculateLayerBbox(layer) {
+		let layerBbox;
+		if (layer.bbox) {
+			layerBbox = layer.bbox;
+		}else {
+			layerBbox = bboxFromGeoJson(layer);
 		}
+		return polygonFromBBOX(layerBbox);
 	}
 
 }

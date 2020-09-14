df94da9b3bcb5e132286ecab805c4a6f43979a8c
PiniAmiram <46754845+PiniAmiram@users.noreply.github.com>
2020-09-14 21:02:06 +0300

    annotation opacity turn off/on bug fix (#3405)
    * annotation opacity turn off/on bug fix
    
    * change the syntax

--- src/app/@ansyn/imagery/model/visualizers/initial-annotations-feature-properties.ts
+++ src/app/@ansyn/imagery/model/visualizers/initial-annotations-feature-properties.ts
@@ -74,7 +74,7 @@ export function validateFeatureProperties(feature: Feature<any>): Feature<any> {
 	featureJson.properties = {
 		... featureJson.properties,
 		id: (!!id && typeof id === 'string') ? id : defaultProperties.id,
-		labelSize: (!!labelSize && typeof labelSize === 'number') ? labelSize : defaultProperties.labelSize,
+		labelSize: (labelSize !== null && typeof labelSize === 'number') ? labelSize : defaultProperties.labelSize,
 		icon: (!!icon && typeof icon === 'string') ? icon : defaultProperties.icon,
 		showMeasures: typeof showMeasures === 'boolean' ? showMeasures : defaultProperties.showMeasures,
 		showArea: typeof showArea === 'boolean' ? showArea : defaultProperties.showArea,
@@ -88,18 +88,18 @@ export function validateFeatureProperties(feature: Feature<any>): Feature<any> {
 			opacity: !!opacity ? opacity : defaultStyle.opacity,
 			initial: {
 				stroke: (!!initial && !!initial.stroke && typeof initial.stroke === 'string') ? initial.stroke : defaultStyle.initial.stroke,
-				'stroke-width': (!!initial && !!initial['stroke-width'] && typeof initial['stroke-width'] === 'number') ? initial['stroke-width'] : defaultStyle.initial['stroke-width'],
+				'stroke-width': (!!initial && initial['stroke-width'] !== null && typeof initial['stroke-width'] === 'number') ? initial['stroke-width'] : defaultStyle.initial['stroke-width'],
 				fill: (!!initial && !!initial.fill && typeof initial.fill === 'string') ? initial.fill : defaultStyle.initial.fill,
-				'fill-opacity': (!!initial && !!initial['fill-opacity'] && typeof initial['fill-opacity'] === 'number') ? initial['fill-opacity'] : defaultStyle.initial['fill-opacity'],
-				'stroke-opacity': (!!initial && !!initial['stroke-opacity'] && typeof initial['stroke-opacity'] === 'number') ? initial['stroke-opacity'] : defaultStyle.initial['stroke-opacity'],
-				'stroke-dasharray': (!!initial && !!initial['stroke-dasharray'] && typeof initial['stroke-dasharray'] === 'number') ? initial['stroke-dasharray'] : defaultStyle.initial['stroke-dasharray'],
+				'fill-opacity': (!!initial && initial['fill-opacity'] !== null && typeof initial['fill-opacity'] === 'number') ? initial['fill-opacity'] : defaultStyle.initial['fill-opacity'],
+				'stroke-opacity': (!!initial && initial['stroke-opacity'] !== null && typeof initial['stroke-opacity'] === 'number') ? initial['stroke-opacity'] : defaultStyle.initial['stroke-opacity'],
+				'stroke-dasharray': (!!initial && initial['stroke-dasharray'] !== null && typeof initial['stroke-dasharray'] === 'number') ? initial['stroke-dasharray'] : defaultStyle.initial['stroke-dasharray'],
 				'marker-size': (!!initial && !!initial['marker-size'] && typeof initial['marker-size'] === 'string') ? initial['marker-size'] : defaultStyle.initial['marker-size'],
 				'marker-color': (!!initial && !!initial['marker-color'] && typeof initial['marker-color'] === 'string') ? initial['marker-color'] : defaultStyle.initial['marker-color'],
 				label: {
 					overflow: (!!initial && !!initial.label && !!initial.label.overflow && typeof initial.label.overflow === 'boolean') ? initial.label.overflow : defaultStyle.initial.label.overflow,
 					stroke: (!!initial && !!initial.label && !!initial.label.stroke && typeof initial.label.stroke === 'string') ? initial.label.stroke : defaultStyle.initial.label.stroke,
 					fill: (!!initial && !!initial.label && !!initial.label.fill && typeof initial.label.fill === 'string') ? initial.label.fill : defaultStyle.initial.label.fill,
-					offsetY: (!!initial && !!initial.label && !!initial.label.offsetY && typeof initial.label.offsetY === 'number') ? initial.label.offsetY : defaultStyle.initial.label.offsetY
+					offsetY: (!!initial && !!initial.label && initial.label.offsetY !== null && typeof initial.label.offsetY === 'number') ? initial.label.offsetY : defaultStyle.initial.label.offsetY
 				}
 			}
 		},

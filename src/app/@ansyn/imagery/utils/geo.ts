ed376bce550547791297149c5fb737997aebe6ba
unknown <tktur9@gmail.com>
2020-09-15 10:41:15 +0300

    fixing overlay bug

--- src/app/@ansyn/imagery/utils/geo.ts
+++ src/app/@ansyn/imagery/utils/geo.ts
@@ -150,7 +150,10 @@ export function getPolygonIntersectionRatioWithMultiPolygon(extent: Polygon, foo
 		console.warn('getPolygonIntersectionRatioWithMultiPolygon: turf exception', e);
 	}
 
-	return intersectionArea / extentArea > 0.99999999999999 ? 1 : intersectionArea / extentArea;
+	console.log('intersectionRatio', intersectionArea / extentArea);
+	const ratio = intersectionArea / extentArea;
+
+	return intersectionArea / extentArea;
 }
 
 export function isPointContainedInGeometry(point: Point, footprint: MultiPolygon | Point | LineString): boolean {

de1d0385c5284ea31842a47f68b8098f8d95d0ae
Sulimani55 <tktur9@gmail.com>
2020-09-29 10:29:02 +0300

    fix dependencies warnings

--- src/app/@ansyn/ansyn/modules/menu-items/tools/export-maps-popup/export-maps-popup.component.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/tools/export-maps-popup/export-maps-popup.component.ts
@@ -17,7 +17,7 @@ import { IToolsConfig, toolsConfig } from '../models/tools-config';
 import { annotationsClassNameForExport } from '@ansyn/ol';
 import { measuresClassNameForExport } from '../../../plugins/openlayers/plugins/visualizers/tools/measure-distance.visualizer';
 import { IExportMapData, IExportMapMetadata, ImageryCommunicatorService, toDegrees } from '@ansyn/imagery';
-import { jsPDF } from "jspdf";
+import { jsPDF } from 'jspdf';
 import { TranslateService } from '@ngx-translate/core';
 import { IOverlay } from '../../../overlays/models/overlay.model';
 import { Observable, of, EMPTY } from 'rxjs';

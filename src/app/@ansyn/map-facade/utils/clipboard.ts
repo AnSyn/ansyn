0db33fc00eddddabc666d925a395b03ec984db7a
sulimani55 <55398513+sulimani55@users.noreply.github.com>
2020-10-18 16:47:49 +0300

    fixing copy to clipboard on http (#3437)

--- src/app/@ansyn/map-facade/utils/clipboard.ts
+++ src/app/@ansyn/map-facade/utils/clipboard.ts
@@ -1,4 +1,16 @@
 export function copyFromContent(content: string): Promise<any> {
+	if (typeof (navigator.clipboard) === 'undefined') {
+		const textArea = document.createElement('textarea');
+		textArea.value = content;
+		textArea.style.position = 'fixed';
+		document.body.appendChild(textArea);
+		textArea.focus();
+		textArea.select();
+		document.execCommand('copy');
+		document.body.removeChild(textArea);
+		return;
+	}
+
 	return navigator.clipboard.writeText(content);
 }
 

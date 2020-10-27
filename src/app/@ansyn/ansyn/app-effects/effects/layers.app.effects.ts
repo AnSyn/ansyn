1fa8176888f3a84bffa4e2985ffb2b1e6cc37dbf
unknown <pini7671@gmail.com>
2020-10-27 14:07:33 +0200

    display the layers in the correct order

--- src/app/@ansyn/ansyn/app-effects/effects/layers.app.effects.ts
+++ src/app/@ansyn/ansyn/app-effects/effects/layers.app.effects.ts
@@ -9,7 +9,7 @@ import {
 	UpdateLayer,
 	UpdateSelectedLayersIds
 } from '../../modules/menu-items/layers-manager/actions/layers.actions';
-import { CasesActionTypes, SaveCaseAsSuccessAction, UpdateCaseBackendAction, UpdateCaseBackendSaveAs } from '../../modules/menu-items/cases/actions/cases.actions';
+import { CasesActionTypes, SaveCaseAsSuccessAction, UpdateCaseBackendSaveAs } from '../../modules/menu-items/cases/actions/cases.actions';
 import { ILayer, LayerType } from '../../modules/menu-items/layers-manager/models/layers.model';
 import { selectLayers } from '../../modules/menu-items/layers-manager/reducers/layers.reducer';
 import {

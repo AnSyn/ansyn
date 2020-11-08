3f5fef03ca6db95f93d3dc89d703628ca66f05c4
Tzahi Levi <thetzahilevi@gmail.com>
2020-11-08 16:53:31 +0200

    try config from https://angular.io/guide/testing#configure-project-for-travis-ci

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,13 @@ module.exports = function (config) {
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: ['Chrome'],
+		browsers: ['Chrome', 'ChromeHeadlessCI'],
+		customLaunchers: {
+			ChromeHeadlessCI: {
+				base: 'ChromeHeadless',
+				flags: ['--no-sandbox']
+			}
+		},
 		singleRun: false,
 		browserNoActivityTimeout: 80000,
 		browserDisconnectTimeout: 10000,

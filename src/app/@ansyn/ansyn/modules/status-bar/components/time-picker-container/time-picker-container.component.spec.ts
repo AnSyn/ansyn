487c711f8aa093be5b9de5682023ff5a08e91b70
Sulimani55 <tktur9@gmail.com>
2020-11-09 10:37:38 +0200

    self cr

--- src/app/@ansyn/ansyn/modules/status-bar/components/time-picker-container/time-picker-container.component.spec.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/time-picker-container/time-picker-container.component.spec.ts
@@ -25,7 +25,7 @@ describe('TimePickerContainerComponent', () => {
 		fixture.detectChanges();
 	});
 
-	fit('should create', () => {
+	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
 });

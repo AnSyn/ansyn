7552a90e818d38960f47d4a86a20e42331c462c5
Sulimani55 <tktur9@gmail.com>
2020-11-11 15:39:16 +0200

    removing unused message time was not changes

--- src/app/@ansyn/ansyn/modules/core/models/toast-messages.ts
+++ src/app/@ansyn/ansyn/modules/core/models/toast-messages.ts
@@ -3,6 +3,5 @@ export const toastMessages = {
 	showOverlayErrorToast: 'Failed to load overlay',
 	failedToCreateLink: 'Failed to create link',
 	notSupportRangeDates: 'There are no matching overlays on this date',
-	timeWasNotChange: 'The dates have not changed',
 	invalidDate: 'Invalid date'
 };

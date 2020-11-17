c9398535599ede5ecf25becbb27ae211b4ddb9c0
Sulimani55 <tktur9@gmail.com>
2020-11-17 09:47:24 +0200

    fix rtl on pdf export

--- src/app/@ansyn/ansyn/modules/menu-items/tools/export-maps-popup/export-maps-popup.component.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/tools/export-maps-popup/export-maps-popup.component.ts
@@ -199,7 +199,7 @@ export class ExportMapsPopupComponent implements OnInit, OnDestroy {
 						doc.addFont('TTWPGOTT.ttf', 'TTWPGOTT', 'normal');
 						doc.setFont('TTWPGOTT');
 						doc.setFontSize(11);
-						doc.setR2L(this.isRtl);
+						doc.setR2L(false);
 						const loadOverlay = mapToBeExport.mapSettings.data.overlay;
 						const desc = Boolean(loadOverlay) ? this.getDescriptionFromOverlay(loadOverlay) : 'Base Map';
 						doc.text(desc, size[0] / 2, 5, {align: 'center', baseline: 'bottom'});

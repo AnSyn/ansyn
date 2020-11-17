6e8afde5bce4942169a3a3928c112d9ed8a2b9ca
ramtob <ramtob@gmail.com>
2020-11-17 15:14:18 +0200

    RTL: Filters (cont)

--- src/app/@ansyn/ansyn/modules/filters/components/enum-filter-container/enum-filter-container.component.ts
+++ src/app/@ansyn/ansyn/modules/filters/components/enum-filter-container/enum-filter-container.component.ts
@@ -1,5 +1,5 @@
 import { EnumFilterMetadata } from '../../models/metadata/enum-filter-metadata';
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { EnumFilterCounters } from '../../models/counters/enum-filter-counters';
 
 @Component({
@@ -13,7 +13,10 @@ export class EnumFilterContainerComponent implements OnChanges {
 	@Input() isLongFiltersList: boolean;
 	@Output() onMetadataChange = new EventEmitter<EnumFilterMetadata>(true);
 	@Input() filtersSearchResult: any;
+
+	@HostBinding('class.rtl')
 	@Input() isRTL = false;
+
 	fields: { key: any, value: any, filteredCount: number }[] = [];
 
 	onInputClicked(key: string) {

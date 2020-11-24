bac36c76f79483dfffc7fe627a571f1a1668aeed
PiniAmiram <pini7671@gmail.com>
2020-11-24 16:42:19 +0200

    split to parm

--- src/app/@ansyn/ansyn/modules/overlays/services/overlays.service.ts
+++ src/app/@ansyn/ansyn/modules/overlays/services/overlays.service.ts
@@ -75,7 +75,8 @@ export class OverlaysService {
 
 	static parseOverlayDataForDisplay({ overlaysArray, filteredOverlays, specialObjects, favoriteOverlays, showOnlyFavorites }: IOverlayDropSources): IOverlayDrop[] {
 		const criterialOverlays: IOverlay[] = showOnlyFavorites ? [] : overlaysArray.filter(({ id }) => filteredOverlays.includes(id));
-		const dropsFromOverlays: IOverlayDrop[] = criterialOverlays.map(({ id, date, sensorName, icon }) =>   ({ id, date, sensorName, icon, favorite: favoriteOverlays.map(favoriteOverlay => favoriteOverlay.id).includes(id) }));
+		const favoriteOverlayIds: string[] = favoriteOverlays.map(favoriteOverlay => favoriteOverlay.id);
+		const dropsFromOverlays: IOverlayDrop[] = criterialOverlays.map(({ id, date, sensorName, icon }) =>   ({ id, date, sensorName, icon, favorite: favoriteOverlayIds.includes(id) }));
 		const allDrops = [...dropsFromOverlays, ...mapValuesToArray(specialObjects)].sort(sortByDateDesc);
 		return allDrops;
 	}

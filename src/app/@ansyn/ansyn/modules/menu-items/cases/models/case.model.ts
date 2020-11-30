fee9a609dc99ad2fa8dda1c8bf1ae188811ecb5b
sulimani55 <55398513+sulimani55@users.noreply.github.com>
2020-11-30 10:56:37 +0200

    ScreenView - Disable search on split screen (#3517)
    * disable search on change layout
    
    * disable
    
    * disabling search when changing layout screeview
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * cr
    
    * removing unecessary code
    
    * adding zoom filter
    
    * adding zoom filter
    
    * adding zoom filter
    
    * adding zoom filter

--- src/app/@ansyn/ansyn/modules/menu-items/cases/models/case.model.ts
+++ src/app/@ansyn/ansyn/modules/menu-items/cases/models/case.model.ts
@@ -3,7 +3,6 @@ import { Feature, MultiPolygon, Point, Polygon } from 'geojson';
 import { LayoutKey } from '@ansyn/map-facade';
 import { FilterType } from '../../../filters/models/filter-type';
 import { IMapSettings, IMapSettingsData } from '@ansyn/imagery';
-import { ICompressedMapSettings, ICompressedMapSettingsData } from '@ansyn/imagery';
 
 export interface ICasePreview {
 	creationTime: Date;
@@ -42,14 +41,6 @@ export interface IImageManualProcessArgs {
 	Sharpness?: number;
 }
 
-export interface ICompressedImageManualProcessArgs {
-	b?: number;
-	c?: number;
-	g?: number;
-	sa?: number;
-	sh?: number;
-}
-
 export interface IOverlaysManualProcessArgs {
 	[key: string]: IImageManualProcessArgs;
 }
@@ -82,6 +73,7 @@ export interface IDilutedCaseState {
 }
 
 export interface ICaseState extends IDilutedCaseState {
+
 	favoriteOverlays?: IOverlay[];
 	miscOverlays?: IOverlaysHash;
 	maps?: ICaseMapsState;
@@ -123,18 +115,12 @@ export interface ICaseEnumFilterMetadata {
 	disabledEnums: string[];
 }
 
-export interface ICompressedCaseEnumFilterMetadata {
-	un: string[];
-	dis: string[];
-}
-
 export type CaseArrayFilterMetadata = [string, boolean][];
 
 export type CaseFilterMetadata =
 	ICaseBooleanFilterMetadata
 	| ICaseEnumFilterMetadata
 	| ICaseSliderFilterMetadata
-	| ICompressedCaseEnumFilterMetadata
 	| CaseArrayFilterMetadata;
 
 export interface ICaseFilter<T = CaseFilterMetadata> {
@@ -144,21 +130,9 @@ export interface ICaseFilter<T = CaseFilterMetadata> {
 	positive?: boolean;
 }
 
-export interface ICompressedCaseFilter<T = CaseFilterMetadata> {
-	t: FilterType;
-	f: string;
-	m: T;
-	p?: boolean;
-}
-
 export interface ICaseFacetsState {
 	filters?: ICaseFilter[];
 	showOnlyFavorites?: boolean;
-}
-
-export interface ICompressedCaseFacetsState {
-	f?: ICompressedCaseFilter[];
-	fav?: boolean;
 }
 
 export interface ICaseLayersState {
@@ -175,24 +149,10 @@ export interface ICaseMapsState extends IDilutedCaseMapsState {
 	data: ICaseMapState[];
 }
 
-export interface ICompressedCaseMapsState {
-	d: ICompressedCaseMapState[];
-	l: LayoutKey;
-	id: string;
-}
-
 export interface IDilutedCaseMapData extends IMapSettingsData {
 	overlay?: IDilutedOverlay;
 	isAutoImageProcessingActive?: boolean;
 	imageManualProcessArgs?: IImageManualProcessArgs;
-	translationData?: ITranslationData;
-}
-
-export interface ICompressedCaseMapData extends IDilutedCaseMapData {
-	o?: any;
-	p?: ICompressedMapSettingsData;
-	auto?: boolean;
-	man?: ICompressedImageManualProcessArgs;
 	translationData?: ITranslationData;
 }
 
@@ -206,10 +166,6 @@ export interface IDilutedCaseMapState extends IMapSettings {
 
 export interface ICaseMapState extends IMapSettings {
 	data: ICaseMapData;
-}
-
-export interface ICompressedCaseMapState extends ICompressedMapSettings {
-	d?: any;
 }
 
 

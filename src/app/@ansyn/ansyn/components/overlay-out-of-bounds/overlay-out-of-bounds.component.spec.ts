65aed77018f29123511438c827b432b6f1085919
Sulimani55 <tktur9@gmail.com>
2020-12-29 16:01:33 +0200

    fix tests

--- src/app/@ansyn/ansyn/components/overlay-out-of-bounds/overlay-out-of-bounds.component.spec.ts
+++ src/app/@ansyn/ansyn/components/overlay-out-of-bounds/overlay-out-of-bounds.component.spec.ts
@@ -1,45 +1,31 @@
 import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
-import { mapFeatureKey, MapReducer } from '@ansyn/map-facade';
 import { OverlayOutOfBoundsComponent } from './overlay-out-of-bounds.component';
 import { bboxFromGeoJson, ImageryCommunicatorService } from '@ansyn/imagery';
-import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import { geometry } from '@turf/turf';
-import { Store, StoreModule } from '@ngrx/store';
-import { TranslateModule } from '@ngx-translate/core';
 
 describe('OverlayOutOfBoundsComponent', () => {
 	let component: OverlayOutOfBoundsComponent;
 	let fixture: ComponentFixture<OverlayOutOfBoundsComponent>;
 	let imageryCommunicatorService: ImageryCommunicatorService;
-	let store: Store<any>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			providers: [ImageryCommunicatorService],
 			declarations: [OverlayOutOfBoundsComponent],
-			imports: [StoreModule.forRoot({ [mapFeatureKey]: MapReducer }), TranslateModule.forRoot()]
 		})
 			.compileComponents();
 	}));
 
-	beforeEach(inject([ImageryCommunicatorService, Store], (_imageryCommunicatorService: ImageryCommunicatorService, _store: Store<any>) => {
+	beforeEach(inject([ImageryCommunicatorService], (_imageryCommunicatorService: ImageryCommunicatorService) => {
 		fixture = TestBed.createComponent(OverlayOutOfBoundsComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
 		imageryCommunicatorService = _imageryCommunicatorService;
-		store = _store;
 	}));
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
-	});
-
-	it('click on button should call "backToExtent"', () => {
-		spyOn(component, 'backToExtent');
-		const button = fixture.debugElement.query(By.css('button'));
-		button.triggerEventHandler('click', {});
-		expect(component.backToExtent).toHaveBeenCalled();
 	});
 
 	it('backToExtent should calc extent and call fitToExtent', () => {
--- src/app/@ansyn/ansyn/components/overlay-out-of-bounds/overlay-out-of-bounds.component.ts
+++ src/app/@ansyn/ansyn/components/overlay-out-of-bounds/overlay-out-of-bounds.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { bboxFromGeoJson, ImageryCommunicatorService } from '@ansyn/imagery';
 import { take } from 'rxjs/operators';
 import { IEntryComponent } from '@ansyn/map-facade';
@@ -14,16 +14,16 @@ export class OverlayOutOfBoundsComponent implements OnInit, IEntryComponent {
 	constructor(protected communicatorService: ImageryCommunicatorService) {
 	}
 
-	backToExtent(): void {
-		const communicator = this.communicatorService.provide(this.mapId);
-		const extent = bboxFromGeoJson(communicator.mapSettings.data.overlay.footprint);
-		communicator.ActiveMap.fitToExtent(extent)
-			.pipe(take(1))
-			.subscribe();
-	}
-
 	ngOnInit(): void {
 		this.backToExtent();
+	}
+
+	backToExtent(): void {
+		const communicator = this.communicatorService.provide(this.mapId);
+		if (communicator) {
+			const extent = bboxFromGeoJson(communicator.mapSettings.data.overlay.footprint);
+			communicator.ActiveMap.fitToExtent(extent).pipe(take(1)).subscribe();
+		}
 	}
 
 	getType(): string {

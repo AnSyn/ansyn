daeb5e668f38b1697c420bf3f9c381c72b95ba2d
PiniAmiram <pini7671@gmail.com>
2020-12-29 16:03:12 +0200

    lint

--- src/app/@ansyn/ansyn/modules/status-bar/components/advanced-search/advanced-search.component.spec.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/advanced-search/advanced-search.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdvancedSearchComponent } from './advanced-search.component';
 
 	describe('AdvancedSearchComponent', () => {
-	let component: AdvancedSearchComponent;
-	let fixture: ComponentFixture<AdvancedSearchComponent>;
+		let component: AdvancedSearchComponent;
+		let fixture: ComponentFixture<AdvancedSearchComponent>;
 
 	beforeEach(async(() => {
-	TestBed.configureTestingModule({
-		declarations: [ AdvancedSearchComponent ]
-	})
-	.compileComponents();
+		TestBed.configureTestingModule({
+			declarations: [ AdvancedSearchComponent ]
+		})
+		.compileComponents();
 	}));
 
 	beforeEach(() => {
-	fixture = TestBed.createComponent(AdvancedSearchComponent);
-	component = fixture.componentInstance;
-	fixture.detectChanges();
+		fixture = TestBed.createComponent(AdvancedSearchComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
 	});
 
 	it('should create', () => {
-	expect(component).toBeTruthy();
+		expect(component).toBeTruthy();
 	});
 	});
--- src/app/@ansyn/ansyn/modules/status-bar/components/search-options/search-options.component.spec.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/search-options/search-options.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SearchOptionsComponent } from './search-options.component';
 
 	describe('SearchOptionsComponent', () => {
-	let component: SearchOptionsComponent;
-	let fixture: ComponentFixture<SearchOptionsComponent>;
+		let component: SearchOptionsComponent;
+		let fixture: ComponentFixture<SearchOptionsComponent>;
 
 	beforeEach(async(() => {
-	TestBed.configureTestingModule({
+		TestBed.configureTestingModule({
 		declarations: [ SearchOptionsComponent ]
-	})
-	.compileComponents();
+		})
+		.compileComponents();
 	}));
 
 	beforeEach(() => {
-	fixture = TestBed.createComponent(SearchOptionsComponent);
-	component = fixture.componentInstance;
-	fixture.detectChanges();
+		fixture = TestBed.createComponent(SearchOptionsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
 	});
 
 	it('should create', () => {
-	expect(component).toBeTruthy();
+		expect(component).toBeTruthy();
 	});
 	});

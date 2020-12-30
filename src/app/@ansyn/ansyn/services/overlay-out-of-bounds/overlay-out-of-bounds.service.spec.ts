6395acb55e3352f053770605b612174b2a833c41
sulimani55 <tktur9@gmail.com>
2020-12-30 20:55:53 +0200

    fixing tests

--- src/app/@ansyn/ansyn/services/overlay-out-of-bounds/overlay-out-of-bounds.service.spec.ts
+++ src/app/@ansyn/ansyn/services/overlay-out-of-bounds/overlay-out-of-bounds.service.spec.ts
@@ -1,23 +1,31 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { OverlayOutOfBoundsService } from './overlay-out-of-bounds.service';
-import { StoreModule } from "@ngrx/store";
+import { Store, StoreModule } from "@ngrx/store";
 import { geometry } from "@turf/turf";
 import { of } from "rxjs";
 import { bboxFromGeoJson, ImageryCommunicatorService } from "@ansyn/imagery";
+import { cloneDeep } from "lodash";
+import { IMapState, initialMapState, mapStateSelector } from "@ansyn/map-facade";
 
 describe('OverlayOutOfBoundsService', () => {
 	let service: OverlayOutOfBoundsService;
 	let imageryCommunicatorService: ImageryCommunicatorService;
+	let store: Store<any>;
+	const mapState: IMapState = cloneDeep(initialMapState);
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			imports: [StoreModule.forRoot({})]
+			imports: [StoreModule.forRoot({})],
+			providers: [ImageryCommunicatorService]
 		});
 		service = TestBed.inject(OverlayOutOfBoundsService);
 	});
 
-	beforeEach(inject([ImageryCommunicatorService], (_imageryCommunicatorService: ImageryCommunicatorService) => {
+	beforeEach(inject([ImageryCommunicatorService, Store], (_imageryCommunicatorService: ImageryCommunicatorService, _store: Store<any>) => {
 		imageryCommunicatorService = _imageryCommunicatorService;
+		store = _store;
+		const fakeStore = new Map<any, any>([[mapStateSelector, mapState]]);
+		spyOn(store, 'select').and.callFake((selector) => of(fakeStore.get(selector)));
 	}));
 
 	it('should be created', () => {
@@ -29,11 +37,11 @@ describe('OverlayOutOfBoundsService', () => {
 			id: 'mapId',
 			data: { overlay: { footprint: geometry('Polygon', [[[0, 0], [1, 1], [2, 2], [0, 0]]]) } }
 		};
-		service.activeMapId = 'mapId';
+		const activeMapId = 'mapId';
 		const ActiveMap = jasmine.createSpyObj({ fitToExtent: of(true) });
 		spyOn(imageryCommunicatorService, 'provide').and.returnValue(<any>{ ActiveMap, mapSettings });
 		service.backToExtent();
-		const extent = bboxFromGeoJson(imageryCommunicatorService.provide(service.activeMapId).mapSettings.data.overlay.footprint);
+		const extent = bboxFromGeoJson(imageryCommunicatorService.provide(activeMapId).mapSettings.data.overlay.footprint);
 		expect(ActiveMap.fitToExtent).toHaveBeenCalledWith(extent);
 	});
 
--- src/app/@ansyn/ansyn/services/overlay-out-of-bounds/overlay-out-of-bounds.service.ts
+++ src/app/@ansyn/ansyn/services/overlay-out-of-bounds/overlay-out-of-bounds.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, OnDestroy, OnInit } from '@angular/core';
-import { bboxFromGeoJson, ImageryCommunicatorService } from "@ansyn/imagery";
 import { take } from "rxjs/operators";
 import { Store } from "@ngrx/store";
 import { selectActiveMapId } from "@ansyn/map-facade";
+import { bboxFromGeoJson, ImageryCommunicatorService } from "@ansyn/imagery";
+
 
 @Injectable({
 	providedIn: 'root'
 })
-export class OverlayOutOfBoundsService implements OnInit, OnDestroy{
+export class OverlayOutOfBoundsService implements OnInit, OnDestroy {
 
 	constructor(protected store$: Store, protected communicatorService: ImageryCommunicatorService) {
 	}
@@ -21,10 +22,8 @@ export class OverlayOutOfBoundsService implements OnInit, OnDestroy{
 	backToExtent(): void {
 		this.store$.select(selectActiveMapId).pipe(take(1)).subscribe(activeMapId => {
 			const communicator = this.communicatorService.provide(activeMapId);
-			if (communicator) {
-				const extent = bboxFromGeoJson(communicator.mapSettings.data.overlay.footprint);
-				communicator.ActiveMap.fitToExtent(extent).pipe(take(1)).subscribe();
-			}
+			const extent = bboxFromGeoJson(communicator.mapSettings.data.overlay.footprint);
+			communicator.ActiveMap.fitToExtent(extent).pipe(take(1)).subscribe();
 		});
 	}
 }

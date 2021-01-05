80bbee9233b653756132e7fb893eba27211890f1
ramtob <ramtob@gmail.com>
2021-01-05 12:12:02 +0200

    Placing help button

--- src/app/@ansyn/ansyn/modules/status-bar/components/status-bar/status-bar.component.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/status-bar/status-bar.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, HostBinding } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { COMPONENT_MODE } from '../../../../app-providers/component-mode';
+import { LogHelp } from '../../../../../menu/actions/menu.actions';
+import { Store } from '@ngrx/store';
+import { IMenuConfig, IMenuState, MenuConfig } from '@ansyn/menu';
 
 @Component({
 	selector: 'ansyn-status-bar',
@@ -13,7 +16,21 @@ export class StatusBarComponent {
 	@HostBinding('class.rtl')
 	isRTL = this.translateService.instant('direction') === 'rtl';
 
-	constructor(private translateService: TranslateService,
-				@Inject(COMPONENT_MODE) public componentMode: boolean) {}
+	constructor(
+		private translateService: TranslateService,
+		@Inject(COMPONENT_MODE) public componentMode: boolean,
+		@Inject(MenuConfig) public menuConfig: IMenuConfig,
+		protected store: Store<IMenuState>
+	) {}
+
+
+	onHelp() {
+		this.store.dispatch(new LogHelp());
+		this.goToLandingPage();
+	}
+
+	goToLandingPage() {
+		window.open(this.menuConfig.landingPageUrl);
+	}
 
 }

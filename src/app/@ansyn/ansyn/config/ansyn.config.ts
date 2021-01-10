76ffc9e9f4acac7f4cc74de2861f6d6232a1d5cb
unknown <pini7671@gmail.com>
2021-01-10 18:39:01 +0200

    delete import

--- src/app/@ansyn/ansyn/config/ansyn.config.ts
+++ src/app/@ansyn/ansyn/config/ansyn.config.ts
@@ -3,7 +3,6 @@ import { IAlert } from '../modules/alerts/alerts.model';
 import { LayersManagerComponent } from '../modules/menu-items/layers-manager/components/layers-manager/layers-manager.component';
 import { anaglyphSensorAlertKey } from '../modules/plugins/openlayers/plugins/anaglyph-sensor/plugin/anaglyph-sensor.plugin';
 import { AnaglyphSensorAlertComponent } from '../modules/plugins/openlayers/plugins/anaglyph-sensor/alert-component/anaglyph-sensor-alert.component';
-import { FiltersCollectionComponent } from '../modules/filters/components/filters-collection/filters-collection.component';
 import { ResultsTableComponent } from '../modules/menu-items/results/components/results-table/results-table.component';
 import { CasesContainerComponent } from '../modules/menu-items/cases/components/cases-container/cases-container.component';
 

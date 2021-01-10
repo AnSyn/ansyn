4e7c9c29ac173ddaa20372b721c731d923217b72
Ram Tobolski <ramtob@users.noreply.github.com>
2021-01-10 18:12:07 +0200

    moving-menu-bottom-buttons-to-top-button-bar + screens button (#3543)
    * Placing help button
    
    * Adding logo-panel for these buttons
    
    * Moving permission button to logo-panel (not yet deleting the old button)
    
    * Moving logo to logo-panel (not finished)
    
    * Moving fork & logo to logo-panel (cont)
    
    * font size, icon size
    
    * Moving buttons 1 cleaning 2 unit tests
    
    * Moving buttons new icons - credentials icons
    
    * Moving buttons new icons - adding icon-pointer_up for comparison
    
    * Moving buttons new icons - fixing build libs
    
    * Moving buttons new icons - fixing unit tests
    
    * Status bar tools buttons - fix tooltips direction
    
    * Screen button: remove the word Screens
    
    * Screens button: reshaping and relocating
    
    * post merge fixes
    
    * post merge fixes

--- src/app/@ansyn/menu/menu/menu.component.ts
+++ src/app/@ansyn/menu/menu/menu.component.ts
@@ -14,9 +14,7 @@ import {
 	ViewContainerRef
 } from '@angular/core';
 import {
-	ContainerChangedTriggerAction, LogHelp,
-	ResetAppAction,
-	SelectMenuItemAction,
+	ContainerChangedTriggerAction, SelectMenuItemAction,
 	ToggleIsPinnedAction,
 	ToggleMenuCollapse,
 	UnSelectMenuItemAction
@@ -271,15 +269,6 @@ export class MenuComponent implements OnInit, OnDestroy, AfterViewChecked {
 		}
 	}
 
-	onHelp() {
-		this.store.dispatch(new LogHelp());
-		this.goToLandingPage();
-	}
-
-	goToLandingPage() {
-		window.open(this.menuConfig.landingPageUrl);
-	}
-
 	anyMenuItemSelected(): boolean {
 		return Boolean(this.selectedMenuItem);
 	}
@@ -329,10 +318,6 @@ export class MenuComponent implements OnInit, OnDestroy, AfterViewChecked {
 
 		this.animatedElement.style.animation = `${this.collapse ? 'collapsed' : 'unCollapsed'}_${this.isRTL ? 'rtl' : 'ltr'} ${this.collapse ? '.3s' : '.6s'}`;
 
-	}
-
-	resetApp() {
-		this.store.dispatch(new ResetAppAction());
 	}
 
 	ngOnInit() {

e1763dd31af0e591279e375035b970cb5b4d347a
Sulimani55 <tktur9@gmail.com>
2021-01-12 14:43:23 +0200

    change the resolution scale from mm to cm

--- src/app/@ansyn/ansyn/modules/status-bar/components/advanced-search/advanced-search.component.spec.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/advanced-search/advanced-search.component.spec.ts
@@ -1,57 +1,58 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { StoreModule } from '@ngrx/store';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MultipleOverlaysSourceConfig } from '../../../core/models/multiple-overlays-source-config';
 import { casesConfig } from '../../../menu-items/cases/services/cases.service';
 import { SearchPanelComponent } from '../search-panel/search-panel.component';
 
 import { AdvancedSearchComponent } from './advanced-search.component';
 
-	describe('AdvancedSearchComponent', () => {
-		let component: AdvancedSearchComponent;
-		let fixture: ComponentFixture<AdvancedSearchComponent>;
+describe('AdvancedSearchComponent', () => {
+	let component: AdvancedSearchComponent;
+	let fixture: ComponentFixture<AdvancedSearchComponent>;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
-			declarations: [ AdvancedSearchComponent, SearchPanelComponent ],
+			declarations: [AdvancedSearchComponent, SearchPanelComponent],
 			providers: [
-					{
-						provide: MultipleOverlaysSourceConfig,
-						useValue: {
-							indexProviders: {
-								OPEN_AERIAL: {
-									dataInputFiltersConfig: {
-										children: []
+				TranslateService,
+				{
+					provide: MultipleOverlaysSourceConfig,
+					useValue: {
+						indexProviders: {
+							OPEN_AERIAL: {
+								dataInputFiltersConfig: {
+									children: []
+								}
+							}
+						}
+					}
+				},
+				{
+					provide: SearchPanelComponent,
+					useValue: {}
+				},
+				{
+					provide: casesConfig,
+					useValue: {
+						schema: null,
+						defaultCase: {
+							id: 'defaultCaseId',
+							state: {
+								advancedSearchParameters: {
+									resolution: {
+										lowValue: 0,
+										highValue: 0
 									}
 								}
 							}
 						}
-					},
-					{
-						provide: SearchPanelComponent,
-						useValue: {}
-					},
-					{
-						provide: casesConfig,
-						useValue: { 
-							schema: null, 
-							defaultCase: { 
-								id: 'defaultCaseId',
-								state: {
-									advancedSearchParameters: {
-										resolution: {
-											lowValue: 0,
-											highValue: 0
-										}
-									}
-								}
-							} 
-						} 
 					}
+				}
 			],
 			imports: [StoreModule.forRoot({}), TranslateModule.forRoot()]
 		})
-		.compileComponents();
+			.compileComponents();
 	}));
 
 	beforeEach(() => {

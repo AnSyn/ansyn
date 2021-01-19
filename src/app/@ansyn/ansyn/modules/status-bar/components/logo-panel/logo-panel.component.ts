e9277265fcc72c475a3ba7f4a82c61bfaa8c51b7
ramtob <ramtob@gmail.com>
2021-01-19 11:57:43 +0200

    RTL only - ts

--- src/app/@ansyn/ansyn/modules/status-bar/components/logo-panel/logo-panel.component.ts
+++ src/app/@ansyn/ansyn/modules/status-bar/components/logo-panel/logo-panel.component.ts
@@ -1,9 +1,8 @@
-import { Component, HostBinding, Inject, Input } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { IStatusBarState } from '../../reducers/status-bar.reducer';
 import { StatusBarConfig } from '../../models/statusBar.config';
 import { IStatusBarConfig } from '../../models/statusBar-config.model';
-import { TranslateService } from '@ngx-translate/core';
 import { IMenuConfig, LogHelp, MenuConfig, ResetAppAction } from '@ansyn/menu';
 
 @Component({
@@ -14,15 +13,11 @@ import { IMenuConfig, LogHelp, MenuConfig, ResetAppAction } from '@ansyn/menu';
 export class LogoPanelComponent {
 	@Input() version;
 
-	@HostBinding('class.rtl')
-	isRTL = this.translateService.instant('direction') === 'rtl';
-
 	showCredentials = false;
 
 	constructor(
 		protected store$: Store<IStatusBarState>,
 		@Inject(StatusBarConfig) public statusBarConfig: IStatusBarConfig,
-		protected translateService: TranslateService,
 		@Inject(MenuConfig) public menuConfig: IMenuConfig,
 		protected store: Store<any>
 	) {

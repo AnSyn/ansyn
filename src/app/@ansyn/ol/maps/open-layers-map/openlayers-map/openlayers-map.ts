c9023c147eeea6a3ba7ef5e125526bc4eafd299b
Sulimani55 <tktur9@gmail.com>
2021-01-27 16:38:46 +0200

    fixing tests

--- src/app/@ansyn/ol/maps/open-layers-map/openlayers-map/openlayers-map.ts
+++ src/app/@ansyn/ol/maps/open-layers-map/openlayers-map/openlayers-map.ts
@@ -217,7 +217,7 @@ export class OpenLayersMap extends BaseImageryMap<OLMap> {
 	createView(layer): View {
 		return new View({
 			projection: layer.getSource().getProjection(),
-			maxZoom: 31,
+			maxZoom: 21,
 			minZoom: 1
 		});
 	}

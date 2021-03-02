530bf35b080712e5ec8dbede6c4ce5f97955fb7e
yehonatanhal <yehonatanhal@gmail.com>
2021-03-02 15:32:59 +0200

    * Fix lint errors

--- src/app/@ansyn/ansyn/app-effects/effects/menu.app.effects.ts
+++ src/app/@ansyn/ansyn/app-effects/effects/menu.app.effects.ts
@@ -3,7 +3,7 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Observable } from 'rxjs';
 import { IAppState } from '../app.effects.module';
 import { select, Store } from '@ngrx/store';
-import {SetMapSearchBoxTriggerAction, ToggleFooter, UpdateMapSizeAction} from '@ansyn/map-facade';
+import { SetMapSearchBoxTriggerAction, ToggleFooter, UpdateMapSizeAction } from '@ansyn/map-facade';
 import {
 	IMenuConfig,
 	MenuActionTypes,

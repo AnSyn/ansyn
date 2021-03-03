023384efd21b1c16717f703440a8639e362b9ad3
007quardo <77332515+007quardo@users.noreply.github.com>
2021-03-03 14:02:01 +0200

    bug fixes (#3639)
    * bug fixes
    
    * fix unneccesery adds of cli
    
    * fix lint
    
    * fix lint
    
    * fix lint

--- src/app/@ansyn/ansyn/app-effects/effects/cases/select-case.app.effects.ts
+++ src/app/@ansyn/ansyn/app-effects/effects/cases/select-case.app.effects.ts
@@ -93,7 +93,7 @@ export class SelectCaseAppEffects {
 		// filters
 		const { facets } = state;
 
-		const measureIsActive = state.maps.data.some( map => map?.data?.measuresData?.isToolActive);
+		const measureIsActive = state.maps.data.some( map => map?.data?.measuresData?.measures?.length > 0);
 
 		const selectCaseAction = [
 			new SetMapsDataActionStore({ mapsList: data.map(this.parseMapData.bind(this)) }),
@@ -113,7 +113,7 @@ export class SelectCaseAppEffects {
 			new UpdateToolsFlags([{key: toolsFlags.isMeasureToolActive, value: measureIsActive}]),
 			new SelectCaseSuccessAction(payload)
 		];
-
+		
 		return selectCaseAction;
 	}
 

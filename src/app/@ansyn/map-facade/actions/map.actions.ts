7af4fa476b5937249cef0ee530dc726309eee552
Sulimani55 <tktur9@gmail.com>
2021-03-15 12:16:07 +0200

    hotfix

--- src/app/@ansyn/map-facade/actions/map.actions.ts
+++ src/app/@ansyn/map-facade/actions/map.actions.ts
@@ -142,7 +142,9 @@ export class SetFourViewsModeAction implements Action, ILogMessage {
 	}
 
 	logMessage() {
-		return `Activated four views at ${ this.payload.point }`
+		if (this.payload) {
+			return `Activated four views at ${ this?.payload?.point }`
+		}
 	}
 }
 

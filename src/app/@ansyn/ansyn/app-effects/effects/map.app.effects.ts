b8d370cdc25b9c0dfce93baf5cff80fc16d082c1
RomCo13 <46006113+RomCo13@users.noreply.github.com>
2021-03-22 12:12:59 +0200

    back to Worldview button (#3667)
    * turn active sign off when auto image processing active
    
    * revert
    
    * bug fix
    
    * move back to world view action to four view effect
    
    * remove var
    
    * update type
    
    Co-authored-by: PiniAmiram <pini7671@gmail.com>

--- src/app/@ansyn/ansyn/app-effects/effects/map.app.effects.ts
+++ src/app/@ansyn/ansyn/app-effects/effects/map.app.effects.ts
@@ -379,14 +379,16 @@ export class MapAppEffects {
 	onDisableFourViewsMode$ = this.actions$.pipe(
 		ofType(MapActionTypes.SET_FOUR_VIEWS_MODE),
 		filter(({ payload }: SetFourViewsModeAction) => !payload?.active),
-		mergeMap(() => {
+		withLatestFrom(this.store$.select(selectActiveMapId)),
+		mergeMap(([payload, activeMapID]: [SetFourViewsModeAction, string]) => {
 			const oneMapLayout = 'layout1';
 			const regionLayerName = this.translateService.instant(regionLayerDefaultName);
 			return [
 				new SetLayoutAction(oneMapLayout),
 				new ToggleFooter(false),
 				new SetFourViewsOverlaysAction({}),
-				new UpdateLayer({ id: regionLayerId, name: regionLayerName })
+				new UpdateLayer({ id: regionLayerId, name: regionLayerName }),
+				new BackToWorldView({ mapId: activeMapID })
 			];
 		})
 	);

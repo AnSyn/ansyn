87225045cdb2b97ffb6ecb60a171062af664f395
Tzahi Levi <thetzahilevi@gmail.com>
2021-04-21 10:16:08 +0300

    upgrade region visualizer

--- src/app/@ansyn/ansyn/modules/plugins/openlayers/plugins/visualizers/region/region.visualizer.ts
+++ src/app/@ansyn/ansyn/modules/plugins/openlayers/plugins/visualizers/region/region.visualizer.ts
@@ -1,6 +1,6 @@
 import * as turf from '@turf/turf';
-import { combineLatest, EMPTY, Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
+import { combineLatest, EMPTY, merge, Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
 import { Actions, ofType } from '@ngrx/effects';
 import { FeatureCollection, GeometryObject, Position } from 'geojson';
 import { primaryAction as mouseClickCondition } from 'ol/events/condition'
@@ -17,77 +17,75 @@ import Draw from 'ol/interaction/Draw';
 import { AutoSubscription } from 'auto-subscriptions';
 import { distinctUntilChanged, filter, map, mergeMap, take, tap, withLatestFrom } from 'rxjs/operators';
 import { EntitiesVisualizer, OpenLayersProjectionService } from '@ansyn/ol';
-import { selectGeoFilterActive, selectGeoFilterType } from '../../../../../status-bar/reducers/status-bar.reducer';
+import { IGeoFilterStatus, selectGeoFilterStatus } from '../../../../../status-bar/reducers/status-bar.reducer';
 import { UpdateGeoFilterStatus } from '../../../../../status-bar/actions/status-bar.actions';
 import { SetOverlaysCriteriaAction } from '../../../../../overlays/actions/overlays.actions';
 import { selectRegion } from '../../../../../overlays/reducers/overlays.reducer';
 import { CaseGeoFilter, CaseRegionState } from '../../../../../menu-items/cases/models/case.model';
 import { LayersActionTypes, SetLayerSelection } from '../../../../../menu-items/layers-manager/actions/layers.actions';
+import { regionLayerId } from '../../../../../menu-items/layers-manager/models/layers.model';
 
 export abstract class RegionVisualizer extends EntitiesVisualizer {
 	selfIntersectMessage = 'Invalid Polygon (Self-Intersect)';
 	newRegionSelect$ = this.store$.select(selectRegion);
-
-	regionSameAsVisualizer$ = this.newRegionSelect$.pipe(
-		map(region => region.geometry.type === this.geoFilter)
-	);
 
 	isActiveMap$ = this.store$.select(selectActiveMapId).pipe(
 		map((activeMapId: string): boolean => activeMapId === this.mapId),
 		distinctUntilChanged()
 	);
 
-	geoFilterType$: Observable<CaseGeoFilter> = this.store$.select(selectGeoFilterType);
-
-	geoFilterActive$ = this.store$.select(selectGeoFilterActive);
-
-	isTheRightVisualizer$ = this.geoFilterType$.pipe(
-		map(geoFilter => geoFilter === this.geoFilter)
+	isOverlayDisplay$ = this.store$.pipe(
+		select(selectMaps),
+		map((maps) => maps[this.mapId] && Boolean(maps[this.mapId].data.overlay)),
 	);
 
-	@AutoSubscription
-	shouldDrawRegion$ = combineLatest([this.store$.select(selectMaps), this.newRegionSelect$]).pipe(
-		map(([maps, region]) => {
-			const isOverlay = Boolean(maps[this.mapId]?.data.overlay);
-			const isNotScreenViewMode = region.properties.searchMode !== CaseGeoFilter.ScreenView;
-			return isOverlay || isNotScreenViewMode;
-		})
-	);
+	selectGeoFilter$ = this.store$.pipe(select(selectGeoFilterStatus));
 
-	@AutoSubscription
-	updateLayer$: Observable<any> = this.actions$.pipe(
+	regionLayersSelectedChange$ = this.actions$.pipe(
 		ofType<SetLayerSelection>(LayersActionTypes.SET_LAYER_SELECTION),
-		filter(action => action.payload.id === 'region-layer'),
-		tap((action) => {
-			this.setVisibility(action.payload.value);
-		})
+		filter(action => action.payload.id === regionLayerId),
+		map((action) => action.payload.value)
+	);
+
+	isMinimalisticView$ = this.store$.pipe(
+		select(selectIsMinimalistViewMode),
+		map(IsMinimalistView => !IsMinimalistView)
+	);
+
+	shouldDraw$: Observable<boolean> = combineLatest([this.selectGeoFilter$, this.isOverlayDisplay$]).pipe(
+		map(this.shouldDrawRegion.bind(this))
+	);
+
+	@AutoSubscription
+	updateLayer$: Observable<any> = merge(this.isMinimalisticView$, this.regionLayersSelectedChange$).pipe(
+		tap((show) => this.setVisibility(show))
 	);
 
 	@AutoSubscription
 	onContextMenu$: Observable<any> = this.actions$.pipe(
 		ofType<ContextMenuTriggerAction>(MapActionTypes.TRIGGER.CONTEXT_MENU),
-		withLatestFrom(this.isTheRightVisualizer$),
-		filter(([action, rightVisualizer]) => Boolean(rightVisualizer)),
+		withLatestFrom(this.selectGeoFilter$),
+		filter(([action, geoFilter]) => this.isTheRightVisualizer(geoFilter)),
 		map(([{ payload }]) => payload),
 		tap(this.onContextMenu.bind(this))
 	);
 
 	@AutoSubscription
-	interactionChanges$: Observable<any> = combineLatest([this.geoFilterType$, this.geoFilterActive$, this.isActiveMap$]).pipe(
+	interactionChanges$: Observable<any> = combineLatest([this.selectGeoFilter$, this.isActiveMap$]).pipe(
 		tap(this.interactionChanges.bind(this))
 	);
 
 	@AutoSubscription
-	drawChanges$ = combineLatest([this.geoFilterType$, this.newRegionSelect$, this.regionSameAsVisualizer$, this.geoFilterActive$, this.store$.select(selectIsMinimalistViewMode), this.shouldDrawRegion$, this.isTheRightVisualizer$]).pipe(
+	drawChanges$ = combineLatest([this.newRegionSelect$, this.shouldDraw$]).pipe(
 		mergeMap(this.drawChanges.bind(this))
 	);
 
-	constructor(public store$: Store<any>, public actions$: Actions, public projectionService: OpenLayersProjectionService, public geoFilter: CaseGeoFilter) {
+	protected constructor(public store$: Store<any>, public actions$: Actions, public projectionService: OpenLayersProjectionService, public geoFilter: CaseGeoFilter) {
 		super();
 	}
 
-	drawChanges([geoFilterType, region, regionAsVisualizer, isActive, isMinimalistViewMode, shouldDrawRegion, isTheRightVisualizer]) {
-		if (regionAsVisualizer && !isActive && !isMinimalistViewMode && geoFilterType && shouldDrawRegion && isTheRightVisualizer) {
+	drawChanges([region, shouldDraw]) {
+		if (shouldDraw) {
 			return this.drawRegionOnMap(region);
 		}
 		this.clearEntities();
@@ -102,8 +100,8 @@ export abstract class RegionVisualizer extends EntitiesVisualizer {
 				const [geoJsonFeature] = featureCollection.features;
 				const region = this.createRegion(geoJsonFeature);
 				if (region.geometry.type === 'Point' || turf.kinks(region.geometry).features.length === 0) {  // turf way to check if there are any self-intersections
-					this.store$.dispatch(new SetOverlaysCriteriaAction({ region }));
 					this.store$.dispatch(new UpdateGeoFilterStatus({ active: false }));
+					this.store$.dispatch(new SetOverlaysCriteriaAction({ region }));
 				} else {
 					this.store$.dispatch(new SetToastMessageAction({
 						toastText: this.selfIntersectMessage
@@ -134,9 +132,9 @@ export abstract class RegionVisualizer extends EntitiesVisualizer {
 		this.store$.dispatch(new UpdateGeoFilterStatus({ active: false }));
 	}
 
-	interactionChanges([geoFilterSearch, isGeoActive, isActiveMap]: [CaseGeoFilter, boolean, boolean]): void {
+	interactionChanges([geoFilter, isActiveMap]: [IGeoFilterStatus, boolean]): void {
 		this.removeDrawInteraction();
-		if (geoFilterSearch === this.geoFilter && isGeoActive && isActiveMap && this.geoFilter) {
+		if (geoFilter.type === this.geoFilter && geoFilter.active && isActiveMap) {
 			this.createDrawInteraction();
 		}
 	}
@@ -144,6 +142,14 @@ export abstract class RegionVisualizer extends EntitiesVisualizer {
 	onDispose() {
 		this.removeDrawInteraction();
 		super.onDispose();
+	}
+
+	shouldDrawRegion([geoFilter, isOverlayDisplay]) {
+		return this.isTheRightVisualizer(geoFilter) && !geoFilter.active
+	}
+
+	isTheRightVisualizer(geoFilter: IGeoFilterStatus): boolean {
+		return geoFilter.type === this.geoFilter
 	}
 
 	abstract drawRegionOnMap(region: CaseRegionState): Observable<boolean> ;

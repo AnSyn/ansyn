a0a94a0754c4ee350fd861e866111520ec5549d5
yehonatanhal <yehonatanhal@gmail.com>
2021-04-27 16:50:21 +0300

    * remove unneeded separate import

--- src/app/@ansyn/imagery/public_api.ts
+++ src/app/@ansyn/imagery/public_api.ts
@@ -96,6 +96,5 @@ export { IMAGERY_SLOW_ZOOM_FACTOR } from './model/imagery-zoom.model';
 export { toDegrees, toRadians, getAngleDegreeBetweenPoints, randomColor } from './utils/math';
 export { ExtentCalculator } from './utils/extent-calculator';
 export { ImageryComponent } from './imagery/imagery.component';
-export { getOpacityFromColor } from './utils/color-calculator';
+export { getOpacityFromColor, stringToHexRGB } from './utils/color-calculator';
 export { ExtentCollector } from './utils/extent-collector';
-export { stringToHexRGB } from './utils/color-calculator'
